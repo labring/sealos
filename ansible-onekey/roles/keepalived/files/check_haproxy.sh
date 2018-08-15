49c05d09231c418d37459a37ae951dfb5c739edd
fanux <fhtjob@hotmail.com>
2018-08-15 21:35:16 +0800

    add

--- ansible-onekey/roles/keepalived/files/check_haproxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ `ps -C haproxy --no-header |wc -l` -eq 0 ] ; then
-    systemctl restart haproxy
-    sleep 3
-    if [ `ps -C haproxy --no-header |wc -l` -eq 0 ] ; then
-        systemctl stop keepalived
-    fi
-fi
