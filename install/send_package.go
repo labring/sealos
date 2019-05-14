4829a6cb1d3cddc6462db7a7e4697d65c7a7d715
cuisongliu <cuisongliu@qq.com>
2019-05-14 14:40:16 +0800

    fix port support

--- install/send_package.go
+++ install/send_package.go
@@ -36,7 +36,7 @@ func (s *SealosInstaller) SendPackage(url string) {
 			logger.Debug("please wait for tar zxvf exec")
 			if RemoteFilExist(master, port, kubeLocal) {
 				logger.Warn("host is ", master, ", SendPackage: file is exist")
-				Cmd(master, localCmd)
+				Cmd(master, port, localCmd)
 			} else {
 				if isHTTP {
 					go WatchFileSize(master, port, kubeLocal, GetFileSize(url))
@@ -56,7 +56,7 @@ func (s *SealosInstaller) SendPackage(url string) {
 			logger.Debug("please wait for tar zxvf exec")
 			if RemoteFilExist(node, port, kubeLocal) {
 				logger.Warn("host is ", node, ", SendPackage: file is exist")
-				Cmd(node, localCmd)
+				Cmd(node, port, localCmd)
 			} else {
 				if isHTTP {
 					go WatchFileSize(node, port, kubeLocal, GetFileSize(url))
