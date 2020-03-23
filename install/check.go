f58ba22f75b89263dd7e86d3aa6dba379450fee1
PatHoo <quith@qq.com>
2020-03-23 17:34:34 +0800

    init code

--- install/check.go
+++ install/check.go
@@ -1,9 +1,12 @@
 package install
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/wonderivan/logger"
 	"golang.org/x/crypto/ssh"
 	"os"
+	"strings"
 )
 
 //CheckValid is
@@ -28,6 +31,7 @@ func (s *SealosInstaller) CheckValid() {
 	}
 	var session *ssh.Session
 	var errors []error
+	var hostname []string
 	for _, h := range s.Hosts {
 		session, err := SSHConfig.Connect(h)
 		if err != nil {
@@ -35,10 +39,24 @@ func (s *SealosInstaller) CheckValid() {
 			logger.Error("[%s] ------------ error[%s]", h, err)
 			errors = append(errors, err)
 		} else {
+			//check hostname
+			var stdOut, stdErr bytes.Buffer
+			session.Stdout = &stdOut
+			session.Stderr = &stdErr
+			session.Run("hostname")
+			ret := strings.Replace( stdOut.String(), "\n", "", -1 )
+			fmt.Printf("%s\n", ret)
+			hostname=append(hostname,ret)
+
 			logger.Crit("[%s]  ------------ check ok", h)
 			logger.Crit("[%s]  ------------ session[%p]", h, session)
 		}
 	}
+
+	fmt.Println(hostname[0])
+	fmt.Println(hostname[1])
+	fmt.Println(hostname[2])
+	fmt.Println(hostname[3])
 	defer func() {
 		if session != nil {
 			session.Close()
@@ -48,4 +66,12 @@ func (s *SealosInstaller) CheckValid() {
 		logger.Error("has some linux server is connection ssh is failed")
 		os.Exit(1)
 	}
+	os.Exit(1)
+}
+
+func InSlice (arr []string, val string) bool{
+	for _, v := range arr {
+		if v == val { return true }
+	}
+	return false
 }
