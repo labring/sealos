9c0629c58c0e2d29f889a0595c7c967f90bc78cd
oldhtreefeng <louisehong4168@gmail.com>
2020-07-02 23:11:00 +0800

    \#325 pkg-url 文件写错了位置, 不能及时停止. 判断pkgurl本地文件是否存在, 下载的文件如果小于400M ,则直接退出

--- install/utils_test.go
+++ install/utils_test.go
@@ -122,3 +122,25 @@ func TestDecodeIPs(t *testing.T) {
 		})
 	}
 }
+
+func TestFileExist(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"file exist",args{"/home/louis/.ssh/id_rsa"},true},
+		{"file not exist",args{"/home/louis/.ssh/id_rsa.public"},false},
+		{"PkgFile",args{"/root/kube1.18.0.tar.gz"},false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FileExist(tt.args.path); got != tt.want {
+				t.Errorf("FileExist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
