a252172539865f8b1eb4696fd32eb5c6d8c7afef
louisehong <louisehong4168@gmail.com>
2020-07-27 19:20:20 +0800

    test drone

--- version/version.go
+++ version/version.go
@@ -3,6 +3,7 @@ package version
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 var (
@@ -10,5 +11,14 @@ var (
 	Build     = ""
 	BuildTime = ""
 	// VersionStr = fmt.Sprintf("sealos version %v, build %v %v", Version, Build, runtime.Version())
-	VersionStr = fmt.Sprintf("sealos version %v, build %v %v, Build Time : %v", Version, Build, runtime.Version(), BuildTime)
+	VersionStr = fmt.Sprintf("sealos version %v, build %v %v, Build Time : %v", Version, Build, runtime.Version(), TimeStamp(BuildTime))
 )
+
+// fix drone not support CST timestamp.  # https://github.com/drone/drone/pull/2863
+func TimeStamp(buildTime string)  string {
+	if buildTime == "" {
+		t := time.Now()
+		return fmt.Sprintf("%s", t.Format("2006-01-02 15:04:05"))
+	}
+	return buildTime
+}
