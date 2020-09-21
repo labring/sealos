9af61d0480e9662c4daecda5d91358e59ed03b64
oldthreefeng <louisehong4168@gmail.com>
2020-09-21 10:56:51 +0800

    dockerfile add upx stage build

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,14 @@ ENV GOPROXY=https://goproxy.io
 WORKDIR /root
 
 COPY . .
+RUN go mod download
 
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o /sealos -ldflags "-s -w -X github.com/fanux/sealos/version.Version=latest  -X github.com/fanux/sealos/version.Build= -X github.com/fanux/sealos/version.BuildTime=" main.go
+
+FROM alpine AS UPX
+COPY --from=builder /sealos /sealos
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories && \
 	apk add --update upx && upx /sealos
 FROM scratch
-COPY --from=builder /sealos /bin/sealos
+COPY --from=UPX /sealos /bin/sealos
 ENTRYPOINT ["/bin/sealos"]
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # build.sh v3.0.2
 COMMIT_SHA1=$(git rev-parse --short HEAD || echo "0.0.0")
 BUILD_TIME=$(date "+%F %T")
-go build -o sealos  -ldflags "-s -w -X github.com/fanux/sealos/version.Version=$1 -X github.com/fanux/sealos/version.Build=${COMMIT_SHA1} -X 'github.com/fanux/sealos/version.BuildTime=${BUILD_TIME}'" main.go && \
-command  -v upx &> /dev/null && upx sealos
+goldflags="-s -w -X 'github.com/fanux/sealos/version.Version=$1' -X 'github.com/fanux/sealos/version.Build=${COMMIT_SHA1}' -X 'github.com/fanux/sealos/version.BuildTime=${BUILD_TIME}'"
+go build -o sealos -ldflags "$goldflags" main.go && command -v upx &> /dev/null && upx sealos
