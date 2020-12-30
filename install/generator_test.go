6ec733a7cebb8915e177bcf0c81d6efe9e6706af
oldthreefeng <louisehong4168@gmail.com>
2020-12-31 01:49:44 +0800

    add test in pr and fix go test ./... print OK
    Signed-off-by: oldthreefeng <louisehong4168@gmail.com>

--- install/generator_test.go
+++ install/generator_test.go
@@ -11,7 +11,7 @@ func Test_generatorKubeadmConfig(t *testing.T) {
 }
 
 func TestTemplate(t *testing.T) {
-	var masters = []string{"172.20.241.205", "172.20.241.206", "172.20.241.207"}
+	var masters = []string{"172.20.241.205:22", "172.20.241.206:22", "172.20.241.207:22"}
 	var vip = "10.103.97.1"
 	config := sshutil.SSH{
 		User:     "cuisongliu",
