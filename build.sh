7519f4d989a039a209280026a5868660d94224a1
cuisongliu <cuisongliu@qq.com>
2021-12-07 23:11:23 +0800

    feat(develop): fix ci dir for sealos (#735)
    * feat(develop): fix ci dir for sealos

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# build.sh v3.0.2
-COMMIT_SHA1=$(git rev-parse --short HEAD || echo "0.0.0")
-BUILD_TIME=$(date "+%F %T")
-goldflags="-s -w -X 'github.com/linuxsuren/cobra-extension/version.version=$1' -X 'github.com/linuxsuren/cobra-extension/version.commit=${COMMIT_SHA1}' -X 'github.com/linuxsuren/cobra-extension/version.date=${BUILD_TIME}'"
-go build -o sealos -ldflags "$goldflags" main.go && command -v upx &> /dev/null && upx sealos
