e933c203c4692e792bf1e3ba4f10239a2de4b657
cuisongliu <cuisongliu@qq.com>
2022-04-10 02:50:50 +0800

    refactor(master): add note (#925)

--- hack/release/note.sh
+++ hack/release/note.sh
@@ -17,13 +17,25 @@ echo "### Usage" >> Note.md
 echo "
 \`\`\`sh
 # 下载并安装sealos, sealos是个golang的二进制工具，直接下载拷贝到bin目录即可, release页面也可下载
-$ wget -c https://sealyun-home.oss-cn-beijing.aliyuncs.com/sealos-4.0/latest/sealos && \\
+wget -c https://sealyun-home.oss-cn-beijing.aliyuncs.com/sealos-4.0/latest/sealos-amd64 -O sealos && \\
     chmod +x sealos && mv sealos /usr/bin
+# 下载buildah
+wget https://sealyun-home.oss-accelerate.aliyuncs.com/images/buildah.linux.amd64 --no-check-certificate -O buildah && \
+chmod a+x buildah && mv buildah /usr/bin
+# 创建一个集群
+sealos run registry.cn-hongkong.aliyuncs.com/sealyun/oci-kubernetes:1.22.8 registry.cn-hongkong.aliyuncs.com/sealyun/oci-calico:v3.22.1 --masters 192.168.64.2,192.168.64.22,192.168.64.20 --nodes 192.168.64.21,192.168.64.19
+
 \`\`\`
 " >> Note.md
-echo "### [amd64 下载地址]" >> Note.md
+
+echo "### [amd64 sealos 下载地址]" >> Note.md
 echo "[oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/${VERSION}/sealos-amd64)" >> Note.md
-echo "[latest 版本 oss下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/latest/sealos-amd64)" >> Note.md
-echo "### [arm64 下载地址]" >> Note.md
+echo "[latest 版本 oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/latest/sealos-amd64)" >> Note.md
+echo "### [arm64 sealos 下载地址]" >> Note.md
 echo "[oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/${VERSION}/sealos-arm64)" >> Note.md
 echo "[latest 版本 oss下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/latest/sealos-arm64)" >> Note.md
+
+echo "### [amd64 buildah 下载地址]" >> Note.md
+echo "[oss下载地址](https://sealyun-home.oss-accelerate.aliyuncs.com/images/buildah.linux.amd64)" >> Note.md
+echo "### [arm64 buildah 下载地址]" >> Note.md
+echo "[oss下载地址](https://sealyun-home.oss-accelerate.aliyuncs.com/images/buildah.linux.arm64)" >> Note.md
