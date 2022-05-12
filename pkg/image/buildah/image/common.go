00b274763fbfd1aa3365a886da1169dbfcecd288
maybaby <ybyang7@iflytek.com>
2022-05-12 23:43:23 +0800

    fix: licensne of buildah ref
    Signed-off-by: maybaby <ybyang7@iflytek.com>

--- pkg/image/buildah/image/common.go
+++ pkg/image/buildah/image/common.go
@@ -1,10 +1,10 @@
-// Copyright © 2022 sealos.
+// Copyright © 2022 buildah.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     https://github.com/containers/buildah/blob/main/LICENSE
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
