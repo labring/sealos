c2c412ee12506658784c704c4efe2e2688a120b6
yyf1986 <191557539@qq.com>
2022-05-13 19:41:18 +0800

    feature: add pull commands using buildah

--- pkg/image/buildah/registry/registry.go
+++ pkg/image/buildah/registry/registry.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 191557539@qq.com.
+Copyright 2022 sealos.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
