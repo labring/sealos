2a6c7ffc7cc3211cc123a1d28cdb5f923288dd7a
YAO <yyf1986@users.noreply.github.com>
2022-05-15 15:06:47 +0800

    Update push.go
    feature: add push commands using buildah #986

--- pkg/image/buildah/registry/push.go
+++ pkg/image/buildah/registry/push.go
@@ -1,4 +1,4 @@
-// Copyright © 2022 sealos.
+// Copyright © 2022 buildah.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
