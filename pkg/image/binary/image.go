ab55104afdd4de3ed50f7489ee7e6a01f7dbb660
yyf1986 <191557539@qq.com>
2022-06-22 11:34:48 +0800

    parse images in helm charts #1004

--- pkg/image/binary/image.go
+++ pkg/image/binary/image.go
@@ -109,6 +109,13 @@ func inspectImage(data string) (*v1.Image, error) {
 }
 
 func (d *ImageService) Build(options *types.BuildOptions, contextDir, imageName string) error {
+	// charts
+	chartFetchDir := path.Join(contextDir, constants.ChartsDirName)
+	chartImages, err := buildimage.ParseChartImages(chartFetchDir)
+	logger.Info("fetch chart images: %v", chartImages)
+	if err != nil {
+		return errors.Wrap(err, "get chart images list failed in this context")
+	}
 	//constants.ImageShimDirName
 	imageFetchDir := path.Join(contextDir, constants.ManifestsDirName)
 	yamlImages, err := buildimage.ParseYamlImages(imageFetchDir)
@@ -122,6 +129,7 @@ func (d *ImageService) Build(options *types.BuildOptions, contextDir, imageName 
 		return errors.Wrap(err, "load images list failed in this context")
 	}
 	images = append(images, yamlImages...)
+	images = append(images, chartImages...) // add chart
 	auths, err := registry.GetAuthInfo()
 	if err != nil {
 		return err
