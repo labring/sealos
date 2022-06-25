cb85fb98fc491e75e5154f268ee77bc39a6047c3
zzjin <zzjin@users.noreply.github.com>
2022-06-25 12:51:20 +0800

    Using module to simpler vsc source tree. Closes #1771 (#1172)

--- vendor/github.com/containers/buildah/define/types_unsupported.go
+++
@@ -1,6 +0,0 @@
-// +build !linux,!darwin
-
-package define
-
-// ContainerDevices is currently not implemented.
-type ContainerDevices = []struct{}
--- vendor/github.com/containers/common/libnetwork/netavark/const.go
+++
@@ -1,6 +0,0 @@
-//go:build linux
-// +build linux
-
-package netavark
-
-const defaultBridgeName = "podman"
