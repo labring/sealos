b889b1888b7270012351022d57ce7d6c9572e3f6
cuisongliu <cuisongliu@qq.com>
2022-07-14 15:49:00 +0800

    refactor(main): add note for image and apt deb (#1307)
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- scripts/release/note.sh
+++ scripts/release/note.sh
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "### Usage" >> Note.md
-echo "
+cat << EOF > Note.md
+### Usage
 \`\`\`sh
 # 下载并安装sealos, sealos是个golang的二进制工具，直接下载拷贝到bin目录即可, release页面也可下载
 wget -c https://sealyun-home.oss-cn-beijing.aliyuncs.com/sealos-4.0/latest/sealos-amd64 -O sealos && \\
@@ -24,18 +24,54 @@ sealos run labring/kubernetes:v1.24.0 labring/calico:v3.22.1 \
     --masters 192.168.64.2,192.168.64.22,192.168.64.20 \
     --nodes 192.168.64.21,192.168.64.19 \
     --passwd your-own-ssh-passwd
+\`\`\`
+
+### amd64 sealos 下载地址
+[oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/${VERSION}/sealos-amd64)
+[latest 版本 oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/latest/sealos-amd64)
+### arm64 sealos 下载地址
+[oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/${VERSION}/sealos-arm64)
+[latest 版本 oss下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/latest/sealos-arm64)
+### amd64 buildah 下载地址
+[oss下载地址](https://sealyun-home.oss-accelerate.aliyuncs.com/images/buildah.linux.amd64)
+### arm64 buildah 下载地址
+[oss下载地址](https://sealyun-home.oss-accelerate.aliyuncs.com/images/buildah.linux.arm64)
+
+### Docker images
 
 \`\`\`
-" >> Note.md
+docker pull ghcr.io/${USERNAME:-labring}/lvscare:${VERSION}
+\`\`\`
 
-echo "### [amd64 sealos 下载地址]" >> Note.md
-echo "[oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/${VERSION}/sealos-amd64)" >> Note.md
-echo "[latest 版本 oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/latest/sealos-amd64)" >> Note.md
-echo "### [arm64 sealos 下载地址]" >> Note.md
-echo "[oss 下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/${VERSION}/sealos-arm64)" >> Note.md
-echo "[latest 版本 oss下载地址](https://${BUCKETNAME:-sealyun-home}.${OSSENDPOINT:-oss-cn-beijing.aliyuncs.com}/sealos-4.0/latest/sealos-arm64)" >> Note.md
+### APT 源
 
-echo "### [amd64 buildah 下载地址]" >> Note.md
-echo "[oss下载地址](https://sealyun-home.oss-accelerate.aliyuncs.com/images/buildah.linux.amd64)" >> Note.md
-echo "### [arm64 buildah 下载地址]" >> Note.md
-echo "[oss下载地址](https://sealyun-home.oss-accelerate.aliyuncs.com/images/buildah.linux.arm64)" >> Note.md
+Use your public APT Repository URL to install DEB packages:
+
+\`\`\`
+https://apt.fury.io/cuisongliu/
+\`\`\`
+
+To enable, add the following file **/etc/apt/sources.list.d/fury.list**:
+
+\`\`\`
+deb [trusted=yes] https://apt.fury.io/cuisongliu/ /
+\`\`\`
+
+### Yum源
+
+Use your public YUM Repository URL to install RPM packages:
+
+\`\`\`
+https://yum.fury.io/cuisongliu/
+\`\`\`
+
+To enable, add the following file **/etc/yum.repos.d/fury.repo**:
+
+\`\`\`
+[fury]
+name=Gemfury Private Repo
+baseurl=https://yum.fury.io/cuisongliu/
+enabled=1
+gpgcheck=0
+\`\`\`
+EOF
