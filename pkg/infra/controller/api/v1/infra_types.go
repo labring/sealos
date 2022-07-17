3be784f70669894b1cab3a90f9b6400b6107776d
fanux <fhtjob@hotmail.com>
2022-07-17 12:11:15 +0800

    add infra CRD spec
    Signed-off-by: fanux <fhtjob@hotmail.com>

--- pkg/infra/controller/api/v1/infra_types.go
+++ pkg/infra/controller/api/v1/infra_types.go
@@ -17,19 +17,81 @@ limitations under the License.
 package v1
 
 import (
+	"github.com/labring/sealos/pkg/types/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+/*
+apiVersion: infra.sealos.io/v1
+kind: Infra
+metadata:
+  name: aws-infra-demo
+  annotations:
+    infra.sealos.io/VpcID: v-xxxxx
+    infra.sealos.io/RouteID: v-xxxxx
+    infra.sealos.io/IgwID: v-xxxxxx
+    infra.sealos.io/EgwID: v-xxxxxx
+    infra.sealos.io/SubnetID: v-xxxxx
+    infra.sealos.io/SecurityGroupID: v-xxxxx
+    infra.sealos.io/EIPID: v-xxxxx
+spec:
+    regionIds: [cn-hangzhou, cn-shanghai]
+    zoneIds: [cn-hangzhou-a, cn-hangzhou-b]
+    ssh:
+       passwd: xxx
+       pk: /root/.ssh/id_rsa
+       port: 22
+       user: root
+   hosts:
+   - roles: [master, aaa, bbb] # required
+     count: 3 # Required
+     # key values resources.
+     resources:
+        cpu: 2
+        memory: 4
+        # other resources like GPU
+     # ENUM: amd64/arm64 (NOTE: the default value is amd64)
+     flavor: ecs.t5-lc1m2.large
+     arch: amd64
+     # ENUM: ubuntu:20.04, centos:7.2 and so on.
+     image: utuntu:20.04
+     disks:
+     - capacity: 50
+       # ENUM: system/data
+       type: system
+*/
+
+type Hosts struct {
+	Roles []string `json:"hosts,omitempty"`
+	Count int      `json:"count,omitempty"`
+	// key values resources.
+	// cpu: 2
+	// memory: 4
+	// other resources like GPU
+	Resources map[string]string `json:"resources,omitempty"`
+	// ecs.t5-lc1m2.large
+	Flavor string `json:"flavor,omitempty"`
+	// ENUM: amd64/arm64 (NOTE: the default value is amd64)
+	Arch string `json:"arch,omitempty"`
+	// ENUM: ubuntu:20.04, centos:7.2 and so on.
+	Image string `json:"arch,omitempty"`
+	Disks []struct {
+		Capacity string `json:"capacity,omitempty"`
+		// ENUM: system/data
+		Type string `json:"type,omitempty"`
+	}
+}
 
 // InfraSpec defines the desired state of Infra
 type InfraSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// desired state of infra
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Infra. Edit infra_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// RegionIDs is cloud provider regionID list
+	RegionIDs []string    `json:"regionIDs,omitempty"`
+	ZoneIDs   []string    `json:"regionIDs,omitempty"`
+	SSH       v1beta1.SSH `json:"ssh,omitempty"`
+	Hosts     []Hosts     `json:"hosts,omitempty"`
 }
 
 // InfraStatus defines the observed state of Infra
