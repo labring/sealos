adbf8c011c6416887fed1bc62ba1906411eaddda
zzjin <tczzjin@gmail.com>
2022-07-20 14:56:07 +0000

    logger support fulltext color
    Signed-off-by: zzjin <tczzjin@gmail.com>

--- pkg/utils/logger/logger.go
+++ pkg/utils/logger/logger.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -80,6 +81,9 @@ func newConsoleCore(le zapcore.LevelEnabler) zapcore.Core {
 	zec := zap.NewProductionEncoderConfig()
 	zec.EncodeLevel = zapcore.LowercaseColorLevelEncoder
 	zec.EncodeTime = zapcore.ISO8601TimeEncoder
+	zec.EncodeTime = shortTimeEncoder
+	// zec.EncodeTime = zapcore.ISO8601TimeEncoder
+	zec.ConsoleSeparator = " "
 
 	consoleEncoder := zapcore.NewConsoleEncoder(zec)
 
@@ -100,6 +104,12 @@ func newFileCore(filename string, le zapcore.LevelEnabler) zapcore.Core {
 	return zapcore.NewCore(fileEncoder, fileLogger, le)
 }
 
+const shortTimeLayout = "2006-01-02T15:04:05"
+
+func shortTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format(shortTimeLayout))
+}
+
 // IsDebugMode check DebugLevel enabled
 func IsDebugMode() bool {
 	return defaultLogger.Core().Enabled(zapcore.DebugLevel)
@@ -107,41 +117,41 @@ func IsDebugMode() bool {
 
 // Fatal logs a message at emergency level and exit.
 func Fatal(f interface{}, v ...interface{}) {
-	defaultLogger.Sugar().Fatalf(formatLog(f, v...))
+	defaultLogger.Sugar().Fatalf(formatLog(zapcore.FatalLevel, f, v...))
 }
 
 // Panic logs a message at emergency level and exit.
 func Panic(f interface{}, v ...interface{}) {
-	defaultLogger.Sugar().Panicf(formatLog(f, v...))
+	defaultLogger.Sugar().Panicf(formatLog(zapcore.PanicLevel, f, v...))
 }
 
 // Error logs a message at error level.
 func Error(f interface{}, v ...interface{}) {
-	defaultLogger.Sugar().Errorf(formatLog(f, v...))
+	defaultLogger.Sugar().Errorf(formatLog(zapcore.ErrorLevel, f, v...))
 }
 
 // Warn logs a message at warning level.
 func Warn(f interface{}, v ...interface{}) {
-	defaultLogger.Sugar().Warnf(formatLog(f, v...))
+	defaultLogger.Sugar().Warnf(formatLog(zapcore.WarnLevel, f, v...))
 }
 
 // Info logs a message at info level.
 func Info(f interface{}, v ...interface{}) {
-	defaultLogger.Sugar().Infof(formatLog(f, v...))
+	defaultLogger.Sugar().Infof(formatLog(zapcore.InfoLevel, f, v...))
 }
 
 // Debug logs a message at debug level.
 func Debug(f interface{}, v ...interface{}) {
-	defaultLogger.Sugar().Debugf(formatLog(f, v...))
+	defaultLogger.Sugar().Debugf(formatLog(zapcore.DebugLevel, f, v...))
 }
 
-func formatLog(f interface{}, v ...interface{}) string {
+func formatLog(l zapcore.Level, f interface{}, v ...interface{}) string {
 	var msg string
 	switch f := f.(type) {
 	case string:
 		msg = f
 		if len(v) == 0 {
-			return msg
+			return appendColor(l, msg)
 		}
 		if strings.Contains(msg, "%") && !strings.Contains(msg, "%%") {
 			//format string
@@ -152,9 +162,23 @@ func formatLog(f interface{}, v ...interface{}) string {
 	default:
 		msg = fmt.Sprint(f)
 		if len(v) == 0 {
-			return msg
+			return appendColor(l, msg)
 		}
 		msg += strings.Repeat(" %v", len(v))
 	}
-	return fmt.Sprintf(msg, v...)
+	return appendColor(l, fmt.Sprintf(msg, v...))
+}
+
+func appendColor(l zapcore.Level, s string) string {
+	// default all red
+	c := uint8(31)
+	switch l {
+	case zapcore.DebugLevel:
+		c = uint8(35) // Magenta
+	case zapcore.InfoLevel:
+		c = uint8(34) // Blue
+	case zapcore.WarnLevel:
+		c = uint8(33) // Yellow
+	}
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", c, s)
 }
