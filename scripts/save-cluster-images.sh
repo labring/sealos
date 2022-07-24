8ef79c86f93ef36d2031e8c9451b04b085fae737
Mercurio <signormercurio@gmail.com>
2022-07-24 20:47:27 +0800

    ci: fix sudo error
    Signed-off-by: Mercurio <signormercurio@gmail.com>

--- scripts/save-cluster-images.sh
+++ scripts/save-cluster-images.sh
@@ -3,21 +3,25 @@
 IMAGE=localhost:5000/labring/lvscare:$GIT_COMMIT_SHORT_SHA-$ARCH
 PATCH=docker.io/labring/patch:$GIT_COMMIT_SHORT_SHA-$ARCH
 PATCH_DIR=docker/patch
+SEALOS=sudo sealos
+
+# download sealos
+wget https://github.com/labring/sealos/releases/download/v4.0.0/sealos_4.0.0_linux_amd64.tar.gz
+tar -zxf sealos_4.0.0_linux_amd64.tar.gz sealos
+chmod +x sealos && sudo mv sealos /usr/bin/sealos
 
 # resolve buildah conflicts
 sudo apt remove buildah
 sudo apt autoremove
 
 # use correct image name
-mkdir -p $PATCH_DIR/images/shim
-echo "$IMAGE" > $PATCH_DIR/images/shim/lvscareImage
-sed -i "s#__lvscare__#$IMAGE#g" $PATCH_DIR/Dockerfile
+cd $PATCH_DIR
+mkdir -p images/shim
+echo "$IMAGE" > images/shim/lvscareImage
+sed -i "s#__lvscare__#$IMAGE#g" Dockerfile
 
-# download sealos
-wget https://github.com/labring/sealos/releases/download/v4.0.0/sealos_4.0.0_linux_amd64.tar.gz
-tar -zxf sealos_4.0.0_linux_amd64.tar.gz sealos
-chmod +x sealos
+# build patch image, must be under $PATCH_DIR
+$SEALOS build -t $PATCH --platform linux/$ARCH .
 
-# build and save patch image
-sudo ./sealos build -t $PATCH --platform linux/$ARCH -f $PATCH_DIR/Dockerfile $PATCH_DIR
-sudo ./sealos save -o patch-$ARCH.tar $PATCH
+# save patch image
+cd - && $SEALOS save -o patch-$ARCH.tar $PATCH
