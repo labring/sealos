ebf328e6f587b8e263748fa893c8f95db96b6200
Mercurio <signormercurio@gmail.com>
2022-07-24 18:15:51 +0800

    ci: fix path error
    Signed-off-by: Mercurio <signormercurio@gmail.com>

--- scripts/save-cluster-images.sh
+++ scripts/save-cluster-images.sh
@@ -2,22 +2,19 @@
 
 IMAGE=localhost:5000/labring/lvscare:$COMMIT_SHA-$ARCH
 PATCH=docker.io/labring/patch:$COMMIT_SHA-$ARCH
-PWD=docker/sealos
+SEALOS=docker/sealos/sealos
+PATCH_DIR=docker/patch
 
-cd $PWD
-
-# make sure sealos binary is executable and resolve buildah conflicts
-chmod +x sealos
+# resolve buildah conflicts
 sudo apt remove buildah
 sudo apt autoremove
 
 # use correct image name
-mkdir -p docker/patch/images/shim
-echo "$IMAGE" > docker/patch/images/shim/lvscareImage
-sed -i "s#__lvscare__#$IMAGE#g" docker/patch/Dockerfile
+mkdir -p $PATCH_DIR/images/shim
+echo "$IMAGE" > $PATCH_DIR/images/shim/lvscareImage
+sed -i "s#__lvscare__#$IMAGE#g" $PATCH_DIR/Dockerfile
 
-cd docker/patch
-
-# build and save patch image
-sudo $PWD/sealos build -t $PATCH --platform linux/$ARCH .
-cd ../.. && sudo $PWD/sealos save -o patch-$ARCH.tar $PATCH
+# make sure sealos binary is executable, build and save patch image
+chmod +x $SEALOS
+sudo $SEALOS build -t $PATCH --platform linux/$ARCH -f $PATCH_DIR/Dockerfile $PATCH_DIR
+sudo $SEALOS save -o patch-$ARCH.tar $PATCH
