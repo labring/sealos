0874386990090e59312a895fc794e12ec6d468d8
中弈 <fhtjob@hotmail.com>
2022-08-16 15:57:02 +0800

    init applications controller, add application actions define (#1538)
    Signed-off-by: fanux <fanux@sealos.io>

--- controllers/app/main.go
+++ controllers/app/main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "App")
 		os.Exit(1)
 	}
+	if err = (&controllers.ActionsReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Actions")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
