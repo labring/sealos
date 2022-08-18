9bebed5f4ac7d41c3d7c10c9949deb4d60bc8e9f
cuisongliu <cuisongliu@qq.com>
2022-08-18 23:29:12 +0800

    feature(main): add user controller logic
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- controllers/user/api/v1/usergroup_types.go
+++ controllers/user/api/v1/usergroup_types.go
@@ -17,35 +17,21 @@ limitations under the License.
 package v1
 
 import (
-	v1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// UserGroupSpec defines the desired state of UserGroup
-type UserGroupSpec struct {
-}
-
-// UserGroupStatus defines the observed state of UserGroup
-type UserGroupStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster,shortName=ug
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 
 // UserGroup is the Schema for the usergroups API
 type UserGroup struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	// RoleRef can only reference a ClusterRole in the global namespace.
-	// If the RoleRef cannot be resolved, the Authorizer must return an error.
-	RoleRef v1.RoleRef `json:"roleRef" protobuf:"bytes,3,opt,name=roleRef"`
 }
 
 //+kubebuilder:object:root=true
