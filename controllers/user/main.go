69558db49346bab6ccc6072e23b68a60a5b11ede
中弈 <fhtjob@hotmail.com>
2022-08-19 17:58:08 +0800

    add account and charge CRD (#1580)
    Signed-off-by: fanux <fanux@sealos.io>
    
    Signed-off-by: fanux <fanux@sealos.io>

--- controllers/user/main.go
+++ controllers/user/main.go
@@ -108,6 +108,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "UserGroupBinding")
 		os.Exit(1)
 	}
+	if err = (&controllers.AccountReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Account")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
