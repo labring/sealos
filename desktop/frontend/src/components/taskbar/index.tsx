534f4bf36c4a64405c201532d9b143d027a60921
LeezQ <lizhenq2009@gmail.com>
2022-08-29 16:41:55 +0800

    feat: tabbar style
    Signed-off-by: LeezQ <lizhenq2009@gmail.com>

--- desktop/frontend/src/components/taskbar/index.tsx
+++ desktop/frontend/src/components/taskbar/index.tsx
@@ -18,57 +18,61 @@ const Taskbar = () => {
 
   return (
     <div className={styles.taskbar}>
-      <div className={styles.taskcont}>
-        <div className={styles.tasksCont} data-menu="task" data-side={'center'}>
-          <div className={styles.tsbar}>
-            <Icon
-              className={clsx(styles.tsIcon, styles.widget)}
-              src="widget"
-              width={24}
-              click="WIDGTOGG"
-            />
+      <div className="flex items-center">
+        <div className={clsx(styles.tsIcon)}>
+          <Icon src="widget" width={24} />
+        </div>
+      </div>
+      <div className={styles.tsbar}>
+        <div className={clsx(styles.tsIcon)}>
+          <Icon src="home" width={24} />
+        </div>
+        <div className={clsx(styles.tsIcon)}>
+          <Icon src="github" width={24} />
+        </div>
 
-            <Icon className={styles.tsIcon} src="home" width={24} click="STARTOGG" />
+        <div className={clsx(styles.tsIcon)}>
+          <Icon src="search" width={24} />
+        </div>
+
+        <div className={clsx(styles.tsIcon)}>
+          <Icon src="settings" width={24} />
+        </div>
+      </div>
+
+      <div className={styles.taskright}>
+        <div>
+          <Icon width={10} />
+        </div>
+        <div
+          className="prtclk handcr my-1 px-1 hvlight items-center flex rounded"
+          onClick={clickDispatch}
+          data-action="PANETOGG"
+        >
+          <Icon className="mr-1" src="wifi" width={16} />
+          <Battery />
+        </div>
+
+        <div
+          className={clsx(styles.taskDate, 'm-1 handcr prtclk rounded hvlight')}
+          onClick={clickDispatch}
+          data-action="CALNTOGG"
+        >
+          <div>
+            {new Date().toLocaleTimeString('en-US', {
+              hour: 'numeric',
+              minute: 'numeric'
+            })}
+          </div>
+          <div>
+            {new Date().toLocaleDateString('en-US', {
+              year: '2-digit',
+              month: '2-digit',
+              day: 'numeric'
+            })}
           </div>
         </div>
-        <div className={styles.taskright}>
-          <div
-            className="px-2 prtclk handcr hvlight flex"
-            onClick={clickDispatch}
-            data-action="BANDTOGG"
-          >
-            <Icon width={10} />
-          </div>
-          <div
-            className="prtclk handcr my-1 px-1 hvlight items-center flex rounded"
-            onClick={clickDispatch}
-            data-action="PANETOGG"
-          >
-            <Icon className="mr-1" src="wifi" width={16} />
-            <Battery />
-          </div>
-
-          <div
-            className={clsx(styles.taskDate, 'm-1 handcr prtclk rounded hvlight')}
-            onClick={clickDispatch}
-            data-action="CALNTOGG"
-          >
-            <div>
-              {new Date().toLocaleTimeString('en-US', {
-                hour: 'numeric',
-                minute: 'numeric'
-              })}
-            </div>
-            <div>
-              {new Date().toLocaleDateString('en-US', {
-                year: '2-digit',
-                month: '2-digit',
-                day: 'numeric'
-              })}
-            </div>
-          </div>
-          <Icon className={clsx(styles.graybd, 'my-4')} width={6} click="SHOWDSK" />
-        </div>
+        <Icon className={clsx(styles.graybd, 'my-4')} width={6} />
       </div>
     </div>
   );
