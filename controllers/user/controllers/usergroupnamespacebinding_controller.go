ddc6a97c5f462fcac0cf9de8972ead232744b67d
cuisongliu <cuisongliu@qq.com>
2022-09-06 22:31:42 +0800

    feature(main):  add sync user role
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- controllers/user/controllers/usergroupnamespacebinding_controller.go
+++ controllers/user/controllers/usergroupnamespacebinding_controller.go
@@ -19,6 +19,10 @@ package controllers
 import (
 	"context"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"golang.org/x/sync/errgroup"
+
 	"github.com/labring/sealos/controllers/user/controllers/helper"
 	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
@@ -79,6 +83,15 @@ func (r *UserGroupNamespaceBindingController) syncNamespace(ctx context.Context,
 		var err error
 		ns := &v1.Namespace{}
 		ns.Name = ugBinding.Subject.Name
+		if err = r.Get(ctx, client.ObjectKeyFromObject(ns), ns); err != nil {
+			if !apierrors.IsNotFound(err) {
+				return err
+			}
+		}
+		if !ns.CreationTimestamp.IsZero() {
+			r.Logger.V(1).Info("namespace UserGroupBinding namespace is created", "OperationResult", change, "namespace", ns.Name)
+			return nil
+		}
 		if change, err = controllerutil.CreateOrUpdate(ctx, r.Client, ns, func() error {
 			if err = controllerutil.SetControllerReference(ugBinding, ns, r.Scheme); err != nil {
 				return err
@@ -96,6 +109,39 @@ func (r *UserGroupNamespaceBindingController) syncNamespace(ctx context.Context,
 	}
 }
 
+func (r *UserGroupNamespaceBindingController) mergeFromUser(ctx context.Context, ugBinding *userv1.UserGroupBinding) (users []userv1.UserGroupBinding) {
+	ugUserBindingList := make([]userv1.UserGroupBindingList, 0)
+	var ugUserBinding userv1.UserGroupBindingList
+	if err := r.List(ctx, &ugUserBinding, client.MatchingFields{"userGroupRef": ugBinding.UserGroupRef}); err != nil {
+		r.Logger.Error(err, "list ugUserBinding error from cache", "userGroupRef", ugBinding.UserGroupRef)
+	}
+	ugUserBindingList = append(ugUserBindingList, ugUserBinding)
+	if err := r.List(ctx, &ugUserBinding, client.MatchingFields{"subject.kind": "User"}); err != nil {
+		r.Logger.Error(err, "list ugUserBinding error from cache", "subject.kind", "User")
+	}
+	ugUserBindingList = append(ugUserBindingList, ugUserBinding)
+
+	var newAny helper.Any
+
+	for i, item := range ugUserBindingList {
+		newIAny := helper.NewAny(nil)
+		for _, j := range item.Items {
+			newIAny = newIAny.InsertValue(j.Name, j)
+		}
+		if i == 0 {
+			newAny = newIAny
+		}
+		newAny = newAny.Intersection(newIAny)
+	}
+	values := newAny.ListValue()
+	for _, value := range values {
+		if u, ok := value.(userv1.UserGroupBinding); ok {
+			users = append(users, u)
+		}
+	}
+	return
+}
+
 func (r *UserGroupNamespaceBindingController) syncRoleBinding(ctx context.Context, ugBinding *userv1.UserGroupBinding) {
 	roleBindingConditionType := userv1.ConditionType("UGNamespaceBindingSyncReady")
 	condition := &userv1.Condition{
@@ -106,40 +152,49 @@ func (r *UserGroupNamespaceBindingController) syncRoleBinding(ctx context.Contex
 		Reason:             string(userv1.Ready),
 		Message:            "sync ug namespace binding successfully",
 	}
+	users := r.mergeFromUser(ctx, ugBinding)
 	defer r.saveCondition(ugBinding, condition)
 	userName := ugBinding.Annotations[userAnnotationOwnerKey]
-	if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		var change controllerutil.OperationResult
-		var err error
-		roleBinding := &rbacv1.RoleBinding{}
-		roleBinding.Name = ugBinding.UserGroupRef + "-role"
-		roleBinding.Namespace = ugBinding.Subject.Name
 
-		if change, err = controllerutil.CreateOrUpdate(ctx, r.Client, roleBinding, func() error {
-			if err = controllerutil.SetControllerReference(ugBinding, roleBinding, r.Scheme); err != nil {
-				return err
-			}
-			roleBinding.Annotations = map[string]string{userAnnotationOwnerKey: userName}
-			roleBinding.Subjects = []rbacv1.Subject{
-				{
-					Kind:     ugBinding.Subject.Kind,
-					Name:     ugBinding.Subject.Name,
-					APIGroup: rbacv1.SchemeGroupVersion.Group,
-				},
-			}
-			roleBinding.RoleRef = rbacv1.RoleRef{
-				APIGroup: rbacv1.SchemeGroupVersion.Group,
-				Kind:     "ClusterRole",
-				Name:     roleNamespaceByUser,
-			}
+	eg, _ := errgroup.WithContext(context.Background())
+	for _, v := range users {
+		user := v
+		eg.Go(func() error {
+			return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+				var change controllerutil.OperationResult
+				var err error
+				roleBinding := &rbacv1.RoleBinding{}
+				roleBinding.Name = user.Subject.Name + "-role"
+				roleBinding.Namespace = ugBinding.Subject.Name
 
-			return nil
-		}); err != nil {
-			return errors.Wrap(err, "unable to create namespace UserGroupBinding roleBinding")
-		}
-		r.Logger.V(1).Info("create or update namespace UserGroupBinding roleBinding", "OperationResult", change)
-		return nil
-	}); err != nil {
+				if change, err = controllerutil.CreateOrUpdate(ctx, r.Client, roleBinding, func() error {
+					if err = controllerutil.SetControllerReference(ugBinding, roleBinding, r.Scheme); err != nil {
+						return err
+					}
+					roleBinding.Annotations = map[string]string{userAnnotationOwnerKey: userName}
+					roleBinding.Subjects = []rbacv1.Subject{
+						{
+							Kind:     "User",
+							Name:     user.Subject.Name,
+							APIGroup: rbacv1.SchemeGroupVersion.Group,
+						},
+					}
+					roleBinding.RoleRef = rbacv1.RoleRef{
+						APIGroup: rbacv1.SchemeGroupVersion.Group,
+						Kind:     "ClusterRole",
+						Name:     roleNamespaceByUser,
+					}
+
+					return nil
+				}); err != nil {
+					return errors.Wrap(err, "unable to create namespace UserGroupBinding roleBinding")
+				}
+				r.Logger.V(1).Info("create or update namespace UserGroupBinding roleBinding", "OperationResult", change, "user", user.Subject.Name)
+				return nil
+			})
+		})
+	}
+	if err := eg.Wait(); err != nil {
 		helper.SetConditionError(condition, "SyncUGUserBindingError", err)
 		r.Recorder.Eventf(ugBinding, v1.EventTypeWarning, "syncUGUserBinding", "Sync UGUserBinding roleBinding %s is error: %v", ugBinding.Name, err)
 	}
--- controllers/user/main.go
+++ controllers/user/main.go
@@ -90,10 +90,6 @@ func main() {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
-
-	//TODO ADD Cache
-	//mgr.GetCache().IndexField()
-
 	if err = (&controllers.UserReconciler{}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "User")
 		os.Exit(1)
