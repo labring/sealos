97157a962ff87745fbc23703a048e66ab3e766c8
LeezQ <lizhenq2009@gmail.com>
2022-09-29 23:25:41 +0800

    feat: add sealos sdk demo

--- desktop/frontend/src/pages/demo.tsx
+++ desktop/frontend/src/pages/demo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ClientSDK from 'sealos-desktop-sdk';
 const sdk = new ClientSDK({
   appKey: 'this is your appKey',
@@ -6,16 +6,24 @@ const sdk = new ClientSDK({
 });
 
 export default function Demo() {
+  const [userInfo, setUserInfo] = useState<any>({});
+  const [connected, setConnected] = useState<boolean>(false);
   useEffect(() => {
     (async () => {
-      const isConnect = await sdk.connect();
-      console.log('is connect: ', isConnect);
+      const connectRes = await sdk.connect();
+      setConnected(connectRes.connect);
       const userInfo = await sdk.getUserInfo();
       console.log('user info: ', userInfo);
+      setUserInfo(userInfo);
     })();
 
     return () => {};
   }, []);
 
-  return <div>Demo</div>;
+  return (
+    <div>
+      <div>connected: {connected.toString()}</div>
+      <div>user info: {JSON.stringify(userInfo)}</div>
+    </div>
+  );
 }
