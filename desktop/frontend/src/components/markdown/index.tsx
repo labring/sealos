99af3443ad44e86da921cf9f8b9d7f4fcae954c0
zhu jing yang <3161362058@qq.com>
2022-11-01 19:40:19 +0800

    add sealos cloud provider fontend
    Signed-off-by: zhu jing yang <3161362058@qq.com>

--- desktop/frontend/src/components/markdown/index.tsx
+++ desktop/frontend/src/components/markdown/index.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import rehypeHighlight from 'rehype-highlight'
-
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import styles from './index.module.scss';
 
 const MarkDown = ({ text }: { text: string }) => {
-	return <ReactMarkdown
-		rehypePlugins={[rehypeHighlight]}
-	>{text ?? ''}</ReactMarkdown>
-}
+  const copyContent = () => {
+    navigator.clipboard.writeText(text);
+  };
+  return (
+    <div className={styles.copyMark}>
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                showLineNumbers={true}
+                // style={darkMode ? them.dark : them.light}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          }
+        }}
+      >
+        {text ?? ''}
+      </ReactMarkdown>
+      <div className={styles.copyMarkBtn} onClick={() => copyContent()}>
+        复制
+      </div>
+    </div>
+  );
+};
 
 export default MarkDown;
--- desktop/frontend/src/mock/infra.ts
+++ desktop/frontend/src/mock/infra.ts
@@ -1,17 +1,17 @@
 import { CRDMeta } from '../services/backend/kubernetes';
 
 const infraMeta: CRDMeta = {
-    group: 'infra.sealos.io',
-    version: 'v1',
-    namespace: 'infra-system',
-    plural: 'infras'
+  group: 'infra.sealos.io',
+  version: 'v1',
+  namespace: 'infra-system',
+  plural: 'infras'
 };
 
 const infraCRDTemplate: string = `
 apiVersion: infra.sealos.io/v1
 kind: Infra
 metadata:
-  name: {{ .infra_name }}
+  name: {{ .infraName }}
   namespace: {{ .namespace }}
 spec:
     hosts: 
@@ -19,27 +19,63 @@ spec:
       count: {{ .masterCount }} 
       flavor: {{ .masterType }}
       image: "ami-08bb4e3ce08ca7ddb"
+      disks:
+      - capacity: {{.masterDisk}}
+        type: "gp3"
+        name: "/dev/sda2"
     - roles: [ node ] # required
       count: {{ .nodeCount }}
       flavor: {{ .nodeType }}
       image: "ami-08bb4e3ce08ca7ddb"
+      disks:
+      - capacity: {{.nodeDisk}}
+        type: "gp2"
+        name: "/dev/sda2"
 `;
 
 const clusterMeta: CRDMeta = {
-    group: 'cluster.sealos.io',
-    version: 'v1',
-    namespace: 'cluster-system',
-    plural: 'clusters'
+  group: 'cluster.sealos.io',
+  version: 'v1',
+  namespace: 'cluster-system',
+  plural: 'clusters'
 };
 
 const clusterCRDTemplate: string = `
 apiVersion: cluster.sealos.io/v1
 kind: Cluster
 metadata:
-  name: {{ .cluster_name }}
+  name: {{ .clusterName }}
   namespace: {{ .namespace }}
 spec:
-  infra: {{ .infra_name}}
+  infra: {{ .infraName}}
   images: [ {{ .image1}},{{ .image2}} ]
 `;
-export { infraMeta, infraCRDTemplate,clusterCRDTemplate };
+const InfraMarkDownContent = ` 
+apiVersion: infra.sealos.io/v1
+kind: Infra
+metadata:
+  name: {{.infraName}}
+spec:
+  hosts:
+  - roles: [master] 
+    count: {{.masterCount}}
+    flavor: {{.masterType}}
+    disks:
+    - capacity: {{.masterDisk}}
+  - roles: [ node ] 
+    count: {{.nodeCount}} 
+    flavor: {{.nodeType}}
+    disks:
+    - capacity: {{.nodeDisk}}
+---
+apiVersion: cluster.sealos.io/v1
+kind: Cluster
+metadata:
+  name: {{.clusterName}}
+spec:
+  infra: {{.infraName}}
+  images:
+  - labring/kubernetes:v1.24.0
+  - labring/calico:v3.22.1
+`;
+export { infraMeta, infraCRDTemplate, clusterCRDTemplate, InfraMarkDownContent };
