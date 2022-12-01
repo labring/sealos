8f9109c7f3101bf530d2a1ce54fe8828daca9a20
lingdie <lingdie.yy@outlook.com>
2022-12-01 16:36:42 +0800

    fix

--- controllers/imagehub/main.go
+++ controllers/imagehub/main.go
@@ -117,6 +117,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DataPack")
 		os.Exit(1)
 	}
+	if err = (&controllers.OrgclusterrolebindingReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Orgclusterrolebinding")
+		os.Exit(1)
+	}
+
 	if err = (&imagehubv1.Image{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "Image")
 		os.Exit(1)
@@ -129,16 +137,7 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "Organization")
 		os.Exit(1)
 	}
-
-	if err = (&controllers.OrgclusterrolebindingReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Orgclusterrolebinding")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
-
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
