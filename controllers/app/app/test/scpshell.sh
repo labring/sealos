5f9273d871b56201827e36ada27932364f61a69f
cdjianghan <328804165@qq.com>
2022-12-05 14:32:31 +0800

    new pr remove pkg/image I need to move my push logic

--- controllers/app/app/test/scpshell.sh
+++
@@ -1,3 +0,0 @@
-tar -cvf /Users/jianghan/GolandProjects/abc.tar /Users/jianghan/GolandProjects/sealos/
-scp -r /Users/jianghan/GolandProjects/sealos/ sealos@ofc6.lafyun.com:/home/sealos/jianghanMount
-rm -rf /Users/jianghan/GolandProjects/abc.tar
--- pkg/image/app-image/imageCrdBuilder.go
+++ pkg/image/app-image/imageCrdBuilder.go
@@ -9,13 +9,17 @@ import (
 	"github.com/labring/sealos/pkg/utils/file"
 	"github.com/labring/sealos/pkg/utils/logger"
 	"github.com/pkg/errors"
-	"gopkg.in/yaml.v3"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/client-go/discovery"
+	memory "k8s.io/client-go/discovery/cached"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"math/rand"
@@ -26,10 +30,10 @@ import (
 )
 
 const (
-	AppPath          = "/app"
-	ActionPath       = "/app/actions"
-	READMEpath       = "/README.md"
-	ConfigFileName   = "appconfig.yaml"
+	AppPath          = "app"
+	ActionPath       = "actions"
+	READMEpath       = "README.md"
+	ConfigFileName   = "config.yaml"
 	TemplateFileName = "template.yaml"
 	CMDFileName      = "CMD"
 	DefaultNamespace = "default"
@@ -38,7 +42,7 @@ const (
 type ImageCRDBuilder struct {
 	imagename   string
 	clustername string
-	content     *Content
+	Content     *Content
 }
 
 type Content struct {
@@ -49,8 +53,23 @@ type Content struct {
 }
 
 func NewImageCRDBuilder(imageName string) *ImageCRDBuilder {
-	return &ImageCRDBuilder{imageName, "", nil}
+	return &ImageCRDBuilder{imageName, "", &Content{
+		README:         "",
+		ActionTemplate: make(map[string]string),
+		ActionCMD:      make(map[string]string),
+		AppConfig:      nil,
+	}}
 }
+
+//Action path example
+//.
+//├── config.yaml
+//├── install
+//│		├── cmd
+//│	    └── template.yaml
+//└── uninstall
+//      ├── cmd
+//      └── template.yaml
 
 func (icb *ImageCRDBuilder) Run() error {
 	MountPoint, err := icb.CreateContainer()
@@ -93,93 +112,75 @@ func (icb *ImageCRDBuilder) CreateContainer() (string, error) {
 	return manifest.MountPoint, nil
 }
 func (icb *ImageCRDBuilder) GetAppContent(MountPoint string) error {
-	icb.content = &Content{}
-	if !file.IsExist(MountPoint + AppPath) {
+	if !file.IsExist(filepath.Join(MountPoint, AppPath)) {
 		return fmt.Errorf("app dir can't find")
 	}
-
-	if file.IsExist(MountPoint + READMEpath) {
-		c, err := FileReadUtil(MountPoint + READMEpath)
+	if file.IsExist(filepath.Join(MountPoint, READMEpath)) {
+		c, err := FileReadUtil(filepath.Join(MountPoint, READMEpath))
 		if err != nil {
 			return err
 		}
-		icb.content.README = string(c)
+		icb.Content.README = string(c)
 	}
 
-	if !file.IsExist(MountPoint + ActionPath) {
-		file, err := os.Open(MountPoint)
-		if err != nil {
-			return err
-		}
-		fileinfo, err := file.Readdir(-1)
-		for _, f := range fileinfo {
-			if f.Name() == ConfigFileName {
-				var c []byte
-				c, err = FileReadUtil(MountPoint + READMEpath)
-				if err != nil {
-					return err
-				}
-				if err = icb.ConfigParse(c); err != nil {
-					return err
-				}
-				continue
-			}
-			if f.IsDir() {
-				ff, err := os.Open(MountPoint + ActionPath + "/" + f.Name())
-				if err != nil {
-					return err
-				}
-				actionfiles, err := ff.Readdir(-1)
-				for _, actionfile := range actionfiles {
-					if actionfile.Name() == TemplateFileName {
-						var c []byte
-						c, err = FileReadUtil(MountPoint + ActionPath + "/" + actionfile.Name())
-						if err != nil {
-							return err
-						}
-						icb.content.ActionTemplate[f.Name()] = string(c)
-					}
-					if actionfile.Name() == CMDFileName {
-						var c []byte
-						c, err = FileReadUtil(MountPoint + ActionPath + "/" + actionfile.Name())
-						if err != nil {
-							return err
-						}
-						icb.content.ActionCMD[f.Name()] = string(c)
-					}
-				}
-			}
-		}
-	} else {
-		return fmt.Errorf("actions dir cant'find")
+	file, err := os.Open(filepath.Join(MountPoint, AppPath))
+	if err != nil {
+		return err
 	}
+	fileinfo, err := file.Readdir(-1)
+	for _, f := range fileinfo {
+		if f.Name() == ConfigFileName {
+			var c []byte
+			c, err = FileReadUtil(filepath.Join(MountPoint, AppPath, ConfigFileName))
+			if err != nil {
+				return err
+			}
+			if err = icb.ConfigParse(c); err != nil {
+				return err
+			}
+			continue
+		}
+		if f.IsDir() {
+			ff, err := os.Open(filepath.Join(MountPoint, AppPath, f.Name()))
+			if err != nil {
+				return err
+			}
+			actionfiles, err := ff.Readdir(-1)
+			for _, actionfile := range actionfiles {
+				if actionfile.Name() == TemplateFileName {
+					var c []byte
+					c, err = FileReadUtil(filepath.Join(MountPoint, AppPath, f.Name(), actionfile.Name()))
+					if err != nil {
+						return err
+					}
+					icb.Content.ActionTemplate[f.Name()] = string(c)
+				}
+				if actionfile.Name() == CMDFileName {
+					var c []byte
+					c, err = FileReadUtil(filepath.Join(MountPoint, AppPath, f.Name(), actionfile.Name()))
+					if err != nil {
+						return err
+					}
+					icb.Content.ActionCMD[f.Name()] = string(c)
+				}
+			}
+		}
+	}
+	err = icb.TemplateCmdParse()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (icb *ImageCRDBuilder) AppContentApply() error {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		return err
-	}
-	dynamicClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-	gvr := schema.GroupVersionResource{Version: "v1", Resource: "Images"}
-	dyclient := dynamicClient.Resource(gvr).Namespace("default")
 	utd := unstructured.Unstructured{}
-	utd.Object, err = runtime.DefaultUnstructuredConverter.ToUnstructured(icb.content.AppConfig)
-	if err != nil {
-		return err
-	}
+	utd.Object, _ = runtime.DefaultUnstructuredConverter.ToUnstructured(icb.Content.AppConfig)
+	gvk := icb.Content.AppConfig.GroupVersionKind()
+	dyclient, _ := GetGVRdyClient(&gvk, "default")
 	if _, err := dyclient.Create(context.TODO(), &utd, metav1.CreateOptions{}); err != nil {
+		fmt.Println("dyclient.Create err")
 		if !apierrors.IsAlreadyExists(err) {
 			return errors.Wrap(err, "unable to create secret")
 		}
@@ -224,15 +225,60 @@ func FileReadUtil(filePath string) ([]byte, error) {
 
 func (icb *ImageCRDBuilder) ConfigParse(c []byte) error {
 	config := &imagev1.Image{}
-	if err := yaml.Unmarshal(c, config); err != nil {
-		return err
+	_, _, err := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme).Decode(c, nil, config)
+	if err != nil {
+		panic(fmt.Errorf("failed to get GVK: %v", err))
 	}
-	icb.content.AppConfig = config
-	for k, v := range icb.content.ActionTemplate {
-		config.Spec.DetailInfo.AppActions.Actions[imagev1.ActionName(k)] = imagev1.Template(string(v))
+
+	icb.Content.AppConfig = config
+	return nil
+}
+
+func (icb *ImageCRDBuilder) TemplateCmdParse() error {
+	for k, v := range icb.Content.ActionTemplate {
+		if icb.Content.AppConfig.Spec.DetailInfo.AppActions.Actions == nil {
+			icb.Content.AppConfig.Spec.DetailInfo.AppActions.Actions = make(map[imagev1.ActionName]imagev1.Template)
+		}
+		icb.Content.AppConfig.Spec.DetailInfo.AppActions.Actions[imagev1.ActionName(k)] = imagev1.Template(v)
 	}
-	for k, v := range icb.content.ActionCMD {
-		config.Spec.DetailInfo.AppActions.CMD[imagev1.ActionName(k)] = imagev1.CMD(string(v))
+	for k, v := range icb.Content.ActionCMD {
+		if icb.Content.AppConfig.Spec.DetailInfo.AppActions.CMD == nil {
+			icb.Content.AppConfig.Spec.DetailInfo.AppActions.CMD = make(map[imagev1.ActionName]imagev1.CMD)
+		}
+		icb.Content.AppConfig.Spec.DetailInfo.AppActions.CMD[imagev1.ActionName(k)] = imagev1.CMD(v)
 	}
 	return nil
+}
+
+func GetGVRdyClient(gvk *schema.GroupVersionKind, namespace string) (dr dynamic.ResourceInterface, err error) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return
+	}
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(config)
+	if err != nil {
+		return
+	}
+	mapperGVRGVK := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(discoveryClient))
+	resourceMapper, err := mapperGVRGVK.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return
+	}
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return
+	}
+	if resourceMapper.Scope.Name() == meta.RESTScopeNameNamespace {
+		dr = dynamicClient.Resource(resourceMapper.Resource).Namespace(namespace)
+	} else {
+		dr = dynamicClient.Resource(resourceMapper.Resource)
+	}
+	return
 }
