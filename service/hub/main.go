243318b42458ba788929ad6fc860e04657be426e
lingdie <lingdie.yy@outlook.com>
2022-12-06 20:31:58 +0800

    delete useless func.

--- service/hub/main.go
+++ service/hub/main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"math/rand"
 	"net"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/cesanta/glog"
-	fsnotify "gopkg.in/fsnotify.v1"
-
 	"github.com/labring/service/hub/server"
 )
 
@@ -49,66 +43,6 @@ func ServeOnce(c *server.Config, cf string) (*server.AuthServer, *http.Server) {
 }
 
 func (rs *RestartableServer) Serve(c *server.Config) {
-	rs.authServer, rs.hs = ServeOnce(c, rs.configFile)
-	rs.WatchConfig()
-}
-
-func (rs *RestartableServer) WatchConfig() {
-	w, err := fsnotify.NewWatcher()
-	if err != nil {
-		glog.Fatalf("Failed to create watcher: %s", err)
-	}
-	defer w.Close()
-
-	stopSignals := make(chan os.Signal, 1)
-	signal.Notify(stopSignals, syscall.SIGTERM, syscall.SIGINT)
-
-	err = w.Add(rs.configFile)
-	watching, needRestart := err == nil, false
-	for {
-		select {
-		case <-time.After(1 * time.Second):
-			if !watching {
-				err = w.Add(rs.configFile)
-				if err != nil {
-					glog.Errorf("Failed to set up config watcher: %s", err)
-				} else {
-					watching, needRestart = true, true
-				}
-			} else if needRestart {
-				rs.MaybeRestart()
-				needRestart = false
-			}
-		case ev := <-w.Events:
-			if ev.Op == fsnotify.Remove {
-				glog.Warningf("Config file disappeared, serving continues")
-				_ = w.Remove(rs.configFile)
-				watching, needRestart = false, false
-			} else if ev.Op == fsnotify.Write {
-				needRestart = true
-			}
-		case s := <-stopSignals:
-			signal.Stop(stopSignals)
-			glog.Infof("Signal: %s", s)
-			if err := rs.hs.Shutdown(context.Background()); err != nil {
-				glog.Errorf("HTTP server Shutdown: %v", err)
-			}
-			rs.authServer.Stop()
-			glog.Exitf("Exiting")
-		}
-	}
-}
-
-func (rs *RestartableServer) MaybeRestart() {
-	glog.Infof("Validating new config")
-	c, err := server.LoadConfig(rs.configFile)
-	if err != nil {
-		glog.Errorf("Failed to reload config (server not restarted): %s", err)
-		return
-	}
-	glog.Infof("Config ok, restarting server")
-	rs.hs.Close()
-	rs.authServer.Stop()
 	rs.authServer, rs.hs = ServeOnce(c, rs.configFile)
 }
 
