e28da5da29a24204b83bc247129b662bd24c3f83
gitccl <chenchuanle6@gmail.com>
2022-12-13 21:45:11 +0800

    feat: deploy terminal in user's namespace and support apisix ingress

--- controllers/terminal/main.go
+++ controllers/terminal/main.go
@@ -31,6 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	apisix "github.com/apache/apisix-ingress-controller/pkg/kube/apisix/apis/config/v2beta3"
+
 	terminalv1 "github.com/labring/sealos/controllers/terminal/api/v1"
 	"github.com/labring/sealos/controllers/terminal/controllers"
 	//+kubebuilder:scaffold:imports
@@ -44,6 +46,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(terminalv1.AddToScheme(scheme))
+	utilruntime.Must(apisix.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
