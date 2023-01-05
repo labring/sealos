803c86add8b6fc209d6ce4f4dc732234c21e5564
mond77 <1208119827@qq.com>
2023-01-05 12:57:28 +0800

    same result if upgrade multiple times or retry after failure
    modify license

--- pkg/runtime/upgrade.go
+++ pkg/runtime/upgrade.go
@@ -1,30 +1,28 @@
-/*
-Copyright 2022 cuisongliu@qq.com.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright © 2022 sealos.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package runtime
 
 import (
 	"fmt"
-	"time"
 )
 
 var (
 	upgradeApplyCmd = "kubeadm upgrade apply %s"
 	upradeNodeCmd   = "kubeadm upgrade node"
-	drainNodeCmd    = "kubectl drain %s --ignore-daemonsets"
+	//drainNodeCmd    = "kubectl drain %s --ignore-daemonsets"
+	cordonNodeCmd   = "kubectl cordon %s"
 	uncordonNodeCmd = "kubectl uncordon %s"
 	daemonReload    = "systemctl daemon-reload"
 	restartKubelet  = "systemctl restart kubelet"
@@ -45,27 +43,36 @@ func (k *KubeadmRuntime) upgradeCluster(version string) error {
 		}
 		upgradeNodes = append(upgradeNodes, ip)
 	}
-	return k.upgradeNode(upgradeNodes)
+	return k.upgradeOtherNodes(upgradeNodes)
 }
 
 func (k *KubeadmRuntime) upgradeMaster0(version string) error {
 	master0ip := k.getMaster0IP()
+	master0Name, err := k.getRemoteInterface().Hostname(master0ip)
+	if err != nil {
+		return err
+	}
 	//install kubeadm:{version} at master0
-	err := k.sshCmdAsync(master0ip,
+	err = k.sshCmdAsync(master0ip,
 		fmt.Sprintf("cp -rf %s/kubeadm /usr/bin", kubeBinaryPath),
 		//execute  kubeadm upgrade apply {version} at master0
 		fmt.Sprintf(upgradeApplyCmd, version),
+		// kubectl drain <node-to-drain> --ignore-daemonsets
+		// fmt.Sprintf(drainNodeCmd, nodename),
+		//kubectl cordon <node-to-cordon>
+		fmt.Sprintf(cordonNodeCmd, master0Name),
 		//install kubelet:{version},kubectl{version} at master0
 		fmt.Sprintf("cp -rf %s/kubectl /usr/bin", kubeBinaryPath),
 		fmt.Sprintf("cp -rf %s/kubelet /usr/bin", kubeBinaryPath),
 		//reload kubelet daemon
 		daemonReload,
 		restartKubelet,
+		fmt.Sprintf(uncordonNodeCmd, master0Name),
 	)
 	return err
 }
 
-func (k *KubeadmRuntime) upgradeNode(ips []string) error {
+func (k *KubeadmRuntime) upgradeOtherNodes(ips []string) error {
 	for _, ip := range ips {
 		nodename, err := k.getRemoteInterface().Hostname(ip)
 		if err != nil {
@@ -76,30 +83,21 @@ func (k *KubeadmRuntime) upgradeNode(ips []string) error {
 			fmt.Sprintf("cp -rf %s/kubeadm /usr/bin", kubeBinaryPath),
 			//upgrade other control-plane and nodes
 			upradeNodeCmd,
-			//kubectl drain <node-to-drain> --ignore-daemonsets
-			fmt.Sprintf(drainNodeCmd, nodename),
+			// kubectl drain <node-to-drain> --ignore-daemonsets
+			// fmt.Sprintf(drainNodeCmd, nodename),
+			//kubectl cordon <node-to-cordon>
+			fmt.Sprintf(cordonNodeCmd, nodename),
 			//install kubelet:{version},kubectl{version} at the node
 			fmt.Sprintf("cp -rf %s/kubectl /usr/bin", kubeBinaryPath),
 			fmt.Sprintf("cp -rf %s/kubelet /usr/bin", kubeBinaryPath),
 			//reload kubelet daemon
 			daemonReload,
 			restartKubelet,
+			fmt.Sprintf(uncordonNodeCmd, nodename),
 		)
 		if err != nil {
 			return err
 		}
-		//kubectl uncordon <node-to-uncordon>
-		err = k.sshCmdAsync(ip, fmt.Sprintf(uncordonNodeCmd, nodename))
-		now := time.Now()
-		//restart api-server takes a few seconds
-		for err != nil {
-			if time.Now().After(now.Add(2 * time.Minute)) {
-				return err
-			}
-			time.Sleep(5 * time.Second)
-			err = k.sshCmdAsync(ip, fmt.Sprintf(uncordonNodeCmd, nodename))
-		}
 	}
-
 	return nil
 }
