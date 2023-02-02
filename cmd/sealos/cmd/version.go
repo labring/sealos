7865092e1f8b331c722fc204721701963e1abd3c
cdjianghan <328804165@qq.com>
2023-02-02 14:55:50 +0800

    add kubernetes version cri version output in sealos verison cmd

--- cmd/sealos/cmd/version.go
+++ cmd/sealos/cmd/version.go
@@ -18,7 +18,9 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/labring/sealos/pkg/clusterfile"
 	"github.com/labring/sealos/pkg/constants"
+	"sigs.k8s.io/yaml"
 
 	"github.com/labring/sealos/pkg/version"
 
@@ -26,6 +28,7 @@ import (
 )
 
 var shortPrint bool
+var output string
 
 func newVersionCmd() *cobra.Command {
 	var versionCmd = &cobra.Command{
@@ -34,19 +37,18 @@ func newVersionCmd() *cobra.Command {
 		Args:    cobra.NoArgs,
 		Example: `sealos version`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			marshalled, err := json.Marshal(version.Get())
-			if err != nil {
-				return err
-			}
 			if shortPrint {
 				fmt.Println(version.Get().String())
-			} else {
-				fmt.Println(string(marshalled))
+				return nil
+			}
+			if err := PrintInfo(); err != nil {
+				return err
 			}
 			return nil
 		},
 	}
 	versionCmd.Flags().BoolVar(&shortPrint, "short", false, "if true, print just the version number.")
+	versionCmd.Flags().StringVarP(&output, "output", "o", "yaml", "One of 'yaml' or 'json'")
 	return versionCmd
 }
 
@@ -56,4 +58,30 @@ func init() {
 
 func getContact() string {
 	return fmt.Sprintf(constants.Contact, version.Get().String())
+}
+
+func PrintInfo() error {
+	var (
+		marshalled []byte
+	)
+	Output := &version.Output{}
+	Output.SealosVersion = version.Get()
+	cluster, err := clusterfile.GetClusterFromName(clusterName)
+	Output.KubernetesVersion = version.GetKubernetesVersion(cluster)
+	Output.CriRuntimeVersion = version.GetCriRuntimeVersion()
+
+	if output == "json" {
+		marshalled, err = json.Marshal(&Output)
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(marshalled))
+		return nil
+	}
+	marshalled, err = yaml.Marshal(&Output)
+	if err != nil {
+		return err
+	}
+	fmt.Printf(string(marshalled))
+	return nil
 }
