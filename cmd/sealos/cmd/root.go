3abea498495addba47a9dfe17258ae132c4e1d36
fengxsong <fengxsong@outlook.com>
2023-02-08 01:27:41 +0000

    fix: reduce error output
    Signed-off-by: fengxsong <fengxsong@outlook.com>

--- cmd/sealos/cmd/root.go
+++ cmd/sealos/cmd/root.go
@@ -45,7 +45,9 @@ var rootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		if rootCmd.SilenceErrors {
+			fmt.Println(err)
+		}
 		os.Exit(1)
 	}
 }
