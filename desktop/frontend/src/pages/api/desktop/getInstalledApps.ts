d26ddcbdd247a14de45ca92e81e6a8f00d6d9473
zhu jing yang <3161362058@qq.com>
2023-02-22 20:35:05 +0800

    server error, still return default installedApps
    Signed-off-by: zhu jing yang <3161362058@qq.com>

--- desktop/frontend/src/pages/api/desktop/getInstalledApps.ts
+++ desktop/frontend/src/pages/api/desktop/getInstalledApps.ts
@@ -1,13 +1,7 @@
-import * as k8s from '@kubernetes/client-node';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import {
-  CRDMeta,
-  K8sApi,
-  ListCRD,
-  GetUserDefaultNameSpace
-} from '../../../services/backend/kubernetes';
-import { JsonResp } from '../response';
 import installedApps from 'mock/installedApps';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { GetUserDefaultNameSpace, K8sApi, ListCRD } from '../../../services/backend/kubernetes';
+import { JsonResp } from '../response';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { kubeconfig } = req.body;
@@ -34,9 +28,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
     JsonResp([...installedApps, ...appItems], res);
   } catch (err) {
-    if (err instanceof k8s.HttpError) {
-      console.log(err.body.message);
-    }
-    JsonResp(err, res);
+    JsonResp(installedApps, res);
   }
 }
