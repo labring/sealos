7b58585cb76f02941556f609be3cad15b0e56561
lingdie <lingdie.yy@outlook.com>
2023-02-22 11:27:44 +0800

    delete all action.

--- controllers/app/main.go
+++ controllers/app/main.go
@@ -96,13 +96,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "App")
 		os.Exit(1)
 	}
-	if err = (&controllers.ActionReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Action")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
