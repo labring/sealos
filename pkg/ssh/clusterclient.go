044abe05e8dcb560a4a31fbe8f0034339a695ab1
Mihai Țimbota-Belin <52357594+Mihai22125@users.noreply.github.com>
2023-03-16 03:34:47 +0200

    fix concurrent map access in clusterClient (#2812)
    * bugfix: address concurrent map access in clusterClient
    * remove comment
    
    ---------
    
    Signed-off-by: Mihai Țimbota-Belin <52357594+Mihai22125@users.noreply.github.com>

--- pkg/ssh/clusterclient.go
+++ pkg/ssh/clusterclient.go
@@ -16,16 +16,17 @@ package ssh
 
 import (
 	"strings"
+	"sync"
 
 	"github.com/labring/sealos/pkg/types/v1beta1"
 )
 
-// clusterClient is it necessary to use Mutex lock?
 type clusterClient struct {
 	cluster  *v1beta1.Cluster
 	isStdout bool
 	configs  map[string]*Option
 	cache    map[*Option]Interface
+	mutex    sync.RWMutex
 }
 
 func overSSHConfig(original, override *v1beta1.SSH) {
@@ -52,7 +53,10 @@ func overSSHConfig(original, override *v1beta1.SSH) {
 }
 
 func (cc *clusterClient) getSSHOptionForHost(host string) (*Option, error) {
-	if v, ok := cc.configs[host]; ok {
+	cc.mutex.RLock()
+	v, ok := cc.configs[host]
+	cc.mutex.RUnlock()
+	if ok {
 		return v, nil
 	}
 	sshConfig := cc.cluster.Spec.SSH.DeepCopy()
@@ -65,7 +69,9 @@ func (cc *clusterClient) getSSHOptionForHost(host string) (*Option, error) {
 		}
 	}
 	opt := newOptionFromSSH(sshConfig, cc.isStdout)
+	cc.mutex.Lock()
 	cc.configs[host] = opt
+	cc.mutex.Unlock()
 	return opt, nil
 }
 
@@ -74,14 +80,18 @@ func (cc *clusterClient) getClientForHost(host string) (Interface, error) {
 	if err != nil {
 		return nil, err
 	}
+	cc.mutex.RLock()
 	client := cc.cache[sshConfig]
+	cc.mutex.RUnlock()
 	if client == nil {
 		var err error
 		client, err = newFromOptions(sshConfig)
 		if err != nil {
 			return nil, err
 		}
+		cc.mutex.Lock()
 		cc.cache[sshConfig] = client
+		cc.mutex.Unlock()
 	}
 	return client, nil
 }
