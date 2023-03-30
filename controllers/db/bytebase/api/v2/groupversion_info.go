721c987cf6cf38d93ca428d2ff7c5a8750f04926
Yun Pan <dinoallo@netc.it>
2023-03-30 18:26:46 +0800

    add BytebaseControllerConfig - 2

--- controllers/db/bytebase/api/v2/groupversion_info.go
+++ controllers/db/bytebase/api/v2/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v2 contains API Schema definitions for the db.sealos.io v2 API group
 // +kubebuilder:object:generate=true
-// +groupName=db.sealos.io.db.sealos.io
+// +groupName=db.sealos.io
 package v2
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "db.sealos.io.db.sealos.io", Version: "v2"}
+	GroupVersion = schema.GroupVersion{Group: "db.sealos.io", Version: "v2"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
