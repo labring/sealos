c36a9f3b77d01ee8c911d236351d8d7071a3ac6f
jiahui <bxy4543@gmail.com>
2023-04-14 10:40:10 +0800

    optimize reset node `ip link delete` command.

--- pkg/runtime/node.go
+++ pkg/runtime/node.go
@@ -122,11 +122,12 @@ func (k *KubeadmRuntime) deleteNodes(nodes []string) error {
 }
 
 func (k *KubeadmRuntime) deleteNode(node string) error {
-	//remove node
-	if len(k.getMasterIPList()) > 0 {
-		if err := k.deleteKubeNode(node); err != nil {
-			return fmt.Errorf("delete node %s failed %v", node, err)
+	return k.resetNode(node, func() {
+		//remove node
+		if len(k.getMasterIPList()) > 0 {
+			if err := k.RemoveNodeFromK8sClient(node); err != nil {
+				logger.Warn(fmt.Errorf("delete node %s failed %v", node, err))
+			}
 		}
-	}
-	return k.resetNode(node)
+	})
 }
