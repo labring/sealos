e069fe77bf270bef245bcda520425128a265fb98
cuisongliu <cuisongliu@qq.com>
2023-04-18 17:33:03 +0800

    docs(main): fix release note to gorelease
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -36,6 +36,30 @@ fi
 
 echo "$release_notes" > CHANGELOG/CHANGELOG-"${TAG#v}".md
 
+VERSION=$TAG
+PREV_VERSION=$3
+VERSION=${VERSION#v}
+if [ -z "$PREV_VERSION" ]; then
+  PREV_VERSION=$(git describe --abbrev=0 --tags v${VERSION}^)
+fi
+PREV_VERSION=${PREV_VERSION#v}
+
+# shellcheck disable=SC2129
+cat << EOF >> CHANGELOG/CHANGELOG-"${VERSION}".md
+## Changelog since v${PREV_VERSION}
+
+### What's Changed
+EOF
+
+git log --pretty=format:"* %h - %an - %s" v"${PREV_VERSION}"...v"${VERSION}" | sed -E 's/#[0-9]+/https:\/\/github.com\/labring\/sealos\/pull\/&/g' >>  CHANGELOG/CHANGELOG-"${VERSION}".md
+
+echo -e "\n\n**Full Changelog**: https://github.com/$REPO/compare/v${PREV_VERSION}...v${VERSION}" >>  CHANGELOG/CHANGELOG-"${VERSION}".md
+
+
+# shellcheck disable=SC2028
+echo "\n# Changelog" > CHANGELOG/CHANGELOG.md
+echo -e "\nAll notable changes to this project will be documented in this file.\n" >> CHANGELOG/CHANGELOG.md
+
 for file in $(ls CHANGELOG |grep -v '^CHANGELOG.md$' | sort -V -r); do
     version=$(echo $file | sed -E 's/CHANGELOG-(.*)\.md/\1/')
     echo -e "- [CHANGELOG-${version}.md](./CHANGELOG-${version}.md)" >> CHANGELOG/CHANGELOG.md
