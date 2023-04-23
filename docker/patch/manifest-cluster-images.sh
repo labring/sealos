908039e028e6128a7ff62f5a1099fd251d97599f
cuisongliu <cuisongliu@qq.com>
2023-04-23 12:48:18 +0800

    docs(main): fix ci patch images (#2971)
    * docs(main): fix ci patch images
    
    Signed-off-by: cuisongliu <cuisongliu@qq.com>
    
    * docs(main): fix ci patch images
    
    Signed-off-by: cuisongliu <cuisongliu@qq.com>
    
    ---------
    
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

---
+++ docker/patch/manifest-cluster-images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+IMAGE_NAME=ghcr.io/${OWNER}/sealos-patch:$GIT_COMMIT_SHORT_SHA
+sudo sealos push ${IMAGE_NAME}-amd64
+sudo sealos push ${IMAGE_NAME}-arm64
+sudo buildah images
+sudo buildah manifest create ${IMAGE_NAME}
+sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-amd64"
+sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-arm64"
+sudo buildah manifest push --all "$IMAGE_NAME" docker://"$IMAGE_NAME" && echo "$IMAGE_NAME push success"
+sudo buildah images
