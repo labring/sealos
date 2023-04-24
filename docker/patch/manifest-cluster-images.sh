ae24e9cadfe9b57d5cf190900e287fd0f2952570
lingdie <lingdie.yy@outlook.com>
2023-04-24 16:33:20 +0800

    add build cluster image and mutate image tag

--- docker/patch/manifest-cluster-images.sh
+++ docker/patch/manifest-cluster-images.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-IMAGE_NAME=ghcr.io/${OWNER}/sealos-patch:$GIT_COMMIT_SHORT_SHA
-sudo sealos push ${IMAGE_NAME}-amd64
-sudo sealos push ${IMAGE_NAME}-arm64
-sudo buildah images
-sudo buildah manifest create ${IMAGE_NAME}
-sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-amd64"
-sudo buildah manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-arm64"
-sudo buildah manifest push --all "$IMAGE_NAME" docker://"$IMAGE_NAME" && echo "$IMAGE_NAME push success"
-sudo buildah images
+REPO_NAME=${1:-sealos-patch}
+IMAGE_NAME=ghcr.io/${OWNER}/${REPO_NAME}:$GIT_COMMIT_SHORT_SHA
+sudo sealos push "${IMAGE_NAME}"-amd64
+sudo sealos push "${IMAGE_NAME}"-arm64
+sudo sealos images
+sudo sealos manifest create "${IMAGE_NAME}"
+sudo sealos manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-amd64"
+sudo sealos manifest add "$IMAGE_NAME" docker://"$IMAGE_NAME-arm64"
+sudo sealos manifest push --all "$IMAGE_NAME" docker://"$IMAGE_NAME" && echo "$IMAGE_NAME push success"
+sudo sealos images
