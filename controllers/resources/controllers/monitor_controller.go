5fafa2a6ddd457660f2e039cf9ef908bcf40a98d
jiahui <bxy4543@gmail.com>
2023-04-27 16:20:55 +0800

    init resources controller

---
+++ controllers/resources/controllers/monitor_controller.go
@@ -0,0 +1,365 @@
+/*
+Copyright 2023 sealos.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-logr/logr"
+	infrav1 "github.com/labring/sealos/controllers/infra/api/v1"
+	"github.com/labring/sealos/controllers/infra/common"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"math"
+	"os"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+	"time"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// MonitorReconciler reconciles a Monitor object
+type MonitorReconciler struct {
+	client.Client
+	logr.Logger
+	Interval        time.Duration
+	mongoConnectUrl string
+	Scheme          *runtime.Scheme
+	mongoOpts       struct {
+		uri      string
+		username string
+		password string
+	}
+}
+
+type quantity struct {
+	*resource.Quantity
+	detail string
+}
+
+const (
+	MONGO_URI      = "MONGO_URI"
+	MONGO_USERNAME = "MONGO_USERNAME"
+	MONGO_PASSWORD = "MONGO_PASSWORD"
+)
+
+//+kubebuilder:rbac:groups=resources.sealos.io,resources=monitors,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=resources.sealos.io,resources=monitors/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=resources.sealos.io,resources=monitors/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=resourcequotas,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Monitor object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.2/pkg/reconcile
+func (r *MonitorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	r.Info("start resource monitor reconcile", "time", time.Now().Format(time.RFC3339))
+
+	//TODO get mongo connect url from configmap or secret
+	dbCtx := context.Background()
+	mongoClient, err := mongo.Connect(dbCtx, options.Client().ApplyURI(r.mongoOpts.uri),
+		options.Client().SetAuth(options.Credential{Username: r.mongoOpts.username, Password: r.mongoOpts.password}))
+	if err != nil {
+		r.Logger.Error(err, "connect mongo client failed")
+		return ctrl.Result{Requeue: true}, err
+	}
+	defer func() {
+		err := mongoClient.Disconnect(ctx)
+		if err != nil {
+			r.Logger.V(5).Info("disconnect mongo client failed", "err", err)
+		}
+	}()
+
+	var resourceQuota corev1.ResourceQuota
+	var infra infrav1.Infra
+	if err = r.Get(ctx, req.NamespacedName, &resourceQuota); err == nil {
+		if err = r.quotaResourceUsage(ctx, mongoClient, &resourceQuota); err != nil {
+			r.Logger.Error(err, "monitor quota resource")
+			return ctrl.Result{Requeue: true}, err
+		}
+		r.Logger.Info("monitor resource quota", "name", resourceQuota.Name, "namespace", resourceQuota.Namespace)
+	} else if err = r.Get(ctx, req.NamespacedName, &infra); err == nil {
+		if err := r.infraResourceUsage(ctx, mongoClient, &infra); err != nil {
+			r.Logger.Error(err, "monitor infra resource")
+			return ctrl.Result{Requeue: true}, err
+		}
+		r.Logger.Info("monitor infra", "name", infra.Name, "namespace", infra.Namespace)
+	} else {
+		r.Logger.Error(err, "monitor resource failed")
+		return ctrl.Result{Requeue: true}, err
+	}
+	return ctrl.Result{Requeue: true, RequeueAfter: 1 * time.Minute}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *MonitorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	r.Logger = log.Log.WithName("monitor-controller")
+	//TODO get interval
+	r.Interval = time.Minute
+	//TODO get mongo connect url from configmap or secret
+	//r.mongoConnectUrl = "mongodb://192.168.64.17:27017"
+	r.mongoOpts = struct {
+		uri      string
+		username string
+		password string
+	}{
+		uri:      os.Getenv(MONGO_URI),
+		username: os.Getenv(MONGO_USERNAME),
+		password: os.Getenv(MONGO_PASSWORD),
+	}
+	switch {
+	case r.mongoOpts.uri == "":
+		return fmt.Errorf("mongo uri is empty")
+	case r.mongoOpts.username == "":
+		return fmt.Errorf("mongo username is empty")
+	case r.mongoOpts.password == "":
+		return fmt.Errorf("mongo password is empty")
+	}
+	if err := r.preApply(); err != nil {
+		return err
+	}
+	return ctrl.NewControllerManagedBy(mgr).
+		// Uncomment the following line adding a pointer to an instance of the controlled resource as an argument
+		For(&corev1.ResourceQuota{}, builder.WithPredicates(predicate.GenerationChangedPredicate{})).
+		Watches(&source.Kind{Type: &infrav1.Infra{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(predicate.GenerationChangedPredicate{})).
+		Complete(r)
+}
+
+func (r *MonitorReconciler) preApply() error {
+	ctx := context.Background()
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(r.mongoConnectUrl).SetAuth(options.Credential{Username: r.mongoOpts.username, Password: r.mongoOpts.password}))
+	if err != nil {
+		return fmt.Errorf("failed to connect mongo: %v", err)
+	}
+	defer func() {
+		err := mongoClient.Disconnect(ctx)
+		if err != nil {
+			r.Error(err, "disconnect mongo client failed")
+		}
+	}()
+
+	_ = createCompoundIndex(mongoClient, SealosResourcesDBName, SealosMonitorCollectionName)
+	return nil
+}
+
+func (r *MonitorReconciler) quotaResourceUsage(ctx context.Context, mongoClient *mongo.Client, resourceQuota *corev1.ResourceQuota) error {
+	timeStamp := time.Now().UTC()
+
+	resourceQuota.Status.Used[corev1.ResourceCPU] = resourceQuota.Spec.Hard[corev1.ResourceCPU]
+
+	_, err := mongoClient.Database(SealosResourcesDBName).Collection(SealosMonitorCollectionName).InsertMany(ctx, []interface{}{
+		&Monitor{
+			Category: resourceQuota.Namespace,
+			Property: corev1.ResourceCPU.String(),
+			Value:    getResourceValue(corev1.ResourceCPU, resourceQuota.Status.Used[corev1.ResourceCPU]),
+			Time:     timeStamp,
+			//TODO detail
+			Detail: "",
+		},
+		&Monitor{
+			Category: resourceQuota.Namespace,
+			Property: corev1.ResourceMemory.String(),
+			Value:    getResourceValue(corev1.ResourceMemory, resourceQuota.Status.Used[corev1.ResourceMemory]),
+			Time:     timeStamp,
+			//TODO detail
+			Detail: "",
+		},
+		&Monitor{
+			Category: resourceQuota.Namespace,
+			Property: corev1.ResourceStorage.String(),
+			Value:    getResourceValue(corev1.ResourceStorage, resourceQuota.Status.Used[corev1.ResourceStorage]),
+			Time:     timeStamp,
+			//TODO detail
+			Detail: "",
+		},
+	})
+	return err
+	//
+	//var resources map[string]map[corev1.ResourceName]*quantity
+	//var nsQuota map[string]*corev1.ResourceQuota
+	//initResource := func() (rs map[corev1.ResourceName]*quantity) {
+	//	rs = make(map[corev1.ResourceName]*quantity)
+	//	rs[corev1.ResourceCPU] = &quantity{Quantity: resource.NewQuantity(0, resource.DecimalSI), detail: ""}
+	//	rs[corev1.ResourceMemory] = &quantity{Quantity: resource.NewQuantity(0, resource.BinarySI), detail: ""}
+	//	rs[corev1.ResourceStorage] = &quantity{Quantity: resource.NewQuantity(0, resource.BinarySI), detail: ""}
+	//	return
+	//}
+	//
+	//for _, pod := range podList.Items {
+	//	//TODO pending status need skip?
+	//	if pod.Status.Phase != corev1.PodRunning && pod.Status.Phase != corev1.PodPending {
+	//		continue
+	//	}
+	//	if resources[pod.Namespace] == nil {
+	//		resources[pod.Namespace] = initResource()
+	//	}
+	//	for _, container := range pod.Spec.Containers {
+	//		if cpuRequest, ok := container.Resources.Requests[corev1.ResourceCPU]; ok {
+	//			resources[pod.Namespace][corev1.ResourceCPU].Add(cpuRequest)
+	//		} else {
+	//			resources[pod.Namespace][corev1.ResourceCPU].Add(container.Resources.Limits[corev1.ResourceCPU])
+	//		}
+	//		if memoryRequest, ok := container.Resources.Requests[corev1.ResourceMemory]; ok {
+	//			resources[pod.Namespace][corev1.ResourceMemory].Add(memoryRequest)
+	//		} else {
+	//			resources[pod.Namespace][corev1.ResourceMemory].Add(container.Resources.Limits[corev1.ResourceMemory])
+	//		}
+	//	}
+	//	if nsQuota[pod.Namespace] == nil {
+	//		nsQuota[pod.Namespace] = &corev1.ResourceQuota{}
+	//		if err := r.Get(ctx, client.ObjectKey{Name: meteringv1.ResourceQuotaPrefix + pod.Namespace, Namespace: pod.Namespace}, nsQuota[pod.Namespace]); err != nil {
+	//			r.Logger.Error(err, "get resource quota failed", "namespace", pod.Namespace)
+	//			resources[pod.Namespace][corev1.ResourceStorage].detail = err.Error()
+	//			continue
+	//		}
+	//	}
+	//	resources[pod.Namespace][corev1.ResourceStorage].Add(*nsQuota[pod.Namespace].Status.Used.Name("requests.storage", resource.BinarySI))
+	//}
+	//
+	////TODO if cpu, memory, storage is empty, skip
+	//var dataSources []interface{}
+	//for ns, res := range resources {
+	//	dataSources = append(dataSources, &Monitor{
+	//		Category: ns,
+	//		Property: corev1.ResourceCPU.String(),
+	//		Value:    getResourceValue(corev1.ResourceCPU, res),
+	//		Time:     timeStamp,
+	//		Detail:   res[corev1.ResourceCPU].String(),
+	//	}, &Monitor{
+	//		Category: ns,
+	//		Property: corev1.ResourceMemory.String(),
+	//		Value:    getResourceValue(corev1.ResourceMemory, res),
+	//		Time:     timeStamp,
+	//		Detail:   res[corev1.ResourceMemory].String(),
+	//	}, &Monitor{
+	//		Category: ns,
+	//		Property: corev1.ResourceStorage.String(),
+	//		Value:    getResourceValue(corev1.ResourceStorage, res),
+	//		Time:     timeStamp,
+	//		Detail:   res[corev1.ResourceStorage].String(),
+	//	})
+	//}
+	//_, err := mongoClient.Database(SealosResourcesDBName).Collection(SealosMonitorCollectionName).InsertMany(ctx, dataSources)
+	//return err
+}
+
+func getResourceValue(resourceName corev1.ResourceName, quantity resource.Quantity) int64 {
+	if quantity.Value() != 0 {
+		return int64(math.Ceil(float64(quantity.MilliValue()) / float64(PricesUnit[resourceName].MilliValue())))
+	}
+	return 0
+}
+
+//func getResourceValue(resourceName corev1.ResourceName, res map[corev1.ResourceName]*quantity) int64 {
+//	if res[resourceName] != nil && res[resourceName].MilliValue() != 0 {
+//		return int64(math.Ceil(float64(res[resourceName].MilliValue()) / float64(PricesUnit[resourceName].MilliValue())))
+//	}
+//	return 0
+//}
+
+func initResources() (rs map[corev1.ResourceName]*quantity) {
+	rs = make(map[corev1.ResourceName]*quantity)
+	rs[corev1.ResourceCPU] = &quantity{Quantity: resource.NewQuantity(0, resource.DecimalSI), detail: ""}
+	rs[corev1.ResourceMemory] = &quantity{Quantity: resource.NewQuantity(0, resource.BinarySI), detail: ""}
+	rs[corev1.ResourceStorage] = &quantity{Quantity: resource.NewQuantity(0, resource.BinarySI), detail: ""}
+	return
+}
+
+func (r *MonitorReconciler) infraResourceUsage(ctx context.Context, mongoClient *mongo.Client, infra *infrav1.Infra) error {
+
+	var infraList *infrav1.InfraList
+	err := r.List(ctx, infraList, client.InNamespace(infra.Namespace))
+	if err != nil {
+		return err
+	}
+	timeStamp := time.Now().UTC()
+	infraResources := initResources()
+	for i := range infraList.Items {
+		infra := infraList.Items[i]
+		// if infra is not running, skip it
+		if !r.checkInfraStatusRunning(infra) {
+			continue
+		}
+		for _, host := range infra.Spec.Hosts {
+			cnt := host.Count
+			flavor := host.Flavor
+			//TODO 单位统一 getInfraCPUQuantity/getInfraMemoryQuantity/getInfraDiskQuantity
+			infraResources[corev1.ResourceCPU].Add(*getInfraCPUQuantity(flavor, cnt))
+			infraResources[corev1.ResourceMemory].Add(*getInfraMemoryQuantity(flavor, cnt))
+			for _, disk := range host.Disks {
+				infraResources[corev1.ResourceStorage].Add(*getInfraDiskQuantity(disk.Capacity))
+			}
+		}
+		r.Logger.Info("infra resources", "namespace", infra.Namespace, "cpu quantity", infraResources[common.CPUResourceName], "memory quantity", infraResources[common.MemoryResourceName], "volume quantity", infraResources[common.VolumeResourceName])
+	}
+	_, err = mongoClient.Database(SealosResourcesDBName).Collection(SealosMonitorCollectionName).InsertMany(ctx, []interface{}{
+		&Monitor{
+			Category: infra.Namespace,
+			Property: PropertyInfraCPU,
+			Value:    getResourceValue(corev1.ResourceCPU, *infraResources[corev1.ResourceCPU].Quantity),
+			Time:     timeStamp,
+			Detail:   "",
+		},
+		&Monitor{
+			Category: infra.Namespace,
+			Property: PropertyInfraMemory,
+			Value:    getResourceValue(corev1.ResourceMemory, *infraResources[corev1.ResourceMemory].Quantity),
+			Time:     timeStamp,
+			Detail:   "",
+		},
+		&Monitor{
+			Category: infra.Namespace,
+			Property: PropertyInfraDisk,
+			Value:    getResourceValue(corev1.ResourceStorage, *infraResources[corev1.ResourceStorage].Quantity),
+			Time:     timeStamp,
+			Detail:   "",
+		},
+	})
+	return err
+}
+
+// checkInfraStatus check infra status
+func (r *MonitorReconciler) checkInfraStatusRunning(infra infrav1.Infra) bool {
+	if infra.Status.Status == infrav1.Running.String() {
+		return true
+	}
+	r.Logger.Info("infra status is not running", "infra name", infra.Name, "infra namespace", infra.Namespace, "infra status", infra.Status.Status)
+	return false
+}
+
+func (r *MonitorReconciler) checkPodStatus(pod corev1.Pod) bool {
+	if pod.Status.Phase == corev1.PodRunning {
+		return true
+	}
+	r.Logger.Info("pod status is  not ready", "pod name", pod.Name, "pod namespace", pod.Namespace, "pod status", pod.Status.Phase)
+	return false
+}
---
+++ controllers/resources/controllers/table.go
@@ -0,0 +1,271 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	corev1 "k8s.io/api/core/v1"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+const (
+	CategoryField = "category"
+	PropertyField = "property"
+	TimeField     = "time"
+	ValueField    = "value"
+	PriceField    = "price"
+	AmountField   = "amount"
+)
+
+/*
+Unified base unit:
+
+CPU: millicore (mCore) (1 core = 1000 millicores) 0.067 / 1000 * 1000000 = 67 unit price
+Memory: Mebibytes (MiB) 0.033 / 1024 (2^30) * 1000000 = 33 unit price
+Storage: Mebibytes (MiB) 0.0021 / 1024 * 1000000 = 2 unit price
+Network bandwidth: Kbps (kilobits per second) not yet available
+*/
+
+//| property     | Price | Detail         |
+//| ------------ | ----- | -------------- |
+//| Cpu          | 67    | mCore unit     |
+//| Memory       | 33    | Mebibytes unit |
+//| Disk         | 2     | Mebibytes unit |
+//| Mongodb      | 1     | feature...     |
+//| Minio        | 1     | Mebibytes unit |
+//| Infra-Cpu    | 67    | mCore unit     |
+//| Infra-Memory | 33    | Mebibytes unit |
+//| Infra-Disk   | 2     | Mebibytes unit |
+//
+// price: 1 = 1000000¥
+
+type Price struct {
+	Property string  `json:"property" bson:"property"`
+	Price    float64 `json:"price" bson:"price"`
+	Detail   string  `json:"detail" bson:"detail"`
+	//Unit     string  `json:"unit" bson:"unit"`
+}
+
+//| Category   | property     | Time       | value |      |
+//| ---------- | ------------ | -------    | ----- | ---- |
+//| Namespace1 | cpu          | timestamp1 | 5     |      |
+//| Namespace1 | memory       | timestamp1 | 100   |      |
+//| Namespace1 | disk         | timestamp1 | 30    |      |
+//| appid1     | mongodb      | timestamp1 | 100   |      |
+//| appid1     | Minio        | timestamp1 | 100   |      |
+//| Namespace2 | Infra-Cpu    | timestamp1 | 5     |      |
+//| Namespace2 | cpu          | timestamp1 | 100   |      |
+//| Namespace2 | Infra-Memory | timestamp1 | 30    |      |
+//| Namespace2 | Infra-Disk   | timestamp1 | 100   |      |
+//| Namespace2 | memory       | timestamp1 | 100   |      |
+//| Namespace2 |  disk        | timestamp1 | 100   |      |
+
+// Composite index: category, property, time, speed up query
+type Monitor struct {
+	Category string    `json:"category" bson:"category"`
+	Property string    `json:"property" bson:"property"`
+	Time     time.Time `json:"time" bson:"time"`
+	Value    int64     `json:"value" bson:"value"`
+	Status   int       `json:"status" bson:"status"`
+	Detail   string    `json:"detail" bson:"detail"`
+}
+
+//| Category   | Property | Time       | Value (average value) | Amount (value * price) | Detail | Status |
+//| ---------- | -------- | ---------- | --------------------- | ---------------------- | ------ | ------ |
+//| Namespace1 | Cpu      | 2023010112 | 1000                  | 67000                  |        | 0      |
+
+type Metering struct {
+	Category string `json:"category" bson:"category"`
+	//time 2023010112 -> 2023-01-01 11:00:00 - 2023-01-01 12:00:00
+	Amount int64 `json:"amount" bson:"amount"`
+	// 2023010112 -> 2023-01-01 12:00:00
+	Property string    `json:"property" bson:"property"`
+	Value    int64     `json:"value" bson:"value"`
+	Time     time.Time `json:"time" bson:"time"`
+	Detail   string    `json:"detail" bson:"detail"`
+	// 0 -> not settled, 1 -> settled, -1 -> deleted, -2 -> refunded
+	Status int `json:"status" bson:"status"`
+}
+
+const (
+	SealosResourcesDBName        = "sealos-resources"
+	SealosMonitorCollectionName  = "monitor"
+	SealosPricesCollectionName   = "prices"
+	SealosMeteringCollectionName = "metering"
+	SealosBillingCollectionName  = "billing"
+)
+
+const (
+	PropertyInfraCPU    = "infra-cpu"
+	PropertyInfraMemory = "infra-memory"
+	PropertyInfraDisk   = "infra-disk"
+)
+
+var PricesUnit = map[corev1.ResourceName]*resource.Quantity{
+	corev1.ResourceCPU:     resource.NewQuantity(1000, resource.DecimalSI), // 1 CPU core (1000 mCore)
+	corev1.ResourceMemory:  resource.NewQuantity(1<<20, resource.BinarySI), // 1 MiB
+	corev1.ResourceStorage: resource.NewQuantity(1<<20, resource.BinarySI), // 1 MiB
+}
+
+// mCore
+var infraCPUMap = map[string]int{
+	"t2.medium":     2000,
+	"t2.large":      2000,
+	"t2.xlarge":     4000,
+	"ecs.c7.large":  2000,
+	"ecs.g7.large":  2000,
+	"ecs.g7.xlarge": 4000,
+}
+
+// GiB
+var infraMemoryMap = map[string]int{
+	"t2.medium":     4,
+	"t2.large":      8,
+	"t2.xlarge":     16,
+	"ecs.c7.large":  4,
+	"ecs.g7.large":  8,
+	"ecs.g7.xlarge": 16,
+}
+
+// GiB
+func getCPUQuantity(flavor string) *resource.Quantity {
+	if v, ok := infraCPUMap[flavor]; ok {
+		return resource.NewQuantity(int64(v), resource.DecimalSI)
+	}
+	return nil
+}
+
+// MiB
+func getInfraCPUQuantity(flavor string, count int) *resource.Quantity {
+	if v, ok := infraCPUMap[flavor]; ok {
+		return resource.NewQuantity(int64(v*count), resource.DecimalSI)
+	}
+	return nil
+}
+
+// Gib
+func getInfraMemoryQuantity(flavor string, count int) *resource.Quantity {
+	if v, ok := infraMemoryMap[flavor]; ok {
+		return resource.NewQuantity(int64((v*count)<<30), resource.BinarySI)
+	}
+	return nil
+}
+
+// Gib
+func getInfraDiskQuantity(capacity int) *resource.Quantity {
+	return resource.NewQuantity(int64(capacity<<30), resource.BinarySI)
+}
+
+func GetPrices(mongoClient *mongo.Client) ([]Price, error) {
+	collection := mongoClient.Database(SealosResourcesDBName).Collection(SealosPricesCollectionName)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, fmt.Errorf("get all prices error: %v", err)
+	}
+	var prices []Price
+	if err = cursor.All(ctx, &prices); err != nil {
+		return nil, fmt.Errorf("get all prices error: %v", err)
+	}
+	return prices, nil
+}
+
+// create unique index
+func createUniqueIndex(collection *mongo.Collection, field string) error {
+	indexModel := mongo.IndexModel{
+		Keys:    bson.D{{Key: field, Value: 1}},
+		Options: options.Index().SetUnique(true),
+	}
+
+	_, err := collection.Indexes().CreateOne(context.Background(), indexModel)
+	if err != nil {
+		return fmt.Errorf("failed to create unique index on %s: %v", field, err)
+	}
+	return nil
+}
+
+// 复合索引 加速查询
+func createCompoundIndex(client *mongo.Client, dbName, collectionName string) error {
+	collection := client.Database(dbName).Collection(collectionName)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	indexModel := mongo.IndexModel{
+		Keys: bson.D{
+			{Key: CategoryField, Value: 1},
+			{Key: PropertyField, Value: 1},
+			{Key: TimeField, Value: 1},
+		},
+	}
+
+	_, err := collection.Indexes().CreateOne(ctx, indexModel)
+	return err
+}
+
+func ensureCompoundIndex(client *mongo.Client, dbName, collName string, indexKeys bson.M) error {
+	collection := client.Database(dbName).Collection(collName)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// List all indexes in the collection
+	cursor, err := collection.Indexes().List(ctx)
+	if err != nil {
+		return err
+	}
+	defer cursor.Close(ctx)
+
+	// Check if the compound index already exists
+	indexExists := false
+	for cursor.Next(ctx) {
+		var index bson.M
+		if err := cursor.Decode(&index); err != nil {
+			return err
+		}
+		if keys, ok := index["key"].(bson.M); ok {
+			if reflect.DeepEqual(keys, indexKeys) {
+				indexExists = true
+				break
+			}
+		}
+	}
+
+	// Create the compound index if it doesn't exist
+	if !indexExists {
+		_, err := collection.Indexes().CreateOne(ctx, mongo.IndexModel{
+			Keys: indexKeys,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func GetAllPricesMap(mongoClient *mongo.Client) (map[string]Price, error) {
+	collection := mongoClient.Database(SealosResourcesDBName).Collection(SealosPricesCollectionName)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, fmt.Errorf("get all prices error: %v", err)
+	}
+	var prices []Price
+	if err = cursor.All(ctx, &prices); err != nil {
+		return nil, fmt.Errorf("get all prices error: %v", err)
+	}
+	var pricesMap = make(map[string]Price, len(prices))
+	for i := range prices {
+		pricesMap[strings.ToLower(prices[i].Property)] = prices[i]
+	}
+	return pricesMap, nil
+}
