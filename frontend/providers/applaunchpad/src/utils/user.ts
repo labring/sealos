476d37d62f6dc6e763a85c56af7a04a0fd94c4be
archer <545436317@qq.com>
2023-05-06 11:27:29 +0800

    feat: logs analyses

--- frontend/providers/applaunchpad/src/utils/user.ts
+++ frontend/providers/applaunchpad/src/utils/user.ts
@@ -2,9 +2,10 @@ import yaml from 'js-yaml';
 export const getUserKubeConfig = () => {
   let kubeConfig: string =
     process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_MOCK_USER || '' : '';
+
   try {
     const store = localStorage.getItem('session');
-    if (store) {
+    if (!kubeConfig && store) {
       kubeConfig = JSON.parse(store)?.kubeconfig;
     }
   } catch (err) {
