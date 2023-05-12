03c68fa07501f3c0819b864b2603a124115a5742
zhu jing yang <3161362058@qq.com>
2023-05-12 15:32:40 +0800

    feat: desktop pwa
    Signed-off-by: zhu jing yang <3161362058@qq.com>

--- frontend/desktop/src/pages/_document.tsx
+++ frontend/desktop/src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
+import { theme } from '@/styles/chakraTheme';
 
 export default function Document() {
   return (
@@ -19,9 +21,12 @@ export default function Document() {
 
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
