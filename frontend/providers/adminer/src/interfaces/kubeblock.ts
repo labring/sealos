3a66249c892341f44e2f7a7a637a9f505e0dcaa0
zzjin <zzjin@users.noreply.github.com>
2023-05-24 17:04:39 +0800

    Dev adminer (#3120)
    * temp commit
    
    Signed-off-by: zzjin <tczzjin@gmail.com>
    
    * temp fix 2

---
+++ frontend/providers/adminer/src/interfaces/kubeblock.ts
@@ -0,0 +1,93 @@
+import * as k8s from '@kubernetes/client-node';
+
+export interface KubeBlockCluster extends k8s.KubernetesObject {
+  spec: KubeBlockClusterSpec;
+  status?: KubeBlockClusterStatus;
+}
+
+export interface KubeBlockClusterSpec {
+  affinity: object;
+  clusterDefinitionRef: string;
+  clusterVersionRef: string;
+  componentSpecs: object[];
+  terminationPolicy: string;
+}
+export interface KubeBlockClusterStatus {
+  clusterDefGeneration: number;
+  components: object;
+  conditions: k8s.V1Condition[];
+  observedGeneration: number;
+  phase: string;
+}
+
+const labelName = 'clusterdefinition.kubeblocks.io/name';
+
+// this template is suite for golang(kubernetes and sealos)'s template engine
+export async function generateKubeBlockClusterTemplate(
+  kc: k8s.KubeConfig,
+  namespace: string
+): Promise<string[]> {
+  try {
+    const connections: string[] = [];
+    // get kubeblock crd list
+    const kubeblockResp = await kc
+      .makeApiClient(k8s.CustomObjectsApi)
+      .listNamespacedCustomObject(
+        'apps.kubeblocks.io',
+        'v1alpha1',
+        namespace,
+        'clusters',
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        labelName
+      );
+
+    const kubeblockList = kubeblockResp.body as k8s.KubernetesListObject<KubeBlockCluster>;
+
+    if (kubeblockList.items.length === 0) {
+      return connections;
+    }
+
+    const secretClient = kc.makeApiClient(k8s.CoreV1Api);
+
+    for (const item of kubeblockList.items) {
+      if (item.status?.phase !== 'Running') {
+        continue;
+      }
+
+      if (item.metadata?.labels && item.metadata.labels[labelName] !== '') {
+        const clusterType = item.metadata.labels[labelName];
+        if (!item.metadata.name || !item.metadata.namespace) {
+          continue;
+        }
+
+        if (clusterType === 'postgresql') {
+          // get secret
+          const secretName = item.metadata.name + '-conn-credential';
+          const secret = await secretClient.readNamespacedSecret(
+            secretName,
+            item.metadata.namespace
+          );
+
+          if (!secret.body?.data) {
+            continue;
+          }
+
+          const username = atob(secret.body.data['username']);
+          const password = atob(secret.body.data['postgres-password']);
+          const host = atob(secret.body.data['host']);
+          const port = atob(secret.body.data['port']);
+
+          const pgConnection = 'pgsql://' + username + ':' + password + '@' + host + ':' + port;
+          connections.push(pgConnection);
+        }
+      }
+    }
+
+    return Promise.resolve(connections);
+  } catch (error) {
+    return Promise.resolve([]);
+  }
+}
