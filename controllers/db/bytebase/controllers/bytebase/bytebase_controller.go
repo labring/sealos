03018ce93fadf2344e363d859cdeded6f4068c49
Yun Pan <dinoallo@netc.it>
2023-05-29 11:09:44 +0800

    fix typo

--- controllers/db/bytebase/controllers/bytebase/bytebase_controller.go
+++ controllers/db/bytebase/controllers/bytebase/bytebase_controller.go
@@ -74,7 +74,7 @@ type Reconciler struct {
 
 // +kubebuilder:rbac:groups=bytebase.db.sealos.io,resources=bytebases,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=bytebase.db.sealos.io,resources=bytebases/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=bytebase.db.sesalos.io,resources=bytebases/finalizers,verbs=update
+// +kubebuilder:rbac:groups=bytebase.db.sealos.io,resources=bytebases/finalizers,verbs=update
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
