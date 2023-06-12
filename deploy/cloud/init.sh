3c2e28f10cf8084749c9046e38d1918b412db275
lingdie <lingdie.yy@outlook.com>
2023-06-12 12:53:05 +0800

    delete auth service

--- deploy/cloud/init.sh
+++ deploy/cloud/init.sh
@@ -6,7 +6,7 @@ sealos pull ghcr.io/labring/sealos-cloud-app-controller:dev
 sealos pull ghcr.io/labring/sealos-cloud-desktop-frontend:dev
 sealos pull ghcr.io/labring/sealos-cloud-terminal-frontend:dev
 sealos pull ghcr.io/labring/sealos-cloud-applaunchpad-frontend:dev
-sealos pull ghcr.io/labring/sealos-cloud-dbprovider-frontend:dev
+
 
 
 sealos save -o tars/user.tar ghcr.io/labring/sealos-cloud-user-controller:dev
@@ -15,4 +15,4 @@ sealos save -o tars/app.tar ghcr.io/labring/sealos-cloud-app-controller:dev
 sealos save -o tars/frontend-desktop.tar  ghcr.io/labring/sealos-cloud-desktop-frontend:dev
 sealos save -o tars/frontend-terminal.tar  ghcr.io/labring/sealos-cloud-terminal-frontend:dev
 sealos save -o tars/frontend-applaunchpad.tar ghcr.io/labring/sealos-cloud-applaunchpad-frontend:dev
-sealos save -o tars/frontend-dbprovider.tar ghcr.io/labring/sealos-cloud-dbprovider-frontend:dev
+
