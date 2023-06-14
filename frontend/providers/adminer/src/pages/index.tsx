a4a56ecee220d927ea7d629b794c995990a41c42
zzjin <tczzjin@gmail.com>
2023-06-14 23:54:33 +0800

    Update adminer frontend to support 404&503
    Signed-off-by: zzjin <tczzjin@gmail.com>

--- frontend/providers/adminer/src/pages/index.tsx
+++ frontend/providers/adminer/src/pages/index.tsx
@@ -6,9 +6,14 @@ import { useEffect, useState } from 'react';
 import { createSealosApp, sealosApp } from 'sealos-desktop-sdk/app';
 import styles from './index.module.scss';
 
-export default function Index() {
+type ServiceEnv = {
+  site: string;
+};
+
+export default function Index(props: ServiceEnv) {
   const { setSession, isUserLogin } = useSessionStore();
   const [url, setUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     return createSealosApp();
@@ -24,39 +29,36 @@ export default function Index() {
     initApp();
   }, [setSession]);
 
-  const { data, isLoading, refetch, isError } = useQuery(
-    ['applyApp'],
-    () => request.post('/api/apply'),
-    {
-      onSuccess: (res) => {
-        if (res?.data?.code === 200 && res?.data?.data) {
-          const url = res?.data?.data;
-          fetch(url, { mode: 'no-cors' })
-            .then(() => {
+  useQuery(['applyApp'], () => request.post('/api/apply'), {
+    onSuccess: (res) => {
+      if (res?.data?.code === 200 && res?.data?.data) {
+        const url = res?.data?.data;
+        if (process.env.NODE_ENV === 'development') {
+          setIsLoading(false);
+          setUrl(url);
+        }
+        fetch(url, { mode: 'cors' })
+          .then((res) => {
+            if (res.status === 200) {
+              setIsLoading(false);
               setUrl(url);
-              window.location.replace(url);
-            })
-            .catch((err) => console.log(err));
-        }
-        if (res?.data?.code === 201) {
-          refetch();
-        }
-      },
-      onError: (err) => {
-        console.log(err, 'err');
-      },
-      refetchInterval: url === '' ? 1000 : false,
-      enabled: url === ''
-    }
-  );
+            }
+          })
+          .catch((err) => {});
+      }
+    },
+    refetchInterval: url === '' ? 500 : false,
+    enabled: url === ''
+  });
+
   if (isLoading) {
     return <div className={clsx(styles.loading, styles.err)}>loading</div>;
   }
 
-  if (!isUserLogin && isError) {
+  if (!isUserLogin && process.env.NODE_ENV === 'production') {
     return (
       <div className={styles.err}>
-        please go to &nbsp;<a href="https://cloud.sealos.io/">sealos</a>
+        please go to &nbsp;<a href={props.site}>{props.site}</a>
       </div>
     );
   }
@@ -72,4 +74,12 @@ export default function Index() {
       )}
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      site: process.env.SITE
+    }
+  };
 }
