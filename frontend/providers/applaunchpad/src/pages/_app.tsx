8ce1178aaeb75670f0e7cfab4e6bd171adbbfcaa
archer <545436317@qq.com>
2023-06-15 16:49:22 +0800

    perf: price box

--- frontend/providers/applaunchpad/src/pages/_app.tsx
+++ frontend/providers/applaunchpad/src/pages/_app.tsx
@@ -106,7 +106,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         });
       } catch (error) {
         changeI18n({
-          currentLanguage: 'zh'
+          currentLanguage: 'en'
         });
       }
     })();
