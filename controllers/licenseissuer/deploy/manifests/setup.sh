41df7e118cc0a64920d11ddbccbe22a19d1384be
ElonMuskkkkkk <yxxchange@163.com>
2023-08-01 11:41:46 +0800

    modify setup.sh

--- controllers/licenseissuer/deploy/manifests/setup.sh
+++ controllers/licenseissuer/deploy/manifests/setup.sh
@@ -16,9 +16,9 @@ fi
 
 kubectl apply -f manifests/deploy.yaml
 
-while kubectl get crd | grep -q "launchers.infostream.sealos.io"; do
+while ! kubectl get crd | grep -q "launchers.infostream.sealos.io"; do
     echo "Waiting for launchers.infostream.sealos.io CRD to be created..."
-    sleep 1
+    sleep 3
 done
 
 kubectl apply -f manifests/customconfig.yaml
