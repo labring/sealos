004edf91722e7205a39752d1226a32eaf2f426ea
ElonMuskkkkkk <yxxchange@163.com>
2023-08-02 18:52:16 +0800

    add preset-admin user

--- deploy/cloud/scripts/init.sh
+++ deploy/cloud/scripts/init.sh
@@ -5,6 +5,7 @@ cloudDomain="127.0.0.1.nip.io"
 tlsCrtPlaceholder="<tls-crt-placeholder>"
 tlsKeyPlaceholder="<tls-key-placeholder>"
 mongodbUri=""
+saltKey=""
 
 function read_env {
   source $1
@@ -53,7 +54,9 @@ function sealos_run_controller {
   # run licenseissuer controller
   sealos run tars/licenseissuer.tar \
   --env canConnectToExternalNetwork="true" \
-  --env enableMonitor="true" 
+  --env enableMonitor="true" \
+  --env MongoURI="$mongodbUri" \
+  --env PasswordSalt="$saltKey"
 }
 
 function sealos_authorize {
@@ -77,6 +80,10 @@ function sealos_authorize {
     # issue license for admin-user
     echo "license issue for admin-user"
     kubectl apply -f manifests/free-license.yaml
+}
+
+function gen_saltKey() {
+    saltKey=$(tr -dc 'a-z0-9' </dev/urandom | head -c64 | base64 -w 0)
 }
 
 function gen_mongodbUri() {
@@ -134,8 +141,10 @@ function mutate_desktop_config() {
   # mutate etc/sealos/desktop-config.yaml by using mongodb uri and two random base64 string
   sed -i -e "s;<your-mongodb-uri-base64>;$(echo -n "$mongodbUri" | base64 -w 0);" etc/sealos/desktop-config.yaml
   sed -i -e "s;<your-jwt-secret-base64>;$(tr -cd 'a-z0-9' </dev/urandom | head -c64 | base64 -w 0);" etc/sealos/desktop-config.yaml
-  sed -i -e "s;<your-password-salt-base64>;$(tr -cd 'a-z0-9' </dev/urandom | head -c64 | base64 -w 0);" etc/sealos/desktop-config.yaml
+  sed -i -e "s;<your-password-salt-base64>;$saltKey;" etc/sealos/desktop-config.yaml
 }
+
+
 
 function install {
   # read env
@@ -152,6 +161,9 @@ function install {
 
   # gen mongodb uri
   gen_mongodbUri
+
+  # gen saltKey
+  gen_saltKey
 
   # sealos run controllers
   sealos_run_controller
