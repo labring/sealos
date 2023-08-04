512f7b8a08bf5de122aa43bd2611080ba259b6e8
jingyang <3161362058@qq.com>
2023-08-04 15:26:26 +0800

    fix getEnv
    Signed-off-by: jingyang <3161362058@qq.com>

--- frontend/desktop/src/pages/api/platform/getEnv.ts
+++ frontend/desktop/src/pages/api/platform/getEnv.ts
@@ -2,16 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { jsonRes } from '@/services/backend/response';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    jsonRes(res, {
-      data: {
-        SEALOS_CLOUD_DOMAIN: process.env.SEALOS_CLOUD_DOMAIN || 'cloud.sealos.io'
-      }
-    });
-  } catch (err: any) {
-    jsonRes(res, {
-      code: 500,
-      data: 'get env error'
-    });
-  }
+  jsonRes(res, {
+    data: {
+      SEALOS_CLOUD_DOMAIN: process.env.SEALOS_CLOUD_DOMAIN || 'cloud.sealos.io'
+    }
+  });
 }
