cb360127f6fcfce13425a891d0fd050f958fdc19
yy <lingdie.yy@outlook.com>
2023-08-14 15:15:38 +0800

    feat: adapt aio cluster image fot changeable port.

--- deploy/cloud/init.sh
+++ deploy/cloud/init.sh
@@ -16,12 +16,13 @@ sealos pull --policy=always ghcr.io/labring/sealos-cloud-licenseissuer-controlle
 sealos save -o tars/user.tar ghcr.io/labring/sealos-cloud-user-controller:latest
 sealos save -o tars/terminal.tar ghcr.io/labring/sealos-cloud-terminal-controller:latest
 sealos save -o tars/app.tar ghcr.io/labring/sealos-cloud-app-controller:latest
-sealos save -o tars/frontend-desktop.tar  ghcr.io/labring/sealos-cloud-desktop-frontend:latest
-sealos save -o tars/frontend-terminal.tar  ghcr.io/labring/sealos-cloud-terminal-frontend:latest
-sealos save -o tars/frontend-applaunchpad.tar ghcr.io/labring/sealos-cloud-applaunchpad-frontend:latest
-sealos save -o tars/frontend-dbprovider.tar ghcr.io/labring/sealos-cloud-dbprovider-frontend:latest
 sealos save -o tars/metering.tar ghcr.io/labring/sealos-cloud-resources-metering-controller:latest
 sealos save -o tars/monitoring.tar ghcr.io/labring/sealos-cloud-resources-controller:latest
 sealos save -o tars/account.tar ghcr.io/labring/sealos-cloud-account-controller:latest
-sealos save -o tars/cost-center.tar ghcr.io/labring/sealos-cloud-costcenter-frontend:latest
 sealos save -o tars/licenseissuer.tar ghcr.io/labring/sealos-cloud-licenseissuer-controller:latest
+
+sealos save -o tars/frontend-desktop.tar  ghcr.io/labring/sealos-cloud-desktop-frontend:latest
+sealos save -o tars/frontend-terminal.tar  ghcr.io/labring/sealos-cloud-terminal-frontend:latest
+sealos save -o tars/frontend-dbprovider.tar ghcr.io/labring/sealos-cloud-dbprovider-frontend:latest
+sealos save -o tars/frontend-costcenter.tar ghcr.io/labring/sealos-cloud-costcenter-frontend:latest
+sealos save -o tars/frontend-applaunchpad.tar ghcr.io/labring/sealos-cloud-applaunchpad-frontend:latest
--- deploy/cloud/scripts/init.sh
+++ deploy/cloud/scripts/init.sh
@@ -2,13 +2,65 @@
 set -ex
 
 cloudDomain="127.0.0.1.nip.io"
+cloudPort=""
+mongodbUri=""
+
 tlsCrtPlaceholder="<tls-crt-placeholder>"
 tlsKeyPlaceholder="<tls-key-placeholder>"
-mongodbUri=""
 saltKey=""
 
-function read_env {
-  source $1
+function prepare {
+  # source .env
+  source etc/sealos/.env
+
+  # kubectl apply namespace, secret and mongodb
+  kubectl apply -f manifests/namespace.yaml
+
+  # apply notifications crd
+  kubectl apply -f manifests/notifications_crd.yaml
+
+  # gen mongodb uri
+  gen_mongodbUri
+
+  # gen saltKey if not set or not found in secret
+  gen_saltKey
+
+  # mutate desktop config
+  mutate_desktop_config
+
+  # create tls secret
+  create_tls_secret
+}
+
+function gen_mongodbUri() {
+  # if mongodbUri is empty then create mongodb and gen mongodb uri
+  if [ -z "$mongodbUri" ]; then
+    echo "no mongodb uri found, create mongodb and gen mongodb uri"
+    kubectl apply -f manifests/mongodb.yaml
+    # if there is no sealos-mongodb-conn-credential secret then wait for mongodb ready
+    while [ -z "$(kubectl get secret -n sealos sealos-mongodb-conn-credential)" ]; do
+      echo "waiting for mongodb secret generated"
+      sleep 5
+    done
+    chmod +x scripts/gen-mongodb-uri.sh
+    mongodbUri=$(scripts/gen-mongodb-uri.sh)
+  fi
+}
+
+function gen_saltKey() {
+    password_salt=$(kubectl get secret desktop-frontend-secret -n sealos -o jsonpath="{.data.password_salt}" 2>/dev/null || true)
+    if [[ -z "$password_salt" ]]; then
+        saltKey=$(tr -dc 'a-z0-9' </dev/urandom | head -c64 | base64 -w 0)
+    else
+        saltKey=$password_salt
+    fi
+}
+
+function mutate_desktop_config() {
+    # mutate etc/sealos/desktop-config.yaml by using mongodb uri and two random base64 string
+    sed -i -e "s;<your-mongodb-uri-base64>;$(echo -n "$mongodbUri" | base64 -w 0);" etc/sealos/desktop-config.yaml
+    sed -i -e "s;<your-jwt-secret-base64>;$(tr -cd 'a-z0-9' </dev/urandom | head -c64 | base64 -w 0);" etc/sealos/desktop-config.yaml
+    sed -i -e "s;<your-password-salt-base64>;$saltKey;" etc/sealos/desktop-config.yaml
 }
 
 function create_tls_secret {
@@ -25,12 +77,13 @@ function create_tls_secret {
 function sealos_run_controller {
   # run user controller
   sealos run tars/user.tar \
-  --env cloudDomain=$cloudDomain \
-  --env cloudPort="6443"
+  --env cloudDomain="$cloudDomain" \
+  --env apiserverPort="6443"
 
   # run terminal controller
   sealos run tars/terminal.tar \
-  --env cloudDomain=$cloudDomain \
+  --env cloudDomain="$cloudDomain" \
+  --env cloudPort="$cloudPort" \
   --env userNamespace="user-system" \
   --env wildcardCertSecretName="wildcard-cert" \
   --env wildcardCertSecretNamespace="sealos-system"
@@ -49,14 +102,50 @@ function sealos_run_controller {
   # run account controller
   sealos run tars/account.tar \
   --env MONGO_URI="$mongodbUri" \
+  --env cloudDomain="$cloudDomain" \
+  --env cloudPort="$cloudPort" \
   --env DEFAULT_NAMESPACE="account-system"
 
   # run licenseissuer controller
   sealos run tars/licenseissuer.tar \
   --env canConnectToExternalNetwork="true" \
-  --env enableMonitor="true" \
-  --env MongoURI="$mongodbUri" \
-  --env PasswordSalt="$saltKey"
+  --env enableMonitor="true"
+}
+
+function sealos_run_frontend {
+  echo "run desktop frontend"
+  sealos run tars/frontend-desktop.tar \
+    --env cloudDomain=$cloudDomain \
+    --env cloudPort=$cloudPort \
+    --env certSecretName="wildcard-cert" \
+    --env passwordEnabled="true" \
+    --config-file etc/sealos/desktop-config.yaml
+
+  echo "run applaunchpad frontend"
+  sealos run tars/frontend-applaunchpad.tar \
+  --env cloudDomain=$cloudDomain \
+  --env cloudPort=$cloudPort \
+  --env certSecretName="wildcard-cert"
+
+  echo "run terminal frontend"
+  sealos run tars/frontend-terminal.tar \
+  --env cloudDomain=$cloudDomain \
+  --env cloudPort=$cloudPort \
+  --env certSecretName="wildcard-cert"
+
+  echo "run dbprovider frontend"
+  sealos run tars/frontend-dbprovider.tar \
+  --env cloudDomain=$cloudDomain \
+  --env cloudPort=$cloudPort \
+  --env certSecretName="wildcard-cert"
+
+  echo "run cost center frontend"
+  sealos run tars/frontend-costcenter.tar \
+  --env cloudDomain=$cloudDomain \
+  --env cloudPort=$cloudPort \
+  --env certSecretName="wildcard-cert" \
+  --env transferEnabled="true" \
+  --env rechargeEnabled="false"
 }
 
 function sealos_authorize {
@@ -76,94 +165,13 @@ function sealos_authorize {
     done
     # issue license for admin-user
     echo "license issue for admin-user"
-
-    # issue license for admin-user
-    echo "license issue for admin-user"
     kubectl apply -f manifests/free-license.yaml
 }
 
-function gen_saltKey() {
-    saltKey=$(tr -dc 'a-z0-9' </dev/urandom | head -c64 | base64 -w 0)
-}
-
-function gen_mongodbUri() {
-  # if mongodbUri is empty then create mongodb and gen mongodb uri
-  if [ -z "$mongodbUri" ]; then
-    echo "no mongodb uri found, create mongodb and gen mongodb uri"
-    kubectl apply -f manifests/mongodb.yaml
-    # if there is no sealos-mongodb-conn-credential secret then wait for mongodb ready
-    while [ -z "$(kubectl get secret -n sealos sealos-mongodb-conn-credential)" ]; do
-      echo "waiting for mongodb secret generated"
-      sleep 5
-    done
-    chmod +x scripts/gen-mongodb-uri.sh
-    mongodbUri=$(scripts/gen-mongodb-uri.sh)
-  fi
-}
-
-function sealos_run_frontend {
-  # mutate desktop config before running desktop
-  echo "mutate desktop config"
-  mutate_desktop_config
-
-  echo "run desktop frontend"
-  sealos run tars/frontend-desktop.tar \
-    --env cloudDomain=$cloudDomain \
-    --env certSecretName="wildcard-cert" \
-    --env passwordEnabled="true" \
-    --config-file etc/sealos/desktop-config.yaml
-
-  echo "run applaunchpad frontend"
-  sealos run tars/frontend-applaunchpad.tar \
-  --env cloudDomain=$cloudDomain \
-  --env certSecretName="wildcard-cert"
-
-  echo "run terminal frontend"
-  sealos run tars/frontend-terminal.tar \
-  --env cloudDomain=$cloudDomain \
-  --env certSecretName="wildcard-cert"
-
-  echo "run dbprovider frontend"
-  sealos run tars/frontend-dbprovider.tar \
-  --env cloudDomain=$cloudDomain \
-  --env certSecretName="wildcard-cert"
-
-  echo "costcenter frontend"
-  sealos run tars/cost-center.tar \
-  --env cloudDomain=$cloudDomain \
-  --env certSecretName="wildcard-cert" \
-  --env transferEnabled="true" \
-  --env rechargeEnabled="false"
-}
-
-
-function mutate_desktop_config() {
-  # mutate etc/sealos/desktop-config.yaml by using mongodb uri and two random base64 string
-  sed -i -e "s;<your-mongodb-uri-base64>;$(echo -n "$mongodbUri" | base64 -w 0);" etc/sealos/desktop-config.yaml
-  sed -i -e "s;<your-jwt-secret-base64>;$(tr -cd 'a-z0-9' </dev/urandom | head -c64 | base64 -w 0);" etc/sealos/desktop-config.yaml
-  sed -i -e "s;<your-password-salt-base64>;$saltKey;" etc/sealos/desktop-config.yaml
-}
-
-
 
 function install {
-  # read env
-  read_env etc/sealos/cloud.env
-
-  # kubectl apply namespace, secret and mongodb
-  kubectl apply -f manifests/namespace.yaml
-
-  # apply notifications crd
-  kubectl apply -f manifests/notifications_crd.yaml
-
-  # create tls secret
-  create_tls_secret $cloudDomain
-
-  # gen mongodb uri
-  gen_mongodbUri
-
-  # gen saltKey
-  gen_saltKey
+  # gen mongodb uri and others
+  prepare
 
   # sealos run controllers
   sealos_run_controller
