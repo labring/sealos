3e00d3f26238d96338829e390a24a5f0044732d0
ElonMuskkkkkk <yxxchange@163.com>
2023-08-15 12:10:09 +0800

    Decoupled pre-registration operations

---
+++ controllers/licenseissuer/launcher/main.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	if err := run("/preset"); err != nil {
+		log.Fatalf("Failed to run /preset: %v", err)
+	}
+
+	if err := run("/manager"); err != nil {
+		log.Fatalf("Failed to run /manager: %v", err)
+	}
+}
+
+func run(path string) error {
+	cmd := exec.Command(path)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
