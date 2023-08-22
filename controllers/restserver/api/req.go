37fd64e5224351ddbdda44f80ba68978b89abf92
sakcer <1514333214@qq.com>
2023-08-22 21:22:04 +0800

    add: restserver for database monitor

---
+++ controllers/restserver/api/req.go
@@ -0,0 +1,136 @@
+package api
+
+import "errors"
+
+type PromRequest struct {
+	User    string
+	Pwd     string
+	NS      string
+	Type    string
+	Query   string
+	Cluster string
+	Range   PromRange
+	// Match_old PromMatch
+}
+
+type PromMetric struct {
+}
+
+// type PromMatch struct {
+// 	Ns      string `mapstructure:"namespace"`
+// 	Po      string `mapstructure:"pod"`
+// 	Inst    string `mapstructure:"instance"`
+// 	Datname string `mapstructure:"datname"`
+// 	Cluster string `mapstructure:"app_kubernetes_io_instance"`
+// }
+
+type PromRange struct {
+	Start string `mapstructure:"start"`
+	End   string `mapstructure:"end"`
+	Step  string `mapstructure:"step"`
+	Time  string `mapstructure:"time"`
+}
+
+type QueryResult struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Metric map[string]string `json:"metric"`
+			Values [][]interface{}   `json:"values"`
+			Value  []interface{}     `json:"value"`
+		} `json:"result"`
+	} `json:"data"`
+}
+
+var (
+	ErrNoAuth          = errors.New("no permission for this namespace")
+	ErrNoSealosHost    = errors.New("unable to get the sealos host")
+	ErrNoPromHost      = errors.New("unable to get the prometheus host")
+	ErrUncompleteParam = errors.New("at least provide both namespace and query")
+	ErrEmptyKubeconfig = errors.New("empty kubeconfig")
+	ErrNilNs           = errors.New("namespace not found")
+)
+
+var (
+	Mysql = map[string]string{
+		"cpu":           "round(max by (pod) (rate(container_cpu_usage_seconds_total{namespace=~\"#\",pod=~\"@-mysql-\\\\d\",container=\"mysql\" }[5m])) / on (pod) (max by (pod) (container_spec_cpu_quota{namespace=~\"#\", pod=~\"@-mysql-\\\\d\",container=\"mysql\"} / 100000)) * 100,0.01)",
+		"memory":        "round(max by (pod)(container_memory_usage_bytes{namespace=~\"#\",pod=~\"@-mysql-\\\\d\",container=\"mysql\"})/ on (pod) (max by (pod) (container_spec_memory_limit_bytes{namespace=~\"#\", pod=~\"@-mysql-\\\\d\", container=\"mysql\"})) * 100,0.01)",
+		"disk_capacity": "(max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_capacity_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-mysql-\\\\d\"}))",
+		"disk_used":     "(max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_used_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-mysql-\\\\d\"}))",
+		"disk":          "round((max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_used_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-mysql-\\\\d\"})) / (max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_capacity_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-mysql-\\\\d\"})) * 100, 0.01)",
+		// "disk": "max by (persistentvolumeclaim,namespace,type) (label_replace(kubelet_volume_stats_capacity_bytes{namespace=~\"#\", persistentvolumeclaim=~\"data-@-mysql-\\\\d\"},\"type\",\"capacity\",\"__name__\", \"(.+)\")) or max by (persistentvolumeclaim,namespace,type)  (label_replace(kubelet_volume_stats_used_bytes{namespace=~\"#\", persistentvolumeclaim=~\"data-@-mysql-\\\\d\"},\"type\",\"used\",\"__name__\", \"(.+)\"))",
+		"uptime":      "sum(mysql_global_status_uptime{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}) by (namespace,app_kubernetes_io_instance,pod)",
+		"connections": "sum(max_over_time(mysql_global_status_threads_connected{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])) by (namespace,app_kubernetes_io_instance,pod)",
+		"commands":    "topk(5, rate(mysql_global_status_commands_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]) > 0)",
+
+		"innodb":       "sum(mysql_global_variables_innodb_buffer_pool_size{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}) by (namespace,app_kubernetes_io_instance,pod)",
+		"slow_queries": "sum(rate(mysql_global_status_slow_queries{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])) by (namespace,app_kubernetes_io_instance,pod)",
+
+		// "hits_ratio": "(rate(mysql_global_status_table_open_cache_hits{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[5m]))/((rate(mysql_global_status_table_open_cache_hits{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[5m]))+(rate(mysql_global_status_table_open_cache_misses{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[5m])))"
+
+		"aborted_conenctions": "sum(rate(mysql_global_status_aborted_connects{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])) by (namespace,app_kubernetes_io_instance,pod)",
+		"table_locks":         "sum(rate(mysql_global_status_table_locks_immediate{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])) by (namespace,app_kubernetes_io_instance,pod)",
+	}
+	Pgsql = map[string]string{
+		// "cpu": "round(max by (pod) (rate(container_cpu_usage_seconds_total{namespace=~\"#\",pod=~\"@-postgresql-\\\\d\" ,container=\"postgresql\"}[5m])) / on (pod) (max by (pod) (kube_pod_container_resource_limits{namespace=~\"#\", pod=~\"@-postgresql-\\\\d\", resource=\"cpu\",container=\"postgresql\"})) * 100,0.01)",
+		"cpu": "round(max by (pod) (rate(container_cpu_usage_seconds_total{namespace=~\"#\",pod=~\"@-postgresql-\\\\d\" ,container=\"postgresql\"}[5m])) / on (pod) (max by (pod) (container_spec_cpu_quota{namespace=~\"#\", pod=~\"@-postgresql-\\\\d\",container=\"postgresql\"} / 100000)) * 100,0.01)",
+
+		"memory": "round(max by (pod)(container_memory_usage_bytes{namespace=~\"#\",pod=~\"@-postgresql-\\\\d\",container=\"postgresql\" })/ on (pod) (max by (pod) (container_spec_memory_limit_bytes{namespace=~\"#\", pod=~\"@-postgresql-\\\\d\", container=\"postgresql\"})) * 100,0.01)",
+		"disk":   "round((max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_used_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-postgresql-\\\\d\"})) / (max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_capacity_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-postgresql-\\\\d\"})) * 100, 0.01)",
+		// "disk": "max by (persistentvolumeclaim,namespace,type) (label_replace(kubelet_volume_stats_capacity_bytes{namespace=~\"#\", persistentvolumeclaim=~\"data-@-postgresql-\\\\d\"},\"type\",\"capacity\",\"__name__\", \"(.+)\")) or max by (persistentvolumeclaim,namespace,type)  (label_replace(kubelet_volume_stats_used_bytes{namespace=~\"#\", persistentvolumeclaim=~\"data-@-postgresql-\\\\d\"},\"type\",\"used\",\"__name__\", \"(.+)\"))",
+		"disk_capacity": "(max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_capacity_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-postgresql-\\\\d\"}))",
+		"disk_used":     "(max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_used_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-postgresql-\\\\d\"}))",
+		"uptime":        "avg (time() - pg_postmaster_start_time_seconds{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}) by(namespace, app_kubernetes_io_instance, pod)",
+		"connections":   "sum(pg_stat_database_numbackends{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"})",
+		"commands":      "sum by (command,app_kubernetes_io_instance)(label_replace(rate(pg_stat_database_tup_deleted{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]),\"command\",\"delete\",\"namespace\",\"(.*)\")) or sum by (command,app_kubernetes_io_instance)(label_replace(rate(pg_stat_database_tup_inserted{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]),\"command\",\"insert\",\"namespace\",\"(.*)\")) or sum by (command,app_kubernetes_io_instance)(label_replace(rate(pg_stat_database_tup_fetched{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]),\"command\",\"fetch\",\"namespace\",\"(.*)\")) or sum by (command,app_kubernetes_io_instance)(label_replace(rate(pg_stat_database_tup_returned{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]),\"command\",\"return\",\"namespace\",\"(.*)\")) or sum by (command,app_kubernetes_io_instance)(label_replace(rate(pg_stat_database_tup_updated{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]),\"command\",\"update\",\"namespace\",\"(.*)\"))",
+
+		"db_size": "pg_database_size_bytes{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}",
+
+		// "hits_ratio": "round(100*sum(pg_stat_database_blks_hit{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}) by (namespace, app_kubernetes_io_instance)/ (sum by (namespace, app_kubernetes_io_instance) (pg_stat_database_blks_hit{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}) + sum by (namespace, app_kubernetes_io_instance) (pg_stat_database_blks_read{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"})) ,0.1)"
+
+		"active_connections": " pg_stat_activity_count{namespace=~\"#\", app_kubernetes_io_instance=~\"@\",state=\"active\"}",
+		// "commits": "label_replace(rate (pg_stat_database_xact_rollback{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]) ,\"command\",\"rollback\", \"namespace\",\"(.+)\") or label_replace(rate (pg_stat_database_xact_commit{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]) ,\"command\",\"commit\", \"namespace\",\"(.+)\")",
+		"rollbacks":        "rate (pg_stat_database_xact_rollback{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])",
+		"commits":          "rate (pg_stat_database_xact_commit{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])",
+		"tx_duration":      "max without(state) (max_over_time(pg_stat_activity_max_tx_duration{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]))",
+		"block_read_time":  "rate(pg_stat_database_blk_read_time{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])",
+		"block_write_time": "rate(pg_stat_database_blk_write_time{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])",
+	}
+
+	Mongo = map[string]string{
+		"cpu":           "round(max by (pod) (rate(container_cpu_usage_seconds_total{namespace=~\"#\",pod=~\"@-mongodb-\\\\d\" ,container=\"mongodb\" }[5m])) / on (pod) (max by (pod) (container_spec_cpu_quota{namespace=~\"#\", pod=~\"@-mongodb-\\\\d\" ,container=\"mongodb\"} / 100000)) * 100,0.01)",
+		"memory":        "round(max by (pod)(container_memory_usage_bytes{namespace=~\"#\",pod=~\"@-mongodb-\\\\d\"  ,container=\"mongodb\"})/ on (pod) (max by (pod) (container_spec_memory_limit_bytes{namespace=~\"#\", pod=~\"@-mongodb-\\\\d\",container=\"mongodb\"})) * 100,0.01)",
+		"disk_capacity": "(max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_capacity_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-mongodb-\\\\d\"}))",
+		"disk":          "round((max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_used_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-mongodb-\\\\d\"})) / (max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_capacity_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-mongodb-\\\\d\"})) * 100, 0.01)",
+		// "disk": "max by (persistentvolumeclaim,namespace,type) (label_replace(kubelet_volume_stats_capacity_bytes{namespace=~\"#\", persistentvolumeclaim=~\"data-@-mongodb-\\\\d\"},\"type\",\"capacity\",\"__name__\", \"(.+)\")) or max by (persistentvolumeclaim,namespace,type)  (label_replace(kubelet_volume_stats_used_bytes{namespace=~\"#\", persistentvolumeclaim=~\"data-@-mongodb-\\\\d\"},\"type\",\"used\",\"__name__\", \"(.+)\"))",
+		"disk_used":   "(max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_used_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-mongodb-\\\\d\"}))",
+		"uptime":      "sum by(namespace, app_kubernetes_io_instance, pod) (mongodb_instance_uptime_seconds{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"})",
+		"connections": "mongodb_connections{namespace=~\"#\", app_kubernetes_io_instance=~\"@\", state=~\"current\"}",
+		"commands":    "label_replace(rate(mongodb_op_counters_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\", type!=\"command\"}[1m])  or irate(mongodb_op_counters_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\", type!=\"command\"}[1m]), \"command\", \"$1\", \"type\", \"(.*)\")",
+
+		"db_size": "mongodb_dbstats_dataSize{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}",
+
+		"document_ops": "rate(mongodb_mongod_metrics_document_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])",
+		"pg_faults":    "rate(mongodb_extra_info_page_faults_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m]) or irate(mongodb_extra_info_page_faults_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])",
+	}
+
+	Redis = map[string]string{
+		"cpu":           "round(max by (pod) (rate(container_cpu_usage_seconds_total{namespace=~\"#\",pod=~\"@-redis-\\\\d\" ,container=\"redis\"}[5m])) / on (pod) (max by (pod) (container_spec_cpu_quota{namespace=~\"#\", pod=~\"@-redis-\\\\d\",container=\"redis\"} / 100000)) * 100,0.01)",
+		"memory":        "round(max by (pod)(container_memory_usage_bytes{namespace=~\"#\",pod=~\"@-redis-\\\\d\",container=\"redis\" })/ on (pod) (max by (pod) (container_spec_memory_limit_bytes{namespace=~\"#\", pod=~\"@-redis-\\\\d\",container=\"redis\"})) * 100,0.01)",
+		"disk_capacity": "(max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_capacity_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-redis-\\\\d\"}))",
+		"disk":          "round((max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_used_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-redis-\\\\d\"})) / (max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_capacity_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-redis-\\\\d\"})) * 100, 0.01)",
+		// "disk": "max by (persistentvolumeclaim,namespace,type) (label_replace(kubelet_volume_stats_capacity_bytes{namespace=~\"#\", persistentvolumeclaim=~\"data-@-redis-\\\\d\"},\"type\",\"capacity\",\"__name__\", \"(.+)\")) or max by (persistentvolumeclaim,namespace,type)  (label_replace(kubelet_volume_stats_used_bytes{namespace=~\"#\", persistentvolumeclaim=~\"data-@-redis-\\\\d\"},\"type\",\"used\",\"__name__\", \"(.+)\"))",
+		"disk_used":   "(max by (persistentvolumeclaim,namespace) (kubelet_volume_stats_used_bytes {namespace=~\"#\", persistentvolumeclaim=~\"data-@-redis-\\\\d\"}))",
+		"uptime":      "redis_uptime_in_seconds{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}",
+		"connections": "sum(redis_connected_clients{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"})",
+		"commands":    "label_replace(sum(irate(redis_commands_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"} [1m])) by (cmd, namespace, app_kubernetes_io_instance), \"command\", \"$1\", \"cmd\", \"(.*)\")",
+
+		"db_items": "sum (redis_db_keys{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}) by (db)",
+
+		"hits_ratio":          "avg(rate(redis_keyspace_hits_total{namespace=~\"#\",app_kubernetes_io_instance=\"@\"}[1m]) / clamp_min((irate(redis_keyspace_misses_total{namespace=~\"#\",app_kubernetes_io_instance=~\"@\"}[1m]) + irate(redis_keyspace_hits_total{namespace=~\"#\",app_kubernetes_io_instance=\"@\"}[1m])), 0.01)) by (pod, app_kubernetes_io_instance)",
+		"commands_duration":   "avg(rate(redis_commands_duration_seconds_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])) by (cmd) / avg(irate(redis_commands_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])) by (cmd)",
+		"blocked_connections": "sum(redis_blocked_clients{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"})",
+		"key_evictions":       "irate(redis_evicted_keys_total{namespace=~\"#\", app_kubernetes_io_instance=~\"@\"}[1m])",
+	}
+)
---
+++ controllers/restserver/server/server.go
@@ -0,0 +1,215 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/labring/sealos/controllers/restserver/api"
+	"github.com/labring/sealos/controllers/restserver/auth"
+	"github.com/labring/sealos/controllers/restserver/request"
+)
+
+type PromServer struct {
+	Config *Config
+}
+
+func NewPromServer(c *Config) (*PromServer, error) {
+	ps := &PromServer{
+		Config: c,
+	}
+	return ps, nil
+}
+
+func (ps *PromServer) Authenticate(errChan chan<- error, pr *api.PromRequest) {
+	if err := auth.Authenticate(pr.NS, pr.Pwd); err != nil {
+		errChan <- err
+		return
+	}
+	errChan <- nil
+}
+
+func (ps *PromServer) Request(resChan chan<- *api.QueryResult, errChan chan<- error, pr *api.PromRequest) {
+	body, err := request.PrometheusPre(pr)
+	if err != nil {
+		errChan <- err
+		return
+	}
+	var result *api.QueryResult
+	if err := json.Unmarshal(body, &result); err != nil {
+		errChan <- err
+		return
+	}
+	errChan <- err
+	resChan <- result
+}
+
+func (ps *PromServer) DBReq(resChan chan<- *api.QueryResult, errChan chan<- error, pr *api.PromRequest) {
+	body, err := request.PrometheusNew(pr)
+	if err != nil {
+		errChan <- err
+		return
+	}
+	var result *api.QueryResult
+	if err := json.Unmarshal(body, &result); err != nil {
+		errChan <- err
+		return
+	}
+	errChan <- err
+	resChan <- result
+}
+
+func (ps *PromServer) ParseRequest(req *http.Request) (*api.PromRequest, error) {
+	pr := &api.PromRequest{}
+
+	auth := req.Header.Get("Authorization")
+
+	if pwd, err := url.PathUnescape(auth); err == nil {
+		pr.Pwd = pwd
+	} else {
+		return nil, err
+	}
+	if pr.Pwd == "" {
+		return nil, api.ErrEmptyKubeconfig
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, err
+	}
+
+	for key, val := range req.Form {
+		switch key {
+		case "query":
+			pr.Query = val[0]
+		case "step":
+			pr.Range.Step = val[0]
+		case "start":
+			pr.Range.Start = val[0]
+		case "end":
+			pr.Range.End = val[0]
+		case "time":
+			pr.Range.Time = val[0]
+		case "namespace":
+			pr.NS = val[0]
+		case "type":
+			pr.Type = val[0]
+		case "app":
+			pr.Cluster = val[0]
+		}
+	}
+
+	if pr.NS == "" || pr.Query == "" {
+		return nil, api.ErrUncompleteParam
+	}
+
+	return pr, nil
+}
+
+// 获取客户端请求的信息
+func (ps *PromServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	pathPrefix := ""
+	switch {
+	case req.URL.Path == pathPrefix+"/query": // 将废弃
+		ps.doReqPre(rw, req)
+	case req.URL.Path == pathPrefix+"/q":
+		ps.doReqNew(rw, req)
+	default:
+		http.Error(rw, "Not found", http.StatusNotFound)
+		return
+	}
+}
+
+func (ps *PromServer) doReqNew(rw http.ResponseWriter, req *http.Request) {
+	pr, err := ps.ParseRequest(req)
+	if err != nil {
+		http.Error(rw, fmt.Sprintf("Bad request (%s)", err), http.StatusBadRequest)
+		log.Printf("Bad request (%s)\n", err)
+		return
+	}
+
+	resChan := make(chan *api.QueryResult)
+	errChan1 := make(chan error)
+	errChan2 := make(chan error)
+
+	go ps.Authenticate(errChan1, pr)
+	go ps.DBReq(resChan, errChan2, pr)
+
+	err1 := <-errChan1
+	err2 := <-errChan2
+	res := <-resChan
+
+	if err1 != nil {
+		http.Error(rw, fmt.Sprintf("Authentication failed (%s)", err1), http.StatusInternalServerError)
+		log.Printf("Authentication failed (%s)\n", err1)
+		return
+	}
+	if err2 != nil {
+		http.Error(rw, fmt.Sprintf("Query failed (%s)", err2), http.StatusInternalServerError)
+		log.Printf("Query failed (%s)\n", err2)
+		return
+	}
+
+	result, err := json.Marshal(res)
+	if err != nil {
+		http.Error(rw, "Result failed (invalid query expression)", http.StatusInternalServerError)
+		log.Printf("Reulst failed (%s)\n", err)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	n, err := rw.Write(result)
+	if err != nil {
+		log.Printf("Reulst failed(%d): %s\n", n, err)
+		http.Error(rw, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (ps *PromServer) doReqPre(rw http.ResponseWriter, req *http.Request) {
+	pr, err := ps.ParseRequest(req)
+	if err != nil {
+		http.Error(rw, fmt.Sprintf("Bad request (%s)", err), http.StatusBadRequest)
+		log.Printf("Bad request (%s)\n", err)
+		return
+	}
+
+	resChan := make(chan *api.QueryResult)
+	errChan1 := make(chan error)
+	errChan2 := make(chan error)
+
+	go ps.Authenticate(errChan1, pr)
+	go ps.Request(resChan, errChan2, pr)
+
+	err1 := <-errChan1
+	err2 := <-errChan2
+	res := <-resChan
+
+	if err1 != nil {
+		http.Error(rw, fmt.Sprintf("Authentication failed (%s)", err1), http.StatusInternalServerError)
+		log.Printf("Authentication failed (%s)\n", err1)
+		log.Printf("Kubeconfig (%s)\n", pr.Pwd)
+		return
+	}
+	if err2 != nil {
+		http.Error(rw, fmt.Sprintf("Query failed (%s)", err2), http.StatusInternalServerError)
+		log.Printf("Query failed (%s)\n", err2)
+		return
+	}
+
+	result, err := json.Marshal(res)
+	if err != nil {
+		http.Error(rw, "Result failed (invalid query expression)", http.StatusInternalServerError)
+		log.Printf("Reulst failed (%s)\n", err)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	n, err := rw.Write(result)
+	if err != nil {
+		log.Printf("Reulst failed(%d): %s\n", n, err)
+		http.Error(rw, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
