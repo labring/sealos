c38ecd607ef9e2a31ac071d9150e6264d070a2a8
archer <545436317@qq.com>
2023-08-31 10:37:30 +0800

    error logs

--- frontend/packages/client-sdk/src/utils/logger.ts
+++ frontend/packages/client-sdk/src/utils/logger.ts
@@ -4,16 +4,13 @@ export function formatTime(date: Date) {
   return dayjs(date).format('YYYY-MM-DD HH:mm:ss');
 }
 
-function sanitizeMsg(msg: string) {
-  return msg.replace(/%[sdj%]/g, '');
-}
-
 export function infoLog(msg: string, obj: Record<string, any> = {}) {
-  console.log(`[INFO] ${formatTime(new Date())} ${sanitizeMsg(msg)}`, obj);
+  console.log(`[INFO] ${formatTime(new Date())} ${msg}`, obj);
 }
 
 export function errLog(msg: string, error: any) {
-  console.log(`[ERROR] ${formatTime(new Date())}`, {
+  console.log(`[ERROR] ${formatTime(new Date())}`);
+  console.log({
     message: msg,
     stack: error?.stack,
     ...(error?.config && {
@@ -36,5 +33,5 @@ export function errLog(msg: string, error: any) {
 }
 
 export function warnLog(msg: string, obj: Record<string, any> = {}) {
-  console.log(`[WARN] ${formatTime(new Date())} ${sanitizeMsg(msg)}`, obj);
+  console.log(`[WARN] ${formatTime(new Date())} ${msg}`, obj);
 }
