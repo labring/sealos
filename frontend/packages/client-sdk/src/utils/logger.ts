3966c7455acaba613533c0e310f56a49234abbd0
archer <545436317@qq.com>
2023-08-31 10:59:57 +0800

    error logs

--- frontend/packages/client-sdk/src/utils/logger.ts
+++ frontend/packages/client-sdk/src/utils/logger.ts
@@ -5,32 +5,13 @@ export function formatTime(date: Date) {
 }
 
 export function infoLog(msg: string, obj: Record<string, any> = {}) {
-  console.log(`[INFO] ${formatTime(new Date())} ${msg}`, obj);
+  console.log(`[INFO] %s %s`, formatTime(new Date()), msg, JSON.stringify(obj));
 }
 
 export function errLog(msg: string, error: any) {
-  console.log(`[ERROR] ${formatTime(new Date())}`, msg);
-  console.log({
-    stack: error?.stack,
-    ...(error?.config && {
-      config: {
-        headers: error.config.headers,
-        url: error.config.url,
-        data: error.config.data
-      }
-    }),
-    ...(error?.body && {
-      body: error?.body
-    }),
-    ...(error?.response && {
-      response: {
-        status: error.response.status,
-        statusText: error.response.statusText
-      }
-    })
-  });
+  console.log(`[ERROR] %s %s %s`, formatTime(new Date()), msg, JSON.stringify(error));
 }
 
 export function warnLog(msg: string, obj: Record<string, any> = {}) {
-  console.log(`[WARN] ${formatTime(new Date())} ${msg}`, obj);
+  console.log(`[WARN] %s %s %s`, formatTime(new Date()), msg, JSON.stringify(obj));
 }
