e17f8eb4a4abead60a15c4df7a1706bb82b896b6
yy <lingdie.yy@outlook.com>
2023-09-07 11:53:06 +0800

    fix lint

--- controllers/user/controllers/suite_test.go
+++ controllers/user/controllers/suite_test.go
@@ -35,7 +35,6 @@ import (
 
 	v1 "github.com/labring/sealos/controllers/user/api/v1"
 
-	userv1 "github.com/labring/sealos/controllers/user/api/v1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	//+kubebuilder:scaffold:imports
@@ -74,9 +73,6 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 
 	err = v1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	err = userv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
