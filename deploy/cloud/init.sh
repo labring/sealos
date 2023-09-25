21909a1d396bd473cf64fad4829a7253c012497b
ElonMuskkkkkk <yxxchange@163.com>
2023-09-25 12:48:28 +0800

    modify deploy config temp for poc test

--- deploy/cloud/init.sh
+++ deploy/cloud/init.sh
@@ -12,8 +12,9 @@ sealos pull --policy=always --platform=linux/"${ARCH}" ghcr.io/labring/sealos-cl
 sealos pull --policy=always --platform=linux/"${ARCH}" ghcr.io/labring/sealos-cloud-resources-controller:latest
 sealos pull --policy=always --platform=linux/"${ARCH}" ghcr.io/labring/sealos-cloud-account-controller:latest
 sealos pull --policy=always --platform=linux/"${ARCH}" ghcr.io/labring/sealos-cloud-costcenter-frontend:latest
-sealos pull --policy=always --platform=linux/"${ARCH}" ghcr.io/labring/sealos-cloud-licenseissuer-controller:latest
+#sealos pull --policy=always --platform=linux/"${ARCH}" ghcr.io/labring/sealos-cloud-licenseissuer-controller:latest
 sealos pull --policy=always --platform=linux/"${ARCH}" ghcr.io/labring/sealos-cloud-template-frontend:latest
+sealos pull --policy=always --platform=linux/"${ARCH}" ghcr.io/yxxchange/sealos-cloud-licenseissuer-controller:latest
 
 sealos save -o tars/user.tar ghcr.io/labring/sealos-cloud-user-controller:latest
 sealos save -o tars/terminal.tar ghcr.io/labring/sealos-cloud-terminal-controller:latest
@@ -21,7 +22,8 @@ sealos save -o tars/app.tar ghcr.io/labring/sealos-cloud-app-controller:latest
 sealos save -o tars/metering.tar ghcr.io/labring/sealos-cloud-resources-metering-controller:latest
 sealos save -o tars/monitoring.tar ghcr.io/labring/sealos-cloud-resources-controller:latest
 sealos save -o tars/account.tar ghcr.io/labring/sealos-cloud-account-controller:latest
-sealos save -o tars/licenseissuer.tar ghcr.io/labring/sealos-cloud-licenseissuer-controller:latest
+#sealos save -o tars/licenseissuer.tar ghcr.io/labring/sealos-cloud-licenseissuer-controller:latest
+sealos save -o tars/licenseissuer.tar ghcr.io/yxxchange/sealos-cloud-licenseissuer-controller:latest
 
 sealos save -o tars/frontend-desktop.tar  ghcr.io/labring/sealos-cloud-desktop-frontend:latest
 sealos save -o tars/frontend-terminal.tar  ghcr.io/labring/sealos-cloud-terminal-frontend:latest
