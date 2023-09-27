ae3535eaeb61e80d77ece6ddddd7ab7f50468d38
jingyang <3161362058@qq.com>
2023-09-27 10:23:58 +0800

    feat:license deploy
    Signed-off-by: jingyang <3161362058@qq.com>

--- frontend/providers/license/src/pages/api/platform/getEnv.ts
+++ frontend/providers/license/src/pages/api/platform/getEnv.ts
@@ -6,13 +6,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export type Response = {
   domain?: string;
   hid: string; // PASSWORD_SALT
+  LICENSE_DOMAIN: string;
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResp>) {
   jsonRes<Response>(res, {
     data: {
       domain: process.env.SEALOS_DOMAIN || 'cloud.sealos.io',
-      hid: hashCrypto(process.env.PASSWORD_SALT || '')
+      hid: hashCrypto(process.env.PASSWORD_SALT || ''),
+      LICENSE_DOMAIN: process.env.LICENSE_DOMAIN || 'cloud.sealos.io'
     }
   });
 }
