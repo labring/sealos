d831b727f39e3522a6950de5132ee0d9bfb7ba1e
yy <lingdie.yy@outlook.com>
2023-10-18 19:04:41 +0800

    fix meta convent.

--- controllers/license/internal/util/meta/meta.go
+++ controllers/license/internal/util/meta/meta.go
@@ -3,7 +3,6 @@ package meta
 import (
 	licensev1 "github.com/labring/sealos/controllers/license/api/v1"
 	claimsutil "github.com/labring/sealos/controllers/license/internal/util/claims"
-	errors "github.com/labring/sealos/controllers/license/internal/util/errors"
 	licenseutil "github.com/labring/sealos/controllers/license/internal/util/license"
 )
 
@@ -16,17 +15,13 @@ type Meta struct {
 }
 
 func New(license *licensev1.License) (*Meta, error) {
-	token, err := licenseutil.ParseLicenseToken(license)
+	claims, err := licenseutil.GetClaims(license)
 	if err != nil {
 		return nil, err
-	}
-	claims, ok := token.Claims.(claimsutil.Claims)
-	if !ok {
-		return nil, errors.ClaimsConventError
 	}
 	return &Meta{
 		Token:          license.Spec.Token,
 		ActivationTime: license.Status.ActivationTime.Format("2006-01-02 15:04:05"),
-		Claims:         claims,
+		Claims:         *claims,
 	}, nil
 }
