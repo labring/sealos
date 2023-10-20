3a87a7f9ddec3b9ca4ba7be17348f852a8102dc2
yy <lingdie.yy@outlook.com>
2023-10-20 19:41:47 +0800

    fix login error, user label.
    Signed-off-by: yy <lingdie.yy@outlook.com>

--- controllers/job/init/internal/util/controller/user.go
+++ controllers/job/init/internal/util/controller/user.go
@@ -2,13 +2,14 @@ package controller
 
 import (
 	"context"
+	"time"
+
+	"github.com/labring/sealos/controllers/job/init/internal/util/common"
+	userv1 "github.com/labring/sealos/controllers/user/api/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-
-	userv1 "github.com/labring/sealos/controllers/user/api/v1"
-
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -41,7 +42,12 @@ func PresetAdminUser(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	_, err = ctrl.CreateOrUpdate(ctx, c, &userv1.User{ObjectMeta: ctrl.ObjectMeta{Name: DefaultAdminUserName}}, func() error { return nil })
+	_, err = ctrl.CreateOrUpdate(ctx, c, &userv1.User{
+		ObjectMeta: ctrl.ObjectMeta{
+			Name:   DefaultAdminUserName,
+			Labels: map[string]string{"uid": common.AdminUID(), "updateTime": time.Now().Format(time.RFC3339)},
+		},
+	}, func() error { return nil })
 	if err != nil {
 		return err
 	}
