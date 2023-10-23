e6128527d1a986750ab3f1dde7eb95087564f2f8
zhujingyang <72259332+zjy365@users.noreply.github.com>
2023-10-24 00:14:52 +0800

    fix docs jump link (#4151)
    Signed-off-by: jingyang <3161362058@qq.com>

--- docs/website/src/pages/components/Banner/index.tsx
+++ docs/website/src/pages/components/Banner/index.tsx
@@ -17,7 +17,8 @@ export default function Banner() {
   };
 
   useEffect(() => {
-    setDoMain(window?.location?.hostname || 'cloud.sealos.io')
+    let url = window?.location?.hostname || 'cloud.sealos.io'
+    setDoMain(url.indexOf('top') !== -1 ?'cloud.sealos.top':'cloud.sealos.io')
     // Get the last display timestamp from localStorage
     const lastDisplayTimestamp = localStorage.getItem('bannerLastDisplay');
     const today = new Date().toLocaleDateString();
@@ -45,6 +46,13 @@ export default function Banner() {
           <div className="title">
             <LogoIcon width={'42px'} height={'42px'} />
             <span className="txt">Sealos</span>
+            <svg 
+              fill='#FFF'
+              className='sealos-banner-btn-close'
+              onClick={closeBanner}
+              xmlns="http://www.w3.org/2000/svg" width="32" height="33" viewBox="0 0 32 33">
+              <path d="M16.3536 18.1531L16.0001 17.7995L15.6465 18.1531L9.11318 24.6864C8.97166 24.8279 8.79211 24.9066 8.5334 24.9066C8.27469 24.9066 8.09513 24.8279 7.95362 24.6864C7.8121 24.5449 7.7334 24.3653 7.7334 24.1066C7.7334 23.8479 7.8121 23.6684 7.95362 23.5268L14.487 16.9935L14.8405 16.64L14.487 16.2864L7.95362 9.75307C7.8121 9.61156 7.7334 9.43201 7.7334 9.17329C7.7334 8.91458 7.8121 8.73502 7.95362 8.59351C8.09513 8.452 8.27468 8.37329 8.5334 8.37329C8.79211 8.37329 8.97166 8.452 9.11318 8.59351L15.6465 15.1268L16.0001 15.4804L16.3536 15.1268L22.887 8.59351C23.0285 8.452 23.208 8.37329 23.4667 8.37329C23.7254 8.37329 23.905 8.452 24.0465 8.59351C24.188 8.73503 24.2667 8.91458 24.2667 9.17329C24.2667 9.432 24.188 9.61156 24.0465 9.75307L17.5132 16.2864L17.1596 16.64L17.5132 16.9935L24.0465 23.5268C24.188 23.6684 24.2667 23.8479 24.2667 24.1066C24.2667 24.3653 24.188 24.5449 24.0465 24.6864C23.905 24.8279 23.7254 24.9066 23.4667 24.9066C23.208 24.9066 23.0285 24.8279 22.887 24.6864L16.3536 18.1531Z"/>
+            </svg>
           </div>
           <div className="sealos-banner-body">
             <div className="banner-title">🎉 10.24 双十一双节降临！</div>
