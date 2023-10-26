579a0163baae48f4547b394530e8bc31196da8ef
sealos-ci-robot <109538726+sealos-ci-robot@users.noreply.github.com>
2023-10-26 14:40:26 +0800

    refactor(main): skip success exec (#4162) (#4170) (#4175)
    Co-authored-by: cuisongliu <cuisongliu@qq.com>

--- pkg/types/v1beta1/cluster.go
+++ pkg/types/v1beta1/cluster.go
@@ -172,16 +172,6 @@ type CommandCondition struct {
 	Message string `json:"message,omitempty"`
 }
 
-func NewSuccessCommandCondition() CommandCondition {
-	return CommandCondition{
-		Type:              CommandConditionTypeSuccess,
-		Status:            v1.ConditionTrue,
-		LastHeartbeatTime: metav1.Now(),
-		Reason:            "Apply Command",
-		Message:           "Applied to cluster successfully",
-	}
-}
-
 func NewFailedCommandCondition(message string) CommandCondition {
 	return CommandCondition{
 		Type:              CommandConditionTypeError,
