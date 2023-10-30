5b32f51febe3c0202c58aa22fc2a08f78985e3e2
jingyang <3161362058@qq.com>
2023-10-30 16:42:10 +0800

    fix
    Signed-off-by: jingyang <3161362058@qq.com>

--- service/license/src/pages/pricing/components/Product.tsx
+++ service/license/src/pages/pricing/components/Product.tsx
@@ -60,7 +60,7 @@ export default function Product() {
   const openPayModal = () => {
     setComplete(1);
     setPayType('wechat');
-    paymentMutation.mutate((1 * 100).toString());
+    paymentMutation.mutate((599 * 100).toString());
     onOpen();
   };
 
