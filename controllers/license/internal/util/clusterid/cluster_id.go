751b0ee986e0b8002b54d310d8728669ff9ff7b0
yy <lingdie.yy@outlook.com>
2023-11-07 17:30:49 +0800

    use kube-system ns uid as cluster id.
    Signed-off-by: yy <lingdie.yy@outlook.com>

--- controllers/license/internal/util/clusterid/cluster_id.go
+++ controllers/license/internal/util/clusterid/cluster_id.go
@@ -1,24 +1,17 @@
 package clusterid
 
-//import (
-//	"context"
-//	"os"
-//
-//	"github.com/labring/sealos/controllers/license/internal/util/database"
-//)
-//
-//func GetClusterID(ctx context.Context, db *database.DataBase) (string, error) {
-//	// TODO get cluster id from database
-//	//id, err := db.GetClusterID(ctx)
-//	//if errors.Is(err, mongo.ErrNoDocuments) {
-//	//	// if cluster id is not set, set it
-//	//	envID := os.Getenv("CLUSTER_ID")
-//	//	if envID == "" {
-//	//		return "", errors.New("cluster id not set")
-//	//	}
-//	//	if err := db.StoreClusterID(ctx, os.Getenv("CLUSTER_ID")); err != nil {
-//	//		return db.GetClusterID(ctx)
-//	//	}
-//	//}
-//	return os.Getenv("CLUSTER_ID"), nil
-//}
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func GetClusterID(ctx context.Context, c client.Client) (string, error) {
+	ns := &corev1.Namespace{}
+	err := c.Get(ctx, client.ObjectKey{Name: "kube-system"}, ns)
+	if err != nil {
+		return "", err
+	}
+	return string(ns.UID), nil
+}
