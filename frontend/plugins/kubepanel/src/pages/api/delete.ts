446804491a945639d627b0393a90c34ba08ab057
wishrem <wishremm@gmail.com>
2023-11-10 16:38:21 +0800

    feat: added delete resource function

--- frontend/plugins/kubepanel/src/pages/api/delete.ts
+++ frontend/plugins/kubepanel/src/pages/api/delete.ts
@@ -1,6 +1,10 @@
 import { ApiBaseParamsMap } from '@/constants/kube-api';
 import { ResourceKey } from '@/constants/kube-object';
+<<<<<<<< HEAD:frontend/plugins/kubepanel/src/pages/api/delete.ts
 import { deleteResource } from '@/services/backend/api';
+========
+import { createResource } from '@/services/backend/api';
+>>>>>>>> ce505fc3 (feat: added delete resource function):frontend/plugins/kubepanel/src/pages/api/create.ts
 import { authSession } from '@/services/backend/auth';
 import { getKubeApiParams } from '@/services/backend/kubernetes';
 import { jsonRes } from '@/services/backend/response';
@@ -18,7 +22,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!apiBaseParams) throw new Error(`invalid resource ${resource}`);
 
     const { serverUrl, requestOpts, namespace } = getKubeApiParams(await authSession(req.headers));
+<<<<<<<< HEAD:frontend/plugins/kubepanel/src/pages/api/delete.ts
     const data = await deleteResource(
+========
+
+    if (!req.body.data) throw new Error(`invalid data ${req.body.data}`);
+    const resourceData = yaml.load(req.body.data);
+
+    const data = await createResource(
+>>>>>>>> ce505fc3 (feat: added delete resource function):frontend/plugins/kubepanel/src/pages/api/create.ts
       {
         urlParams: { ...apiBaseParams, serverUrl, namespace },
         opts: requestOpts
---
+++ frontend/plugins/kubepanel/src/pages/kubepanel/components/kube-object/config/config-map/config-map.tsx
@@ -0,0 +1,92 @@
+import { KubeObjectAge } from '@/components/kube/object/kube-object-age';
+import { ConfigMap } from '@/k8slens/kube-object';
+import { ColumnsType } from 'antd/es/table';
+import { observer } from 'mobx-react';
+import { useState } from 'react';
+import { CONFIG_MAP_STORE } from '@/store/static';
+import { useQuery } from '@tanstack/react-query';
+import ConfigMapDetail from './config-map-detail';
+import Table from '../../../table/table';
+import ActionButton from '../../../action-button/action-button';
+import { deleteResource } from '@/api/delete';
+import { Resources } from '@/constants/kube-object';
+interface DataType {
+  key: string;
+  name: string;
+  keys: string[];
+  creationTimestamp?: string;
+}
+
+const getData = (configMap: ConfigMap): DataType => {
+  return {
+    key: configMap.getName(),
+    name: configMap.getName(),
+    keys: configMap.getKeys(),
+    creationTimestamp: configMap.metadata.creationTimestamp
+  };
+};
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: 'Keys',
+    dataIndex: 'keys',
+    key: 'keys',
+    render: (keys: string[]) => keys.join(', ')
+  },
+  {
+    title: 'Age',
+    dataIndex: 'creationTimestamp',
+    key: 'age',
+    render: (creationTimestamp: string) => <KubeObjectAge creationTimestamp={creationTimestamp} />
+  },
+  {
+    dataIndex: 'name',
+    key: 'action',
+    fixed: 'right',
+    render: (name: string) => (
+      <ActionButton targetName={name} onDelete={() => deleteResource(name, Resources.ConfigMaps)} />
+    )
+  }
+];
+
+const ConfigMapOverviewPage = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [configMap, setConfigMap] = useState<ConfigMap>();
+
+  useQuery(['configMaps'], () => CONFIG_MAP_STORE.fetchData(), {
+    refetchInterval: 5000
+  });
+
+  const dataSource = CONFIG_MAP_STORE.items.map(getData);
+  return (
+    <>
+      <Table
+        title={'Config Maps'}
+        columns={columns}
+        dataSource={dataSource}
+        onRow={(record) => ({
+          onClick: () => {
+            const { key } = record;
+            const configMap = CONFIG_MAP_STORE.items.filter(
+              (configMap) => configMap.getName() === key
+            )[0];
+            setConfigMap(configMap);
+            setOpenDrawer(true);
+          }
+        })}
+      />
+      <ConfigMapDetail
+        configMap={configMap}
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+      />
+    </>
+  );
+};
+
+export default observer(ConfigMapOverviewPage);
