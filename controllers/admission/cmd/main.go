1053cb8a80ec2c1ab137aa6ca333cd95c1777a3a
yy <lingdie.yy@outlook.com>
2023-11-20 15:41:23 +0800

    fix: skip vingress when delete ingress.
    Signed-off-by: yy <lingdie.yy@outlook.com>

--- controllers/admission/cmd/main.go
+++ controllers/admission/cmd/main.go
@@ -18,9 +18,10 @@ package main
 
 import (
 	"flag"
-	v1 "github.com/labring/sealos/controllers/admission/api/v1"
 	"os"
 	"strings"
+
+	v1 "github.com/labring/sealos/controllers/admission/api/v1"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
