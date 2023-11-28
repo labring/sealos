5672c80c34167e063270c61b4087ff4589041fd5
xuziyi <nowinkey@tom.com>
2023-11-28 15:21:10 +0800

    add rbac

--- controllers/account/controllers/namespace_controller.go
+++ controllers/account/controllers/namespace_controller.go
@@ -68,6 +68,7 @@ const (
 //+kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=namespaces/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=core,resources=namespaces/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
