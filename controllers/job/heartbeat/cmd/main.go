224257cda5eae09a10af61e3a3e05e036f21f9eb
yy <lingdie.yy@outlook.com>
2023-12-06 15:48:22 +0800

    docs: add heartbeat for sealos self-host cluster.
    Signed-off-by: yy <lingdie.yy@outlook.com>

---
+++ controllers/job/heartbeat/cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/labring/sealos/controllers/job/heartbeat/internal/cluster"
+	"gopkg.in/resty.v1"
+)
+
+func main() {
+	resource, err := cluster.GetClusterResources()
+	if err != nil {
+		return
+	}
+
+	client := resty.New()
+	_, err = client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(resource).
+		Post("https://license.sealos.io/api/v1alpha/heartbeat/cluster")
+	if err != nil {
+		return
+	}
+}
---
+++ controllers/job/heartbeat/internal/util/client.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	scheme = runtime.NewScheme()
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+}
+
+func NewKubernetesClient() (client.Client, error) {
+	return client.New(ctrl.GetConfigOrDie(), client.Options{Scheme: scheme})
+}
