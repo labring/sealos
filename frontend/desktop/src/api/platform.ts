001477ccceefce13cfa20999326dbb330bbebcb4
jingyang <3161362058@qq.com>
2023-12-06 16:30:50 +0800

    fix:frontend upload convertdata
    Signed-off-by: jingyang <3161362058@qq.com>

--- frontend/desktop/src/api/platform.ts
+++ frontend/desktop/src/api/platform.ts
@@ -2,12 +2,11 @@ import request from '@/services/request';
 
 // handle baidu
 export const uploadConvertData = (newType: number[], url?: string) => {
-  const defaultUrl =
-    window.location.hostname === 'cloud.sealos.io' ? 'https://sealos.io/' : 'https://sealos.run/';
+  const defaultUrl = 'https://sealos.run/';
   const main_url = url || defaultUrl;
   const bd_vid = sessionStorage.getItem('bd_vid');
   if (!bd_vid) {
-    return Promise.reject('Parameter error');
+    return Promise.reject('upload convert data params error');
   }
   return request.post('/api/platform/uploadData', {
     newType,
