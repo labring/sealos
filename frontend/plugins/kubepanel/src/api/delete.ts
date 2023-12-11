27e8896f208d3e5a35a907955507ced504da29df
wishrem <wishremm@gmail.com>
2023-12-11 15:33:17 +0800

    fix: merge conflict

--- frontend/plugins/kubepanel/src/api/delete.ts
+++
@@ -1,7 +0,0 @@
-import { Resources } from '@/constants/kube-object';
-import { ApiResp } from '@/services/kubernet';
-import { DELETE } from '@/services/request';
-
-export const deleteResource = (name: string, resource: Resources) =>
-  DELETE<ApiResp>(`/api/delete?resource=${resource}&name=${name}`);
-
