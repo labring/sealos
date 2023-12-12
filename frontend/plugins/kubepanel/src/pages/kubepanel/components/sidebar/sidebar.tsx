3b80010afa491d091f1cc66ec46d5bc45077d2c0
wishrem <wishremm@gmail.com>
2023-12-12 20:48:38 +0800

    fix: Fixed Sider

--- frontend/plugins/kubepanel/src/pages/kubepanel/components/sidebar/sidebar.tsx
+++ frontend/plugins/kubepanel/src/pages/kubepanel/components/sidebar/sidebar.tsx
@@ -62,7 +62,7 @@ const ResourceSideNav = ({ onClick = () => {} }: Props) => {
   const router = useRouter();
 
   return (
-    <Flex vertical style={{ height: '100vh', backgroundColor: '#F2F2F4' }}>
+    <Flex vertical>
       <div className="border-b-[1px] border-color-border border-solid px-[18px] py-[12px] w-full">
         <div className="flex justify-between align-middle">
           <div className="text-[#24282C] text-[16px] font-medium p-1">KubePanel</div>
