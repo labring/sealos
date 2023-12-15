348608975c04fbf3402ab977819fb9ffcbcad4d9
jingyang <3161362058@qq.com>
2023-12-15 16:55:52 +0800

    fix scroll
    Signed-off-by: jingyang <3161362058@qq.com>

--- frontend/providers/template/src/pages/develop/components/YamlList.tsx
+++ frontend/providers/template/src/pages/develop/components/YamlList.tsx
@@ -8,13 +8,12 @@ const YamlList = ({ yamlList = [] }: { yamlList: YamlItemType[] }) => {
   return (
     <Flex
       flexGrow={1}
-      h="0"
+      // h="0"
       mt={'12px'}
       alignItems={'start'}
       zIndex={1}
       position={'relative'}
-      overflow={'scroll'}
-    >
+      overflow={'scroll'}>
       <Box flexShrink={0} mt={3} borderRadius={'sm'} overflow={'hidden'} bg={'white'}>
         {yamlList.map((file, index) => (
           <Box
@@ -39,8 +38,7 @@ const YamlList = ({ yamlList = [] }: { yamlList: YamlItemType[] }) => {
                   borderColor: 'myGray.200',
                   backgroundColor: 'transparent'
                 })}
-            onClick={() => setSelectedIndex(index)}
-          >
+            onClick={() => setSelectedIndex(index)}>
             {file.filename.replace(/-.*/, '')}
           </Box>
         ))}
