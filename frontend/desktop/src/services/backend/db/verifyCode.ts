4e5bf88bb619e028f4097f26539351a9d53f327d
xudaotutou <13435638964@163.com>
2024-01-02 11:24:03 +0800

    fix(desktop):fix verification code restriction

--- frontend/desktop/src/services/backend/db/verifyCode.ts
+++ frontend/desktop/src/services/backend/db/verifyCode.ts
@@ -3,13 +3,13 @@ import { connectToDatabase } from './mongodb';
 type TVerification_Codes = {
   phone: string;
   code: string;
-  createdTime: number;
+  createdAt: Date;
 };
 
 async function connectToUserCollection() {
   const client = await connectToDatabase();
   const collection = client.db().collection<TVerification_Codes>('verification_codes');
-  await collection.createIndex({ createdTime: 1 }, { expireAfterSeconds: 60 * 5 });
+  await collection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 60 * 5 });
   return collection;
 }
 
@@ -23,7 +23,7 @@ export async function addOrUpdateCode({ phone, code }: { phone: string; code: st
     {
       $set: {
         code,
-        createdTime: new Date().getTime()
+        createdAt: new Date()
       }
     },
     {
@@ -37,10 +37,9 @@ export async function checkSendable({ phone }: { phone: string }) {
   const codes = await connectToUserCollection();
   const result = await codes.findOne({
     phone,
-    createdTime: {
+    createdAt: {
       // 在区间范围内找到就是已经发送过了，不能再发了
-      $gt: new Date().getTime() - 60 * 1000,
-      $lt: new Date().getTime()
+      $gt: new Date(new Date().getTime() - 60 * 1000)
     }
   });
   return !result;
@@ -51,9 +50,9 @@ export async function checkCode({ phone, code }: { phone: string; code: string }
   const result = await codes.findOne({
     phone,
     code,
-    createdTime: {
+    createdAt: {
       // 5分钟内有效
-      $gt: new Date().getTime() - 5 * 60 * 1000
+      $gt: new Date(new Date().getTime() - 5 * 60 * 1000)
     }
   });
   return !!result;
