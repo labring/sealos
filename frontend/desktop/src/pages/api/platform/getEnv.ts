f4e61a65d1ca3373b1acd30a48f1fdb5f8f1b601
xudaotutou <13435638964@163.com>
2024-01-08 17:56:57 +0800

    chore(desktop): add service protocol link (#4463)

--- frontend/desktop/src/pages/api/platform/getEnv.ts
+++ frontend/desktop/src/pages/api/platform/getEnv.ts
@@ -11,34 +11,39 @@ import {
   enableLicense,
   enableRecharge
 } from '@/services/enable';
+import { ApiResp, SystemEnv } from '@/types';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const wechat_client_id = process.env.WECHAT_CLIENT_ID || '';
   const github_client_id = process.env.GITHUB_CLIENT_ID || '';
   const google_client_id = process.env.GOOGLE_CLIENT_ID || '';
-  const service_protocol = process.env.SERVICE_PROTOCOL || '';
-  const private_protocol = process.env.PRIVATE_PROTOCOL || '';
+  const service_protocol_zh = process.env.SERVICE_PROTOCOL_ZH || '';
+  const private_protocol_zh = process.env.PRIVATE_PROTOCOL_ZH || '';
+  const service_protocol_en = process.env.SERVICE_PROTOCOL_EN || '';
+  const private_protocol_en = process.env.PRIVATE_PROTOCOL_EN || '';
   const needGithub = enableGithub();
   const needWechat = enableWechat();
   const needPassword = enablePassword();
   const needSms = enableSms();
   const needGoogle = enableGoogle();
-  const callback_url = process.env.CALLBACK_URL;
+  const callback_url = process.env.CALLBACK_URL || '';
   const stripeEnabled = enableStripe();
   const wechatEnabledRecharge = enableWechatRecharge();
   const licenseEnabled = enableLicense();
   const rechargeEnabled = enableRecharge();
   const guideEnabled = process.env.GUIDE_ENABLED === 'true';
 
-  jsonRes(res, {
+  jsonRes<SystemEnv>(res, {
     data: {
       SEALOS_CLOUD_DOMAIN: process.env.SEALOS_CLOUD_DOMAIN || 'cloud.sealos.io',
       wechat_client_id,
       github_client_id,
       google_client_id,
       callback_url,
-      service_protocol,
-      private_protocol,
+      service_protocol_zh,
+      private_protocol_zh,
+      service_protocol_en,
+      private_protocol_en,
       needPassword,
       needSms,
       needGithub,
--- frontend/desktop/src/pages/signin.tsx
+++ frontend/desktop/src/pages/signin.tsx
@@ -1,5 +1,7 @@
+import { getSystemEnv } from '@/api/platform';
 import SigninComponent from '@/components/signin';
 import { compareFirstLanguages } from '@/utils/tools';
+import { QueryClient, dehydrate } from '@tanstack/react-query';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useEffect } from 'react';
 
@@ -20,8 +22,11 @@ export async function getServerSideProps({ req, res, locales }: any) {
     req?.cookies?.NEXT_LOCALE || compareFirstLanguages(req?.headers?.['accept-language'] || 'zh');
   res.setHeader('Set-Cookie', `NEXT_LOCALE=${local}; Max-Age=2592000; Secure; SameSite=None`);
 
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({ queryKey: ['getPlatformEnv'], queryFn: getSystemEnv });
   const props = {
-    ...(await serverSideTranslations(local, undefined, null, locales || []))
+    ...(await serverSideTranslations(local, undefined, null, locales || [])),
+    dehydratedState: dehydrate(queryClient)
   };
   return {
     props
