e6bf29b3c54b0bcf863532c57772ff7ce888eff9
xudaotutou <13435638964@163.com>
2024-01-09 22:56:01 +0800

    refactor(costcenter): service api (#4475)

--- frontend/providers/costcenter/src/pages/api/billing/propertiesUsedAmount.ts
+++ frontend/providers/costcenter/src/pages/api/billing/propertiesUsedAmount.ts
@@ -1,5 +1,6 @@
 import { authSession } from '@/service/backend/auth';
 import { jsonRes } from '@/service/backend/response';
+import { PropertiesCost } from '@/types';
 import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, resp: NextApiResponse) {
   try {
@@ -22,28 +23,24 @@ export default async function handler(req: NextApiRequest, resp: NextApiResponse
         code: 400,
         message: 'endTime is invalid'
       });
-    const url = process.env.BILLING_URI + '/account/v1alpha1/namespaces';
-    console.log(url);
-    const res = await (
+    const url = process.env.BILLING_URI + '/account/v1alpha1/costs/properties';
+    const res = (await (
       await fetch(url, {
         method: 'POST',
         body: JSON.stringify({
           endTime,
           kubeConfig: kc.exportConfig(),
           owner: user.name,
-          startTime,
-          type: 0
+          startTime
         })
       })
-    ).json();
-    console.log(res);
+    ).json()) as PropertiesCost;
     return jsonRes(resp, {
       code: 200,
-      data: res.data,
-      message: res.message
+      data: res
     });
   } catch (error) {
     console.log(error);
-    jsonRes(resp, { code: 500, message: 'get namespaceList error' });
+    jsonRes(resp, { code: 500, message: 'get billing cost error' });
   }
 }
