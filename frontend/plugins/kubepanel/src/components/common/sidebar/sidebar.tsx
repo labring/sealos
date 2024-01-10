02df9e07de39544e72c832b634e8c69ed31734f4
wishrem <wishremm@gmail.com>
2024-01-10 16:14:07 +0800

    style: reposition components to fix next build problems

---
+++ frontend/plugins/kubepanel/src/components/common/sidebar/sidebar.tsx
@@ -0,0 +1,86 @@
+import {
+  DashboardOutlined,
+  DatabaseOutlined,
+  GatewayOutlined,
+  ReloadOutlined,
+  SettingOutlined
+} from '@ant-design/icons';
+import { Button, Flex, Menu, MenuProps } from 'antd';
+import { useRouter } from 'next/router';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+export enum SideNavItemKey {
+  Overview = 'overview',
+  Pod = 'pod',
+  Deployment = 'deployment',
+  ConfigMap = 'config-map',
+  PersistentVolumeClaim = 'volume-claim',
+  StatefulSet = 'stateful-set',
+  Secret = 'secret',
+  Ingress = 'ingress'
+}
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group'
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type
+  } as MenuItem;
+}
+
+const items: MenuProps['items'] = [
+  getItem('Workload', 'workload', <DashboardOutlined />, [
+    getItem('Overview', SideNavItemKey.Overview),
+    getItem('Pods', SideNavItemKey.Pod),
+    getItem('Deployments', SideNavItemKey.Deployment),
+    getItem('Stateful Sets', SideNavItemKey.StatefulSet)
+  ]),
+  getItem('Config', 'config', <SettingOutlined />, [
+    getItem('Config Maps', SideNavItemKey.ConfigMap),
+    getItem('Secrets', SideNavItemKey.Secret)
+  ]),
+  getItem('Network', 'network', <GatewayOutlined />, [getItem('Ingress', SideNavItemKey.Ingress)]),
+  getItem('Storage', 'storage', <DatabaseOutlined />, [
+    getItem('Persistent Volume Claims', SideNavItemKey.PersistentVolumeClaim)
+  ])
+];
+
+interface Props {
+  onClick?: (key: SideNavItemKey) => void;
+}
+
+export const ResourceSideNav = ({ onClick = () => {} }: Props) => {
+  const router = useRouter();
+
+  return (
+    <Flex vertical>
+      <div className="border-b-[1px] border-color-border border-solid px-[18px] py-[12px] w-full">
+        <div className="flex justify-between align-middle">
+          <div className="text-[#24282C] text-[16px] font-medium p-1">KubePanel</div>
+          <Button
+            type="text"
+            icon={<ReloadOutlined style={{ color: '#219BF4', fontSize: 'large' }} />}
+            onClick={() => router.reload()}
+          />
+        </div>
+      </div>
+      <Menu
+        style={{ backgroundColor: '#F2F2F4', borderRight: 'none' }}
+        defaultSelectedKeys={['overview']}
+        defaultOpenKeys={['workload']}
+        mode="inline"
+        items={items}
+        onClick={({ key }) => onClick(key as SideNavItemKey)}
+      />
+    </Flex>
+  );
+};
---
+++ frontend/plugins/kubepanel/src/pages/kubepanel/layout.tsx
@@ -0,0 +1,52 @@
+import { ConfigProvider, Layout } from 'antd';
+import { ResourceSideNav, SideNavItemKey } from '@/components/common/sidebar/sidebar';
+import { theme } from '@/constants/theme';
+import { useCallback, useState } from 'react';
+import { CollapseType } from 'antd/es/layout/Sider';
+
+const { Sider, Content } = Layout;
+
+interface Props {
+  children: React.ReactNode;
+  onClickSideNavItem?: (key: SideNavItemKey) => void;
+}
+
+type CollapseCallback = (collapsed: boolean, type: CollapseType) => void;
+
+const siderWidth = 256;
+const collapsedWidth = 0;
+
+export default function AppLayout({ children, onClickSideNavItem }: Props) {
+  const [contentMargin, setContentMargin] = useState(siderWidth);
+
+  const onCollapse = useCallback<CollapseCallback>((collapsed, _) => {
+    setContentMargin(collapsed ? collapsedWidth : siderWidth);
+  }, []);
+
+  return (
+    <ConfigProvider theme={theme}>
+      <Layout>
+        <Sider
+          style={{
+            height: '100vh',
+            backgroundColor: '#F2F2F4',
+            position: 'fixed',
+            left: 0,
+            top: 0,
+            bottom: 0
+          }}
+          width={siderWidth}
+          theme="light"
+          onCollapse={onCollapse}
+          breakpoint="lg"
+          collapsedWidth={collapsedWidth}
+        >
+          <ResourceSideNav onClick={onClickSideNavItem} />
+        </Sider>
+        <Layout style={{ marginLeft: contentMargin }}>
+          <Content style={{ backgroundColor: '#F2F2F4' }}>{children}</Content>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
+  );
+}
