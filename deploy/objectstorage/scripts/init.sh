0e9de2ae737a450a5cedda636b4bf4eff5af169d
yy <lingdie.yy@outlook.com>
2024-01-25 14:52:15 +0800

    feat: object storage cluster init. Signed-off-by: yy <lingdie.yy@outlook.com>
    Signed-off-by: yy <lingdie.yy@outlook.com>

---
+++ deploy/objectstorage/scripts/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# TODO
+# 1. create minio instance
+# 2. create prometheus instance
+# 3. run objectstorage controller
+# 4. run objectstorage frontend
+# 5. run objectstorage monitor service
