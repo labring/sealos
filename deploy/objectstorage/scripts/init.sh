d9c183bc40dec7e1a0b34cd054c28350ac910fea
xuziyi <nowinkey@tom.com>
2024-01-28 14:59:01 +0800

    tmp images

--- deploy/objectstorage/scripts/init.sh
+++ deploy/objectstorage/scripts/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # 1. create minio instance
-bash ./minio.sh
+bash minio.sh
 # 2. create prometheus instance
-bash ./prometheus.sh
+bash prometheus.sh
 # 3. run objectstorage controller
-sealos run ghcr.io/labring/sealos-cloud-objectstorage-controller:latest
+sealos run ghcr.io/nowinkeyy/sealos-cloud-objectstorage-controller:latest
 # 4. run objectstorage frontend
-sealos run ghcr.io/labring/sealos-cloud-objectstorage-frontend:latest
+sealos run ghcr.io/nowinkeyy/sealos-cloud-objectstorage-frontend:latest
 # 5. run objectstorage monitor service
-sealos run ghcr.io/labring/sealos-cloud-minio-service:latest
+sealos run ghcr.io/nowinkeyy/sealos-cloud-minio-service:latest
