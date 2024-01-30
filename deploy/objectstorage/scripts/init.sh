317570b477c92c90804942802a2bb695eb638485
xuziyi <nowinkey@tom.com>
2024-01-30 14:50:01 +0800

    add env

--- deploy/objectstorage/scripts/init.sh
+++ deploy/objectstorage/scripts/init.sh
@@ -7,8 +7,8 @@ bash scripts/minio.sh
 # 2. create prometheus instance
 bash scripts/prometheus.sh
 # 3. run objectstorage controller
-sealos run ghcr.io/nowinkeyy/sealos-cloud-objectstorage-controller:latest
+sealos run ghcr.io/nowinkeyy/sealos-cloud-objectstorage-controller:latest -e cloudDomain=${cloudDomain}
 # 4. run objectstorage frontend
-sealos run ghcr.io/nowinkeyy/sealos-cloud-objectstorage-frontend:latest
+sealos run ghcr.io/nowinkeyy/sealos-cloud-objectstorage-frontend:latest -e cloudDomain=${cloudDomain}
 # 5. run objectstorage monitor service
-sealos run ghcr.io/nowinkeyy/sealos-cloud-minio-service:latest
+sealos run ghcr.io/nowinkeyy/sealos-cloud-minio-service:latest -e cloudDomain=${cloudDomain}
