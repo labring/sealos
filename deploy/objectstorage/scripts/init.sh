c7d0122bc3cc3251e0b03164e4385899f21f4a1c
xuziyi <nowinkey@tom.com>
2024-02-26 15:55:56 +0800

    run tars/xxx.tar

--- deploy/objectstorage/scripts/init.sh
+++ deploy/objectstorage/scripts/init.sh
@@ -5,8 +5,8 @@ bash scripts/minio.sh
 # 2. create prometheus instance
 bash scripts/prometheus.sh
 # 3. run objectstorage controller
-sealos run ghcr.io/labring/sealos-cloud-objectstorage-controller:latest -e cloudDomain=${cloudDomain}
+sealos run tars/objectstorage-controller.tar -e cloudDomain=${cloudDomain}
 # 4. run objectstorage frontend
-sealos run ghcr.io/labring/sealos-cloud-objectstorage-frontend:latest -e cloudDomain=${cloudDomain}
+sealos run tars/objectstorage-frontend.tar -e cloudDomain=${cloudDomain}
 # 5. run objectstorage monitor service
-sealos run ghcr.io/labring/sealos-cloud-minio-service:latest -e cloudDomain=${cloudDomain}
+sealos run tars/objectstorage-service.tar -e cloudDomain=${cloudDomain}
