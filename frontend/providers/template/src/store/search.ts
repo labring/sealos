413ee2b1f7874fcc4041036f324c8a917d312657
jingyang <3161362058@qq.com>
2024-03-08 14:30:00 +0800

    feat:template support application marketplace category feature
    Signed-off-by: jingyang <3161362058@qq.com>

--- frontend/providers/template/src/store/search.ts
+++ frontend/providers/template/src/store/search.ts
@@ -1,19 +1,28 @@
+import { ApplicationType } from '@/types/app';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 type State = {
   searchValue: string;
+  appType: ApplicationType;
   setSearchValue: (e: string) => void;
+  setAppType: (e: ApplicationType) => void;
 };
 
 export const useSearchStore = create<State>()(
   devtools(
     immer((set, get) => ({
       searchValue: '',
+      appType: ApplicationType.All,
       setSearchValue(e: string) {
         set((state) => {
           state.searchValue = e;
+        });
+      },
+      setAppType(e: ApplicationType) {
+        set((state) => {
+          state.appType = e;
         });
       }
     }))
