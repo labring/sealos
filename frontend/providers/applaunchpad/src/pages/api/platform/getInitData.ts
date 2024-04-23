e545add5c293a93cb24c346a89c976d3222f3129
yy <lingdie.yy@outlook.com>
2024-04-23 19:32:28 +0800

    chore: update launchpad config to use yaml. Signed-off-by: yy <lingdie.yy@outlook.com>
    Signed-off-by: yy <lingdie.yy@outlook.com>

--- frontend/providers/applaunchpad/src/pages/api/platform/getInitData.ts
+++ frontend/providers/applaunchpad/src/pages/api/platform/getInitData.ts
@@ -1,8 +1,10 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { jsonRes } from '@/services/backend/response';
-import type { FormSliderListType } from '@/types';
-import { readFileSync } from 'fs';
-import { Coin, noGpuSliderKey } from '@/constants/app';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {jsonRes} from '@/services/backend/response';
+import type {FormSliderListType} from '@/types';
+import {readFileSync} from 'fs';
+import {Coin, defaultSliderKey} from '@/constants/app';
+import * as yaml from 'js-yaml';
+
 
 export type Response = {
   SEALOS_DOMAIN: string;
@@ -14,7 +16,7 @@ export type Response = {
 };
 
 export const defaultVal = {
-  [noGpuSliderKey]: {
+  [defaultSliderKey]: {
     cpu: [100, 200, 500, 1000, 2000, 3000, 4000, 8000],
     memory: [64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
   }
@@ -25,10 +27,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!global.FormSliderListConfig) {
       const filename =
         process.env.NODE_ENV === 'development'
-          ? 'data/form_slider_config.json.local'
-          : '/app/data/form_slider_config.json';
+          ? 'data/form_slider_config.yaml'
+          : '/app/data/form_slider_config.yaml';
 
-      const res = JSON.parse(readFileSync(filename, 'utf-8'));
+      const res: any = yaml.load(readFileSync(filename, 'utf-8'));
       console.log(res);
 
       global.FormSliderListConfig = res;
