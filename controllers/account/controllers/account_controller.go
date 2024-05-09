7d6d5c47452ce9643974c4e818f4434ea90f9815
Jiahui <4543bxy@gmail.com>
2024-05-10 00:29:08 +0800

    Update account_controller.go

--- controllers/account/controllers/account_controller.go
+++ controllers/account/controllers/account_controller.go
@@ -267,7 +267,7 @@ func (r *AccountReconciler) DeletePayment(ctx context.Context) error {
 					r.Logger.Error(err, "get payment details failed")
 				}
 				if status == pay.PaymentSuccess {
-					if payment.Status.Status == pay.PaymentSuccess {
+					if payment.Status.Status != pay.PaymentSuccess {
 						continue
 					}
 					r.Logger.Info("payment success, post delete payment cr", "payment", payment, "amount", amount)
