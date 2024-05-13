30bd118e37f942761481cd7b04f473df04be9aad
yy <lingdie.yy@outlook.com>
2024-05-13 16:12:45 +0800

    chore: update sealos cloud cluster image to adapt desktop using yaml as config. Signed-off-by: yy <lingdie.yy@outlook.com>
    Signed-off-by: yy <lingdie.yy@outlook.com>

--- frontend/desktop/deploy/scripts/init.sh
+++ frontend/desktop/deploy/scripts/init.sh
@@ -2,9 +2,12 @@
 set -ex
 kubectl apply -f manifests/deploy.yaml -f manifests/rbac.yaml -f manifests/ingress.yaml
 cm_exists=$(kubectl get cm desktop-frontend-config -n sealos --ignore-not-found=true)
-if [[ -n "cm_exists" ]]; then
+if [[ -n "$cm_exists" ]]; then
   echo "desktop-frontend-config already exists, skip create desktop config"
 else
   echo "create desktop config"
+    sed -i -e "s;<your-internal-jwt-secret>;$(tr -cd 'a-z0-9' </dev/urandom | head -c64);" manifests/config.yaml
+    sed -i -e "s;<your-regional-jwt-secret>;$(tr -cd 'a-z0-9' </dev/urandom | head -c64);" manifests/config.yaml
+    sed -i -e "s;<your-global-jwt-secret>;$(tr -cd 'a-z0-9' </dev/urandom | head -c64);" manifests/config.yaml
   kubectl apply -f manifests/config.yaml --validate=false
 fi
