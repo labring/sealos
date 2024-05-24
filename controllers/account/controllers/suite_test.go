517afa9b942377a74dfe4d30c52dabf84309fbc6
yy <lingdie.yy@outlook.com>
2024-05-24 14:59:44 +0800

    revert account.

--- controllers/account/controllers/suite_test.go
+++ controllers/account/controllers/suite_test.go
@@ -30,6 +30,7 @@ import (
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	//+kubebuilder:scaffold:imports
