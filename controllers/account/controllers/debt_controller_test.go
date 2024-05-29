1ad3c7185c5a88f9ddf3d7fdad96e141ffd24c62
Jiahui <4543bxy@gmail.com>
2024-05-29 16:31:41 +0800

    Feat/ add vms/email debt notification (#4763)
    * optimize debt log & fix named kb cluster backup pvc named.
    * add vms notification for debt
    * add email notification for debt

--- controllers/account/controllers/debt_controller_test.go
+++ controllers/account/controllers/debt_controller_test.go
@@ -16,6 +16,7 @@ package controllers
 
 import (
 	"testing"
+	"time"
 )
 
 func Test_splitSmsCodeMap(t *testing.T) {
@@ -35,5 +36,18 @@ func Test_splitSmsCodeMap(t *testing.T) {
 	}
 	if codeMap[2] != "SMS_987654" {
 		t.Fatal("invalid codeMap")
+	}
+}
+
+func TestGetTimeInUTCPlus8(t *testing.T) {
+	t1 := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
+	t2 := time.Date(2023, 1, 1, 1, 0, 0, 0, time.UTC)
+	t3 := time.Date(2023, 1, 1, 9, 0, 0, 0, time.UTC)
+	t4 := time.Date(2023, 1, 1, 11, 0, 0, 0, time.UTC)
+	t5 := time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC)
+	t6 := time.Date(2023, 1, 1, 13, 0, 0, 0, time.UTC)
+	t7 := time.Date(2023, 1, 1, 23, 0, 0, 0, time.UTC)
+	for _, _t := range []time.Time{t1, t2, t3, t4, t5, t6, t7} {
+		t.Logf("time: %v, timeInUTCPlus8: %v", _t, GetSendVmsTimeInUTCPlus8(_t))
 	}
 }
