ef2723187835a1cb64079e4e2d6fe3755c737cfa
谢金虎 <1050780355@qq.com>
2024-06-14 14:58:40 +0800

    database exception monitor

--- controllers/account/controllers/utils/vms_test.go
+++ controllers/account/controllers/utils/vms_test.go
@@ -13,32 +13,3 @@
 // limitations under the License.
 
 package utils
-
-import (
-	"os"
-	"testing"
-	"time"
-
-	"github.com/volcengine/volc-sdk-golang/service/vms"
-)
-
-func TestSendVms(t *testing.T) {
-	vms.DefaultInstance.Client.SetAccessKey(os.Getenv("VMS_AK"))
-	vms.DefaultInstance.Client.SetSecretKey(os.Getenv("VMS_SK"))
-	var testData = struct {
-		phone        string
-		template     string
-		numberPollNo string
-		sendTime     time.Time
-	}{
-		phone:        "",
-		template:     "",
-		numberPollNo: "",
-		sendTime:     time.Now(),
-	}
-	err := SendVms(testData.phone, testData.template, testData.numberPollNo, testData.sendTime, []string{"10:00-20:00"})
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log("SendVms success")
-}
