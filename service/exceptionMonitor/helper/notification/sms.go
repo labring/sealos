76d48358e4f29e025ef961603680c8e80c185e1d
谢金虎 <1050780355@qq.com>
2024-06-14 15:11:57 +0800

    database exception monitor

--- service/exceptionMonitor/helper/notification/sms.go
+++ service/exceptionMonitor/helper/notification/sms.go
@@ -1,25 +1,38 @@
 package notification
 
 import (
+	"errors"
 	"os"
 	"strings"
 
 	"github.com/alibabacloud-go/dysmsapi-20170525/v3/client"
 	"github.com/alibabacloud-go/tea/tea"
 	"github.com/labring/sealos/controllers/pkg/types"
+	pkgtypes "github.com/labring/sealos/controllers/pkg/types"
 	"github.com/labring/sealos/controllers/pkg/utils/notifier"
 	"github.com/labring/sealos/service/exceptionMonitor/api"
 	"github.com/labring/sealos/service/exceptionMonitor/dao"
 )
 
 func GetPhoneNumberByNS(ns string) (error, string) {
-	provider, err := dao.CK.GetUserOauthProvider(&types.UserQueryOpts{
+	outh, err := dao.CK.GetUserOauthProvider(&types.UserQueryOpts{
 		Owner: ns,
 	})
 	if err != nil {
 		return err, ""
 	}
-	return nil, provider.ProviderID
+	phone, email := "", ""
+	for i := range outh {
+		if outh[i].ProviderType == pkgtypes.OauthProviderTypePhone {
+			phone = outh[i].ProviderID
+		} else if outh[i].ProviderType == pkgtypes.OauthProviderTypeEmail {
+			email = outh[i].ProviderID
+		}
+	}
+	if phone == "" && email == "" {
+		return errors.New("user phone && email is not set, skip sms notification"), ""
+	}
+	return nil, phone
 }
 
 func SendToSms(namespace, databaseName, clusterName, content string) error {
