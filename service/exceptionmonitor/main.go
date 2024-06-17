d5ce12a1a12fdaebeda072288997e7836bf073e4
谢金虎 <1050780355@qq.com>
2024-06-17 15:46:40 +0800

    database exception monitor

--- service/exceptionmonitor/main.go
+++ service/exceptionmonitor/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/labring/sealos/service/exceptionmonitor/api"
@@ -13,19 +11,6 @@ import (
 )
 
 func main() {
-	fmt.Println("111111")
-	envs := os.Environ()
-
-	// 打印所有环境变量
-	fmt.Println("Environment Variables:")
-	for _, env := range envs {
-		// 将 "key=value" 格式的环境变量拆分为键和值
-		pair := strings.SplitN(env, "=", 2)
-		key := pair[0]
-		value := pair[1]
-		fmt.Printf("%s=%s\n", key, value)
-	}
-
 	var err error
 	if err = api.GetENV(); err != nil {
 		fmt.Printf("Failed to get env: %v", err)
