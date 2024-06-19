9c079afdccca3588fcd543160a3fcb2c966e0b69
谢金虎 <1050780355@qq.com>
2024-06-19 12:15:24 +0800

    database exception monitor

--- service/exceptionmonitor/dao/init.go
+++ service/exceptionmonitor/dao/init.go
@@ -13,6 +13,7 @@ var (
 
 func InitCockroachDB() error {
 	var err error
+	os.Setenv("LOCAL_REGION", "6a216614-e658-4482-a244-e4311390715f")
 	CK, err = cockroach.NewCockRoach(os.Getenv("GlobalCockroachURI"), os.Getenv("LocalCockroachURI"))
 	fmt.Println(os.Getenv("GlobalCockroachURI"), os.Getenv("LocalCockroachURI"))
 	if err != nil {
