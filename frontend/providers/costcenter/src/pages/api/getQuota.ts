236703b291d6df3028d579347e8f72689ee7ecda
zijiren233 <pyh1670605849@gmail.com>
2024-06-29 15:26:38 +0800

    opt: fallback use GetUserDefaultNameSpace(user.name)

--- frontend/providers/costcenter/src/pages/api/getQuota.ts
+++ frontend/providers/costcenter/src/pages/api/getQuota.ts
@@ -12,7 +12,7 @@ export default async function handler(req: NextApiRequest, resp: NextApiResponse
     if (user === null) {
       return jsonRes(resp, { code: 403, message: 'user null' });
     }
-    const namespace = kc.contexts[0].namespace || `ns-${user.name}`;
+    const namespace = kc.contexts[0].namespace || GetUserDefaultNameSpace(user.name);
     const quota = await getUserQuota(kc, namespace);
     return jsonRes(resp, {
       code: 200,
