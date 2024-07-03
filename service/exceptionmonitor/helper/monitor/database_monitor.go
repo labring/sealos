41d22d30257777135473eaf3aeedf8e9bbb30d92
谢金虎 <1050780355@qq.com>
2024-07-03 16:27:47 +0800

    database exception monitor

--- service/exceptionmonitor/helper/monitor/database_monitor.go
+++ service/exceptionmonitor/helper/monitor/database_monitor.go
@@ -126,6 +126,7 @@ func cleanClusterStatus(databaseClusterName string) {
 
 func handleClusterException(databaseClusterName, namespace, databaseType, status string) {
 	if _, ok := api.LastDatabaseClusterStatus[databaseClusterName]; !ok && !api.DebtNamespaceMap[namespace] {
+		fmt.Println(111)
 		api.LastDatabaseClusterStatus[databaseClusterName] = status
 		api.ExceptionDatabaseMap[databaseClusterName] = true
 		if err := processClusterException(databaseClusterName, namespace, databaseType, status); err != nil {
