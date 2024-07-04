708e977d11b50680dda2b481b05cc17748a5ab51
Jiahui <4543bxy@gmail.com>
2024-07-04 17:28:45 +0800

    feat: app cost svc (#4843)
    * account service add app cost api
    
    * add transfer all service
    
    * optimize
    
    * fix get user with id
    
    * add white list k8s host for service auth
    
    * add transfer from user id
    
    * swag init
    
    * rename transfer table to UserTransfer
    
    * add transfer id
    
    * swag init
    
    * fix get transfer
    
    * optimize log
    
    * fix
    
    * fix get user
    
    * fix get transfer limit range
    
    * add limitrange log
    
    * fix time range
    
    * fix limit req name

--- service/account/helper/request.go
+++ service/account/helper/request.go
@@ -45,7 +45,7 @@ type TransferAmountReq struct {
 	// @Summary Transfer amount
 	// @Description Transfer amount
 	// @JSONSchema required
-	Amount int64 `json:"amount" bson:"amount" binding:"required" example:"100000000"`
+	Amount int64 `json:"amount" bson:"amount" example:"100000000"`
 
 	// @Summary To user
 	// @Description To user
@@ -56,6 +56,10 @@ type TransferAmountReq struct {
 	// @Description Authentication information
 	// @JSONSchema required
 	Auth `json:",inline" bson:",inline"`
+
+	// @Summary Transfer all
+	// @Description Transfer all amount
+	TransferAll bool `json:"transferAll" bson:"transferAll"`
 }
 
 type ConsumptionRecordReq struct {
@@ -108,6 +112,7 @@ type TimeRange struct {
 
 type Auth struct {
 	Owner      string `json:"owner" bson:"owner" binding:"required" example:"admin"`
+	UserID     string `json:"userID" bson:"userID" example:"admin"`
 	KubeConfig string `json:"kubeConfig" bson:"kubeConfig" binding:"required"`
 }
 
@@ -134,6 +139,9 @@ func ParseTransferAmountReq(c *gin.Context) (*TransferAmountReq, error) {
 	if err := c.ShouldBindJSON(transferAmount); err != nil {
 		return nil, fmt.Errorf("bind json error: %v", err)
 	}
+	if transferAmount.Amount == 0 && !transferAmount.TransferAll {
+		return nil, fmt.Errorf("transfer amount cannot be empty")
+	}
 	return transferAmount, nil
 }
 
@@ -152,6 +160,10 @@ func ParseConsumptionRecordReq(c *gin.Context) (*ConsumptionRecordReq, error) {
 }
 
 type UserBaseReq struct {
+
+	// @Summary Start and end time for the request
+	// @Description Start and end time for the request
+	// @JSONSchema required
 	TimeRange `json:",inline" bson:",inline"`
 
 	// @Summary Authentication information
@@ -173,4 +185,78 @@ func ParseUserBaseReq(c *gin.Context) (*UserBaseReq, error) {
 	}
 	userCosts.Owner = strings.TrimPrefix(userCosts.Owner, "ns-")
 	return userCosts, nil
+}
+
+type AppCostsReq struct {
+	UserBaseReq `json:",inline" bson:",inline"`
+
+	// @Summary Namespace
+	// @Description Namespace
+	Namespace string `json:"namespace,omitempty" bson:"namespace" example:"ns-admin"`
+	// @Summary App type
+	// @Description App type
+	AppType string `json:"appType,omitempty" bson:"appType" example:"app"`
+
+	// @Summary App Name
+	// @Description App Name
+	AppName string `json:"appName,omitempty" bson:"appName" example:"app"`
+
+	// @Summary Page
+	// @Description Page
+	Page int `json:"page,omitempty" bson:"page" example:"1"`
+
+	// @Summary Page Size
+	// @Description Page Size
+	PageSize int `json:"pageSize,omitempty" bson:"pageSize" example:"10"`
+}
+
+func ParseAppCostsReq(c *gin.Context) (*AppCostsReq, error) {
+	userCosts := &AppCostsReq{}
+	if err := c.ShouldBindJSON(userCosts); err != nil {
+		return nil, fmt.Errorf("bind json error: %v", err)
+	}
+	if userCosts.TimeRange.StartTime.Before(time.Now().Add(-6 * humanize.Month)) {
+		userCosts.TimeRange.StartTime = time.Now().Add(-6 * humanize.Month)
+	}
+	if userCosts.TimeRange.EndTime.After(time.Now()) {
+		userCosts.TimeRange.EndTime = time.Now()
+	}
+	userCosts.Owner = strings.TrimPrefix(userCosts.Owner, "ns-")
+	return userCosts, nil
+}
+
+type GetTransferRecordReq struct {
+	UserBaseReq `json:",inline" bson:",inline"`
+
+	// 0: all, 1: in, 2: out
+	// @Summary Type of the request
+	// @Description Type of the request: 0: all, 1: transfer in, 2: transfer out
+	Type int `json:"type,omitempty" bson:"type" example:"0"`
+
+	// @Summary Transfer ID
+	// @Description Transfer ID
+	TransferID string `json:"transferID,omitempty" bson:"transferID" example:"transfer-id-1"`
+
+	// @Summary Page
+	// @Description Page
+	Page int `json:"page,omitempty" bson:"page" example:"1"`
+
+	// @Summary Page Size
+	// @Description Page Size
+	PageSize int `json:"pageSize,omitempty" bson:"pageSize" example:"10"`
+}
+
+func ParseGetTransferRecordReq(c *gin.Context) (*GetTransferRecordReq, error) {
+	transferReq := &GetTransferRecordReq{}
+	if err := c.ShouldBindJSON(transferReq); err != nil {
+		return nil, fmt.Errorf("bind json error: %v", err)
+	}
+	if transferReq.TimeRange.StartTime.Before(time.Now().Add(-6 * humanize.Month)) {
+		transferReq.TimeRange.StartTime = time.Now().Add(-6 * humanize.Month)
+	}
+	if transferReq.TimeRange.EndTime.After(time.Now()) {
+		transferReq.TimeRange.EndTime = time.Now()
+	}
+	transferReq.Owner = strings.TrimPrefix(transferReq.Owner, "ns-")
+	return transferReq, nil
 }
