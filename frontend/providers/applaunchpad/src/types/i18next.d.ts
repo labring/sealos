76e813ec43b421730638bbb4154e00c3a9a446ef
jingyang <3161362058@qq.com>
2024-07-15 16:42:40 +0800

    fix launchpad ci

--- frontend/providers/applaunchpad/src/types/i18next.d.ts
+++ frontend/providers/applaunchpad/src/types/i18next.d.ts
@@ -6,22 +6,12 @@ export interface I18nNamespaces {
   common: typeof common;
 }
 
-export type NestedKeys<T> = T extends object
-  ? {
-      [K in keyof T]-?: K extends string
-        ? T[K] extends object
-          ? `${K}` | `${K}.${NestedKeys<T[K]>}`
-          : `${K}`
-        : never;
-    }[keyof T]
-  : never;
-
 export type I18nNsType = (keyof I18nNamespaces)[];
 
-declare module 'i18next' {
-  interface CustomTypeOptions {
-    returnNull: false;
-    defaultNS: 'common';
-    resources: I18nNamespaces;
-  }
-}
+// declare module 'i18next' {
+//   interface CustomTypeOptions {
+//     returnNull: false;
+//     defaultNS: 'common';
+//     resources: I18nNamespaces;
+//   }
+// }
