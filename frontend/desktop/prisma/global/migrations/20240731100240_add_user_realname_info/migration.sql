a7789860d99a5c57c38fbee07b9f5fbbd618e544
HUAHUAI23 <huahua1319873800@outlook.com>
2024-07-31 10:09:09 +0000

    chore

--- frontend/desktop/prisma/global/migrations/20240731100240_add_user_realname_info/migration.sql
+++ frontend/desktop/prisma/global/migrations/20240731100240_add_user_realname_info/migration.sql
@@ -31,9 +31,16 @@ CREATE TABLE "RealNameAuthProvider" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "backend" STRING NOT NULL,
     "authType" STRING NOT NULL,
+    "maxFailedTimes" INT4 NOT NULL,
     "config" JSONB,
     "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMPTZ(3) NOT NULL,
 
     CONSTRAINT "RealNameAuthProvider_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserRealNameInfo_userUid_key" ON "UserRealNameInfo"("userUid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RestrictedUser_userUid_key" ON "RestrictedUser"("userUid");
