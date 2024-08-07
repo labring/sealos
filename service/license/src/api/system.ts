3e7ec501986518dcf4910889511957710a69bb93
jingyang <3161362058@qq.com>
2024-08-07 14:30:54 +0800

    update uploadConvertData

--- service/license/src/api/system.ts
+++ service/license/src/api/system.ts
@@ -14,9 +14,9 @@ export const getPriceBonus = () =>
 
 // handle baidu
 export const uploadConvertData = (newType: number[], url?: string) => {
-  const defaultUrl = 'https://sealos.run/';
+  const defaultUrl = 'https://sealos.run/self-hosting';
   const main_url = url || defaultUrl;
-  const bd_vid = sessionStorage.getItem('bd_vid');
+  const bd_vid = localStorage.getItem('bd_vid');
   if (!bd_vid) {
     return Promise.reject('upload convert data params error');
   }
