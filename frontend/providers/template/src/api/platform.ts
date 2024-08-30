54c0180ff3e91fbef15a99ce0dad5b5e05ff7aed
zijiren <84728412+zijiren233@users.noreply.github.com>
2024-08-30 15:41:48 +0800

    feat: cost quota and price (#5014)
    * feat: init template cost quota and price
    
    * feat: usage
    
    * feat: price box
    
    * fix: checkQuotaAllow
    
    * fix: get quota
    
    * fix: nodeport pricea
    
    * fix: no used code
    
    * fix: no used code
    
    * fix: CheckQuotaType
    
    * fix: cannot slice number

--- frontend/providers/template/src/api/platform.ts
+++ frontend/providers/template/src/api/platform.ts
@@ -1,6 +1,7 @@
 import { EnvResponse } from '@/types/index';
 import { GET } from '@/services/request';
 import { SystemConfigType, TemplateType } from '@/types/app';
+import type { UserQuotaItemType, userPriceType } from '@/types/user';
 
 export const updateRepo = () => GET('/api/updateRepo');
 
@@ -12,3 +13,11 @@ export const getPlatformEnv = () => GET<EnvResponse>('/api/platform/getEnv');
 export const getSystemConfig = () => {
   return GET<SystemConfigType>('/api/platform/getSystemConfig');
 };
+
+export const getUserQuota = () =>
+  GET<{
+    balance: number;
+    quota: UserQuotaItemType[];
+  }>('/api/platform/getQuota');
+
+export const getResourcePrice = () => GET<userPriceType>('/api/platform/resourcePrice');
