48593c88a82da6733a065790793ffe81fa9b1ff7
zijiren233 <pyh1670605849@gmail.com>
2024-09-02 11:09:37 +0800

    feat: support user external domain

--- frontend/providers/template/src/pages/api/platform/getEnv.ts
+++ frontend/providers/template/src/pages/api/platform/getEnv.ts
@@ -18,7 +18,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     console.log(error, 'errpr-');
   }
 
+  const data = getTemplateEnvs(user_namespace);
+  if (process.env.SEALOS_USER_DOMAIN) data['SEALOS_CLOUD_DOMAIN'] = process.env.SEALOS_USER_DOMAIN;
+
   jsonRes<EnvResponse>(res, {
-    data: getTemplateEnvs(user_namespace)
+    data
   });
 }
