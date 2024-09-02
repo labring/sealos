9d4a49df51fe8a82d63d196b9c89c08e8e2027ea
Alex Lee <3076438032@qq.com>
2024-09-02 14:18:14 +0800

    fix(devbox): ui and k8s bug

--- frontend/providers/devbox/middleware.ts
+++ frontend/providers/devbox/middleware.ts
@@ -5,5 +5,5 @@ export default createMiddleware(routing)
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|fonts|public).*)']
+  matcher: ['/', '/(zh|en)/:path*']
 }
--- frontend/providers/devbox/services/backend/kubernetes.ts
+++ frontend/providers/devbox/services/backend/kubernetes.ts
@@ -257,8 +257,7 @@ export async function getK8s({ kubeconfig }: { kubeconfig: string }) {
     return Promise.reject('用户不存在')
   }
 
-  // const namespace = kc.contexts[0].namespace || GetUserDefaultNameSpace(kube_user.name)
-  const namespace = kc.contexts[0].namespace || 'default'
+  const namespace = kc.contexts[0].namespace || GetUserDefaultNameSpace(kube_user.name)
 
   const applyYamlList = async (yamlList: string[], type: 'create' | 'replace' | 'update') => {
     const formatYaml: k8s.KubernetesObject[] = yamlList
