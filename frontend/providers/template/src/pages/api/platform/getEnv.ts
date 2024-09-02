3eb601315c8925b156b27ecf959b189b36929b01
zijiren233 <pyh1670605849@gmail.com>
2024-09-02 11:51:24 +0800

    fix: template deploy init templates json

--- frontend/providers/template/src/pages/api/platform/getEnv.ts
+++ frontend/providers/template/src/pages/api/platform/getEnv.ts
@@ -18,10 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     console.log(error, 'errpr-');
   }
 
-  const data = getTemplateEnvs(user_namespace);
-  if (process.env.SEALOS_USER_DOMAIN) data['SEALOS_CLOUD_DOMAIN'] = process.env.SEALOS_USER_DOMAIN;
-
   jsonRes<EnvResponse>(res, {
-    data
+    data: getTemplateEnvs(user_namespace)
   });
 }
