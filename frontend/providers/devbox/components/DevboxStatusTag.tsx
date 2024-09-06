1767c885d2247cc891e61100a6cabd0d5925538f
mlhiter <3076438032@qq.com>
2024-09-06 16:59:59 +0800

    chore: remove unused code

--- frontend/providers/devbox/components/DevboxStatusTag.tsx
+++ frontend/providers/devbox/components/DevboxStatusTag.tsx
@@ -15,26 +15,24 @@ const DevboxStatusTag = ({
   const t = useTranslations()
 
   return (
-    <>
-      <Flex
-        color={status.color}
-        backgroundColor={status.backgroundColor}
-        border={showBorder ? '1px solid' : 'none'}
-        borderColor={status.color}
-        py={2}
-        px={3}
-        borderRadius={'24px'}
-        fontSize={'xs'}
-        fontWeight={'bold'}
-        alignItems={'center'}
-        minW={'88px'}
-        whiteSpace={'nowrap'}>
-        <Box w={'6px'} h={'6px'} borderRadius={'10px'} backgroundColor={status.dotColor}></Box>
-        <Box ml={2} flex={1}>
-          {t(label)}
-        </Box>
-      </Flex>
-    </>
+    <Flex
+      color={status.color}
+      backgroundColor={status.backgroundColor}
+      border={showBorder ? '1px solid' : 'none'}
+      borderColor={status.color}
+      py={2}
+      px={3}
+      borderRadius={'24px'}
+      fontSize={'xs'}
+      fontWeight={'bold'}
+      alignItems={'center'}
+      minW={'88px'}
+      whiteSpace={'nowrap'}>
+      <Box w={'6px'} h={'6px'} borderRadius={'10px'} backgroundColor={status.dotColor}></Box>
+      <Box ml={2} flex={1}>
+        {t(label)}
+      </Box>
+    </Flex>
   )
 }
 
