bab49b738cb9a23d86a398d35a45f6ab9b3e32b5
zijiren <84728412+zijiren233@users.noreply.github.com>
2024-09-06 16:12:26 +0800

    feat: applaunchpad log previous (#5047)
    * feat: applaunchpad support previous and sincetime
    
    * feat: since i18n
    
    * fix: stream prefer flush header
    
    * fix: previous cannot prefer flush header
    
    * feat: no_logs_for_now

--- frontend/providers/applaunchpad/src/pages/app/detail/components/LogsModal.tsx
+++ frontend/providers/applaunchpad/src/pages/app/detail/components/LogsModal.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { getPodLogs } from '@/api/app';
 import {
   Modal,
@@ -16,11 +16,47 @@ import { useLoading } from '@/hooks/useLoading';
 import { downLoadBold } from '@/utils/tools';
 import styles from '../index.module.scss';
 import { SealosMenu } from '@sealos/ui';
-
+import Empty from './empty';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { streamFetch } from '@/services/streamFetch';
 import { default as AnsiUp } from 'ansi_up';
 import { useTranslation } from 'next-i18next';
+
+interface sinceItem {
+  key: 'streaming_logs' | 'within_5_minutes' | 'within_1_hour' | 'within_1_day' | 'terminated_logs';
+  since: number;
+  previous: boolean;
+}
+
+const newSinceItems = (baseTimestamp: number): sinceItem[] => {
+  return [
+    {
+      key: 'streaming_logs',
+      since: 0,
+      previous: false
+    },
+    {
+      key: 'within_5_minutes',
+      since: baseTimestamp - 5 * 60 * 1000,
+      previous: false
+    },
+    {
+      key: 'within_1_hour',
+      since: baseTimestamp - 60 * 60 * 1000,
+      previous: false
+    },
+    {
+      key: 'within_1_day',
+      since: baseTimestamp - 24 * 60 * 60 * 1000,
+      previous: false
+    },
+    {
+      key: 'terminated_logs',
+      since: 0,
+      previous: true
+    }
+  ];
+};
 
 const LogsModal = ({
   appName,
@@ -44,6 +80,20 @@ const LogsModal = ({
   const [isLoading, setIsLoading] = useState(true);
   const LogBox = useRef<HTMLDivElement>(null);
   const ansi_up = useRef(new AnsiUp());
+  const [sinceKey, setSinceKey] = useState('streaming_logs');
+  const [sinceTime, setSinceTime] = useState(0);
+  const [previous, setPrevious] = useState(false);
+
+  const switchSince = useCallback(
+    (item: sinceItem) => {
+      setSinceKey(item.key);
+      setPrevious(item.previous);
+      setSinceTime(item.since);
+    },
+    [setSinceKey, setPrevious, setSinceTime]
+  );
+
+  const sinceItems = useMemo(() => newSinceItems(Date.now()), []);
 
   const watchLogs = useCallback(() => {
     // podName is empty. pod may  has been deleted
@@ -56,12 +106,14 @@ const LogsModal = ({
       data: {
         appName,
         podName,
-        stream: true
+        stream: true,
+        sinceTime,
+        previous
       },
       abortSignal: controller,
       firstResponse() {
-        setIsLoading(false);
         setLogs('');
+        setIsLoading(false);
         setTimeout(() => {
           if (!LogBox.current) return;
 
@@ -91,7 +143,7 @@ const LogsModal = ({
       }
     });
     return controller;
-  }, [appName, closeFn, podName]);
+  }, [appName, closeFn, podName, sinceTime, previous]);
 
   useEffect(() => {
     const controller = watchLogs();
@@ -101,13 +153,19 @@ const LogsModal = ({
   }, [watchLogs]);
 
   const exportLogs = useCallback(async () => {
-    const allLogs = await getPodLogs({
-      appName,
-      podName,
-      stream: false
-    });
-    downLoadBold(allLogs, 'text/plain', 'log.txt');
-  }, [appName, podName]);
+    try {
+      const allLogs = await getPodLogs({
+        appName,
+        podName,
+        stream: false,
+        sinceTime,
+        previous
+      });
+      downLoadBold(allLogs, 'text/plain', 'log.txt');
+    } catch (e) {
+      console.log('download log error:', e);
+    }
+  }, [appName, podName, sinceTime, previous]);
 
   return (
     <Modal isOpen={true} onClose={closeFn} isCentered={true} lockFocusAcrossFrames={false}>
@@ -143,6 +201,31 @@ const LogsModal = ({
                 }))}
               />
             </Box>
+            <Box px={3} zIndex={10000}>
+              <SealosMenu
+                width={200}
+                Button={
+                  <MenuButton
+                    minW={'200px'}
+                    h={'32px'}
+                    textAlign={'start'}
+                    bg={'grayModern.100'}
+                    border={theme.borders.base}
+                    borderRadius={'md'}
+                  >
+                    <Flex px={4} alignItems={'center'}>
+                      <Box flex={1}>{t(sinceKey)}</Box>
+                      <ChevronDownIcon ml={2} />
+                    </Flex>
+                  </MenuButton>
+                }
+                menuList={sinceItems.map((item) => ({
+                  isActive: item.key === sinceKey,
+                  child: <Box>{t(item.key)}</Box>,
+                  onClick: () => switchSince(item)
+                }))}
+              />
+            </Box>
             <Button size={'sm'} onClick={exportLogs}>
               {t('Export')}
             </Button>
@@ -150,16 +233,21 @@ const LogsModal = ({
           <ModalCloseButton />
         </ModalHeader>
         <Box flex={'1 0 0'} h={0} position={'relative'} px={'36px'} mt={'24px'}>
-          <Box
-            ref={LogBox}
-            h={'100%'}
-            whiteSpace={'pre'}
-            pb={2}
-            overflow={'auto'}
-            fontWeight={400}
-            fontFamily={'SFMono-Regular,Menlo,Monaco,Consolas,monospace'}
-            dangerouslySetInnerHTML={{ __html: logs }}
-          ></Box>
+          {logs === '' ? (
+            <Empty />
+          ) : (
+            <Box
+              ref={LogBox}
+              h={'100%'}
+              whiteSpace={'pre'}
+              pb={2}
+              overflow={'auto'}
+              fontWeight={400}
+              fontFamily={'SFMono-Regular,Menlo,Monaco,Consolas,monospace'}
+              dangerouslySetInnerHTML={{ __html: logs }}
+            ></Box>
+          )}
+
           <Loading loading={isLoading} fixed={false} />
         </Box>
       </ModalContent>
