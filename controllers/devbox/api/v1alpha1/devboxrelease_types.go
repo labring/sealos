05a848a26122959d140fb79cc5caed5a09d1c4a8
抒情熊 <2669184984@qq.com>
2024-09-09 18:05:55 +0800

    add release old tag

--- controllers/devbox/api/v1alpha1/devboxrelease_types.go
+++ controllers/devbox/api/v1alpha1/devboxrelease_types.go
@@ -41,11 +41,6 @@ const (
 	DevboxReleasePhaseFailed DevboxReleasePhase = "Failed"
 )
 
-// +kubebuilder:printcolumn:name="DevboxName",type="string",JSONPath=".spec.devboxName"
-// +kubebuilder:printcolumn:name="NewTag",type="string",JSONPath=".spec.newTag"
-// +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase"
-// +kubebuilder:printcolumn:name="OriginalImage",type="string",JSONPath=".status.originalImage"
-
 // DevBoxReleaseStatus defines the observed state of DevBoxRelease
 type DevBoxReleaseStatus struct {
 	// +kubebuilder:validation:Optional
@@ -57,6 +52,10 @@ type DevBoxReleaseStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="DevboxName",type="string",JSONPath=".spec.devboxName"
+// +kubebuilder:printcolumn:name="NewTag",type="string",JSONPath=".spec.newTag"
+// +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase"
+// +kubebuilder:printcolumn:name="OriginalImage",type="string",JSONPath=".status.originalImage"
 
 // DevBoxRelease is the Schema for the devboxreleases API
 type DevBoxRelease struct {
