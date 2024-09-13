6338cf80af7bfe451614feac8afaede3acc0f672
zijiren <84728412+zijiren233@users.noreply.github.com>
2024-09-13 13:50:49 +0800

    feat: db log (#5069)
    * feat: db log previous
    
    * fix: translate key type
    
    * fix: remove v8 compile cache
    
    * feat: db migrate previous log

--- frontend/providers/dbprovider/src/pages/api/migrate/getLogByName.ts
+++ frontend/providers/dbprovider/src/pages/api/migrate/getLogByName.ts
@@ -7,6 +7,57 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { PassThrough } from 'stream';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResp>) {
+  const { kc, k8sCore, namespace } = await getK8s({
+    kubeconfig: await authSession(req)
+  });
+
+  const {
+    podName,
+    containerName,
+    stream = false,
+    logSize,
+    previous,
+    sinceTime
+  } = req.body as {
+    containerName: string;
+    podName: string;
+    stream: boolean;
+    logSize?: number;
+    previous?: boolean;
+    sinceTime?: number;
+  };
+
+  if (!podName) {
+    throw new Error('podName is empty');
+  }
+
+  if (!stream) {
+    const sinceSeconds =
+      sinceTime && !!!previous ? Math.floor((Date.now() - sinceTime) / 1000) : undefined;
+    try {
+      const { body: data } = await k8sCore.readNamespacedPodLog(
+        podName,
+        namespace,
+        containerName,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        previous,
+        sinceSeconds,
+        logSize
+      );
+      return jsonRes(res, {
+        data
+      });
+    } catch (error: any) {
+      jsonRes(res, {
+        code: 500,
+        error
+      });
+    }
+  }
+
   let streamResponse: any;
   const logStream = new PassThrough();
 
@@ -28,76 +79,45 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     destroyStream();
   });
 
+  res.setHeader('Content-Type', 'text/event-stream;charset-utf-8');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('X-Accel-Buffering', 'no');
+  res.setHeader('Cache-Control', 'no-cache, no-transform');
+
+  res.flushHeaders();
+
+  logStream.pipe(res);
+
+  const reqData = {
+    follow: true,
+    pretty: false,
+    timestamps: false,
+    tailLines: 1000,
+    previous: !!previous
+  } as any;
+  if (!reqData.previous && sinceTime) {
+    reqData.sinceTime = timestampToRFC3339(sinceTime);
+  }
+
   try {
-    const {
-      podName,
-      containerName,
-      stream = false,
-      logSize
-    } = req.body as {
-      containerName: string;
-      podName: string;
-      stream: boolean;
-      logSize?: number;
-    };
-
-    if (!podName) {
-      throw new Error('podName is empty');
-    }
-    const { kc, k8sCore, namespace } = await getK8s({
-      kubeconfig: await authSession(req)
-    });
-
-    if (!stream) {
-      // get pods
-      try {
-        const { body: data } = await k8sCore.readNamespacedPodLog(
-          podName,
-          namespace,
-          containerName,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          logSize
-        );
-        return jsonRes(res, {
-          data
-        });
-      } catch (error: any) {
-        if (error?.body?.code === 400) {
-          return jsonRes(res, {
-            data: error?.body?.message
-          });
-        }
-      }
-    }
-
     const logs = new Log(kc);
-
-    res.setHeader('Content-Type', 'text/event-stream;charset-utf-8');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('X-Accel-Buffering', 'no');
-    res.setHeader('Cache-Control', 'no-cache, no-transform');
-    logStream.pipe(res);
-
     streamResponse = await logs.log(
       namespace,
       podName,
       containerName,
       logStream,
       (err) => {
-        console.log('pod log err', err);
+        if (err) {
+          console.log('pod log err', err);
+          res.write(err.toString());
+        }
         destroyStream();
       },
-      { follow: true, pretty: false, timestamps: false, tailLines: 1000 }
+      reqData
     );
-  } catch (err: any) {
-    jsonRes(res, {
-      code: 500,
-      error: err
-    });
-  }
+  } catch (err: any) {}
+}
+
+function timestampToRFC3339(timestamp: number) {
+  return new Date(timestamp).toISOString();
 }
