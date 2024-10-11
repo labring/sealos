881ed1ff0787ec9286e2162daa8ffac8313cd84a
jiahui <bxy4543@gmail.com>
2024-10-11 11:14:50 +0800

    Modify the dbprovider cost estimate obtained through the account service

--- frontend/providers/dbprovider/src/pages/api/platform/resourcePrice.ts
+++ frontend/providers/dbprovider/src/pages/api/platform/resourcePrice.ts
@@ -1,8 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import * as yaml from 'js-yaml';
-import { getK8s } from '@/services/backend/kubernetes';
 import { jsonRes } from '@/services/backend/response';
 import { authSession } from '@/services/backend/auth';
+import type { userPriceType } from '@/types/user';
 
 export type Response = {
   cpu: number;
@@ -10,6 +9,17 @@ export type Response = {
   storage: number;
   nodeports: number;
 };
+
+type ResourcePriceType = {
+  data: {
+    properties: {
+      name: string;
+      unit_price: number;
+      unit: string;
+    }[];
+  };
+};
+
 type ResourceType =
   | 'cpu'
   | 'infra-cpu'
@@ -21,75 +31,50 @@ type ResourceType =
   | 'infra-memory'
   | 'infra-disk'
   | 'services.nodeports';
-type PriceCrdType = {
-  apiVersion: 'account.sealos.io/v1';
-  kind: 'PriceQuery';
-  status: {
-    billingRecords: {
-      price: number;
-      resourceType: ResourceType;
-    }[];
-  };
-};
+
 const PRICE_SCALE = 1000000;
 
 export const valuationMap: Record<string, number> = {
   cpu: 1000,
   memory: 1024,
-  storage: 1024
+  storage: 1024,
+  'services.nodeports': 1000
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    // source price
-    const { applyYamlList, k8sCustomObjects, namespace } = await getK8s({
-      kubeconfig: await authSession(req)
-    });
+    const priceResponse = await getResourcePrice();
 
-    const crdJson = {
-      apiVersion: `account.sealos.io/v1`,
-      kind: 'PriceQuery',
-      metadata: {
-        name: 'prices',
-        namespace
-      },
-      spec: {}
-    };
-
-    const crdYaml = yaml.dump(crdJson);
-
-    try {
-      await applyYamlList([crdYaml], 'replace');
-      await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000));
-    } catch (error) {}
-
-    const { body: priceResponse } = (await k8sCustomObjects.getNamespacedCustomObject(
-      'account.sealos.io',
-      'v1',
-      namespace,
-      'pricequeries',
-      crdJson.metadata.name
-    )) as { body: PriceCrdType };
-
-    const data = {
+    const data: userPriceType = {
       cpu: countSourcePrice(priceResponse, 'cpu'),
       memory: countSourcePrice(priceResponse, 'memory'),
       storage: countSourcePrice(priceResponse, 'storage'),
       nodeports: countSourcePrice(priceResponse, 'services.nodeports')
     };
 
-    jsonRes(res, {
+    jsonRes<userPriceType>(res, {
       data
     });
   } catch (error) {
+    console.log(error);
     jsonRes(res, { code: 500, message: 'get price error' });
   }
 }
 
-function countSourcePrice(rawData: PriceCrdType, type: ResourceType) {
-  const rawPrice =
-    rawData?.status?.billingRecords.find((item) => item.resourceType === type)?.price || 1;
+function countSourcePrice(rawData: ResourcePriceType['data']['properties'], type: ResourceType) {
+  const rawPrice = rawData.find((item) => item.name === type)?.unit_price || 1;
   const sourceScale = rawPrice * (valuationMap[type] || 1);
   const unitScale = sourceScale / PRICE_SCALE;
   return unitScale;
 }
+
+const getResourcePrice = async () => {
+  const url = process.env.BILLING_URL;
+
+  const res = await fetch(`${url}/account/v1alpha1/properties`, {
+    method: 'POST'
+  });
+  const data: ResourcePriceType = await res.json();
+
+  return data.data.properties;
+};
