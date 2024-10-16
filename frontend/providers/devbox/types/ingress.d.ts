84e78b897970db8e5f72c48a13036343dd257960
zjy <3161362058@qq.com>
2024-10-16 17:57:53 +0800

    update ingress

--- frontend/providers/devbox/types/ingress.d.ts
+++ frontend/providers/devbox/types/ingress.d.ts
@@ -1,29 +1,6 @@
 export interface IngressListItemType {
-  metadata: {
-    name: string
-    namespace: string
-    creationTimestamp?: Date
-    labels?: { [key: string]: string }
-  }
-  spec: {
-    rules: {
-      host: string
-      http: {
-        paths: {
-          path: string
-          pathType: string
-          backend: {
-            service: {
-              name: string
-              port: number
-            }
-          }
-        }[]
-      }
-    }[]
-    tls?: {
-      hosts: string[]
-      secretName: string
-    }[]
-  }
+  name: string
+  namespace: string
+  host: string
+  port: number
 }
