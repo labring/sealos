309b0635d930fa03f67685e057a7343fbf886884
谢金虎 <1050780355@qq.com>
2024-10-23 11:38:03 +0800

    add quota monitor

---
+++ service/exceptionmonitor/helper/monitor/quota_monitor.go
@@ -0,0 +1,121 @@
+package monitor
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/labring/sealos/service/exceptionmonitor/api"
+	"github.com/labring/sealos/service/exceptionmonitor/helper/notification"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func QuotaMonitor() {
+	for api.QuotaMonitor {
+		if err := checkQuota(); err != nil {
+			log.Printf("Failed to check qouta: %v", err)
+		}
+		time.Sleep(1 * time.Hour)
+	}
+}
+
+func checkQuota() error {
+	namespaceList, _ := api.ClientSet.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+
+	for _, ns := range namespaceList.Items {
+		if !strings.Contains(ns.Name, "ns-") {
+			continue
+		}
+		quotaList, err := api.ClientSet.CoreV1().ResourceQuotas(ns.Name).List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			log.Printf("Failed to check quota: %v", err)
+		}
+		//user debt
+		if len(quotaList.Items) > 1 {
+			continue
+		}
+		nsQuota := notification.NameSpaceQuota{}
+		notificationInfo := notification.Info{
+			ExceptionType:    "Quota",
+			PerformanceType:  "Quota",
+			NotificationType: "exception",
+		}
+		nsQuota.NameSpace = ns.Name
+		processQuota(quotaList, nsQuota)
+		message := notification.GetQuotaMessage(nsQuota)
+		if err := notification.SendFeishuNotification(notificationInfo, message, api.FeishuWebhookURLMap["FeishuWebhookURLOther"]); err != nil {
+			log.Printf("Error sending exception notification:%v", err)
+		}
+	}
+	return nil
+}
+
+func processQuota(quotaList *v1.ResourceQuotaList, nsQuota notification.NameSpaceQuota) {
+	for resourceName, hardQuantity := range quotaList.Items[0].Status.Hard {
+		usedQuantity, exists := quotaList.Items[0].Status.Used[resourceName]
+		if !exists {
+			usedQuantity = *resource.NewQuantity(0, hardQuantity.Format)
+		}
+
+		hardValue, err := quantityToFloat64(hardQuantity, string(resourceName))
+		if err != nil {
+			log.Printf("Analytic hard constraint error : %s , %v\n", resourceName, err)
+			continue
+		}
+
+		usedValue, err := quantityToFloat64(usedQuantity, string(resourceName))
+		if err != nil {
+			fmt.Printf("Analytic usage error :  %s , %v\n", resourceName, err)
+			continue
+		}
+
+		// usage
+		utilization := (usedValue / hardValue) * 100
+
+		if utilization > api.QuotaThreshold {
+			switch resourceName {
+			case "limits.cpu":
+				nsQuota.CPULimit = hardQuantity.String()
+				nsQuota.CPUUsage = fmt.Sprintf("%.2f%%", utilization)
+			case "limits.memory":
+				nsQuota.MemLimit = hardQuantity.String()
+				nsQuota.MemUsage = fmt.Sprintf("%.2f%%", utilization)
+			case "limits.nvidia.com/gpu":
+				nsQuota.GPULimit = hardQuantity.String()
+				nsQuota.GPUUsage = fmt.Sprintf("%.2f%%", utilization)
+			case "limits.ephemeral-storage":
+				nsQuota.EphemeralStorageLimit = hardQuantity.String()
+				nsQuota.EphemeralStorageUsage = fmt.Sprintf("%.2f%%", utilization)
+			case "objectstorage/size":
+				nsQuota.ObjectStorageLimit = hardQuantity.String()
+				nsQuota.ObjectStorageUsage = fmt.Sprintf("%.2f%%", utilization)
+			case "services.nodeports":
+				nsQuota.NodePortLimit = hardQuantity.String()
+				nsQuota.NodePortUsage = fmt.Sprintf("%.2f%%", utilization)
+			case "requests.storage":
+				nsQuota.StorageLimit = hardQuantity.String()
+				nsQuota.StorageUsage = fmt.Sprintf("%.2f%%", utilization)
+			}
+		}
+	}
+}
+
+func quantityToFloat64(q resource.Quantity, resourceName string) (float64, error) {
+	switch resourceName {
+	case "limits.cpu", "requests.cpu", "cpu",
+		"limits.nvidia.com/gpu", "requests.nvidia.com/gpu":
+		return float64(q.MilliValue()), nil
+	case "limits.memory", "requests.memory", "memory",
+		"limits.ephemeral-storage", "requests.ephemeral-storage",
+		"requests.storage", "objectstorage/size":
+		return float64(q.Value()), nil
+	case "services.nodeports":
+		return float64(q.Value()), nil
+	default:
+		return q.AsApproximateFloat64(), nil
+	}
+}
