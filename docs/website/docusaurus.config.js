6e405b933569fa6d2950ebbbea12a681fb51ed2c
zjy <3161362058@qq.com>
2024-10-30 14:48:09 +0800

    update

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -284,20 +284,27 @@ const config = {
               {
                 tagName: 'script',
                 innerHTML: `
-                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-5953N4CP');
-                `,
-              },
-            ],
-            preBodyTags: [
-              {
-                tagName: 'noscript',
-                innerHTML: `
-                  <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5953N4CP"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                  (function() {
+                    const hostname = window.location.hostname;
+                    if (hostname !== 'sealos.run') {
+                      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','GTM-5953N4CP');
+    
+                      // 添加 noscript iframe
+                      const noscript = document.createElement('noscript');
+                      const iframe = document.createElement('iframe');
+                      iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-5953N4CP';
+                      iframe.height = '0';
+                      iframe.width = '0';
+                      iframe.style.display = 'none';
+                      iframe.style.visibility = 'hidden';
+                      noscript.appendChild(iframe);
+                      document.body.appendChild(noscript);
+                    }
+                  })();
                 `,
               },
             ],
