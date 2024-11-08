fb4d3ace124028114500274db56d6e35a374598b
zjy <3161362058@qq.com>
2024-11-08 15:42:59 +0800

    docs:update google script

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -296,6 +296,27 @@ const config = {
                   })();
                 `,
               },
+              {
+                tagName: 'script',
+                innerHTML: `
+                    (function (w, d, s, l, i) {
+                      w[l] = w[l] || []
+                      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+                      var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s),
+                        dl = l != 'dataLayer' ? '&l=' + l : ''
+                      j.async = true
+                      j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i + dl
+                      f.parentNode.insertBefore(j, f)
+
+                      // Initialize dataLayer and gtag function
+                      window.dataLayer = window.dataLayer || []
+                      function gtag () { dataLayer.push(arguments) }
+                      gtag('js', new Date())
+                      gtag('config', 'G-YF5VHZSTE0')
+                    })(window, document, 'script', 'dataLayer', 'G-YF5VHZSTE0')
+                `,
+              }
             ],
             preBodyTags: [
               {
