e76c956f210c1db9633ec16e68905bc33653dc71
yy <lingdie.yy@outlook.com>
2024-11-13 12:15:41 +0800

    step2: change pod create logic from devbox config, delete runtime and runtimeclass crd.

--- controllers/devbox/api/v1alpha1/devbox_types.go
+++ controllers/devbox/api/v1alpha1/devbox_types.go
@@ -54,19 +54,53 @@ const (
 
 type ResourceList map[ResourceName]resource.Quantity
 
-type RuntimeRef struct {
-	// +kubebuilder:validation:Required
-	Name string `json:"name"`
-	// +kubebuilder:validation:Optional
-	Namespace string `json:"namespace,omitempty"`
-}
-
 type NetworkSpec struct {
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=NodePort;Tailnet
 	Type NetworkType `json:"type"`
 	// +kubebuilder:validation:Optional
 	ExtraPorts []corev1.ContainerPort `json:"extraPorts"`
+}
+
+type Config struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=devbox
+	User string `json:"user"`
+
+	// +kubebuilder:validation:Optional
+	Labels map[string]string `json:"labels,omitempty"`
+	// +kubebuilder:validation:Optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Command []string `json:"command,omitempty"`
+	// kubebuilder:validation:Optional
+	Args []string `json:"args,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=/home/devbox/project
+	WorkingDir string `json:"workingDir,omitempty"`
+	// +kubebuilder:validation:Optional
+	Env []corev1.EnvVar `json:"env,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default={/bin/bash,-c}
+	ReleaseCommand []string `json:"releaseCommand,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default={/home/devbox/project/entrypoint.sh}
+	ReleaseArgs []string `json:"releaseArgs,omitempty"`
+
+	// TODO: in v1alpha2 api we need fix the port and app port into one field and create a new type for it.
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default={{name:"devbox-ssh-port",containerPort:22,protocol:TCP}}
+	Ports []corev1.ContainerPort `json:"ports,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default={{name:"devbox-app-port",port:8080,protocol:TCP}}
+	AppPorts []corev1.ServicePort `json:"appPorts,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	VolumeMounts []corev1.VolumeMount `json:"volumeMounts,omitempty"`
+	// +kubebuilder:validation:Optional
+	Volumes []corev1.Volume `json:"volumes,omitempty"`
 }
 
 // DevboxSpec defines the desired state of Devbox
@@ -81,16 +115,13 @@ type DevboxSpec struct {
 	// +kubebuilder:default=false
 	Squash bool `json:"squash"`
 
-	// +kubebuilder:validation:Optional
-	RuntimeRef RuntimeRef `json:"runtimeRef"`
+	// +kubebuilder:validation:Required
+	Image string `json:"image"`
 
 	// +kubebuilder:validation:Optional
 	TemplateID string `json:"templateID"`
 
-	// +kubebuilder:validation:Optional
-	Image string `json:"image"`
-
-	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Required
 	Config Config `json:"config"`
 
 	// +kubebuilder:validation:Required
@@ -176,8 +207,6 @@ type DevboxStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="State",type="string",JSONPath=".spec.state"
-// +kubebuilder:printcolumn:name="RuntimeRef",type="string",JSONPath=".spec.runtimeRef.name"
-// +kubebuilder:printcolumn:name="PodPhase",type="string",JSONPath=".status.podPhase"
 // +kubebuilder:printcolumn:name="NetworkType",type="string",JSONPath=".status.network.type"
 // +kubebuilder:printcolumn:name="NodePort",type="integer",JSONPath=".status.network.nodePort"
 // +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase"
