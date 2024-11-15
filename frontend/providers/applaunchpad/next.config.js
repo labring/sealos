9ff451202f69ea959b81af8553d040da8ecb9eac
zjy <3161362058@qq.com>
2024-11-15 16:55:48 +0800

    add cors

--- frontend/providers/applaunchpad/next.config.js
+++ frontend/providers/applaunchpad/next.config.js
@@ -20,7 +20,21 @@ const nextConfig = {
   transpilePackages: ['@sealos/driver', '@sealos/ui'],
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../')
-  }
+  },
+  async headers () {
+    return [
+      {
+        // 匹配所有 API 路由
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+          { key: "Access-Control-Allow-Credentials", value: "true" }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
