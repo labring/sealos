1baa495799f17ad4cd87a2ae24031a41d4da7e43
mlhiter <3076438032@qq.com>
2024-11-29 17:42:38 +0800

    fix: hydro bug

--- frontend/providers/devbox/components/modals/jetbrainsGuideModal.tsx
+++ frontend/providers/devbox/components/modals/jetbrainsGuideModal.tsx
@@ -69,6 +69,7 @@ const JetBrainsGuideModal = ({
         <ModalContent top={'5%'} maxWidth={'800px'} w={'700px'} h={'80%'} position={'relative'}>
           <ModalHeader pl={10}>{t('use_jetbrains')}</ModalHeader>
           <ModalBody pb={6} overflowY={'auto'}>
+            {/* prepare */}
             <Box pb={6}>
               <Text fontWeight={'bold'} fontSize={'lg'} mb={6}>
                 {t('jetbrains_guide_prepare')}
@@ -112,12 +113,13 @@ const JetBrainsGuideModal = ({
                 </Text>
               </Flex>
               <Stepper orientation="vertical" index={-1} mt={4} gap={0} position={'relative'}>
+                {/* 1 */}
                 <Step>
                   <StepIndicator backgroundColor={'grayModern.100'} borderColor={'grayModern.100'}>
                     <StepStatus incomplete={<StepNumber />} />
                   </StepIndicator>
                   <Box mt={1} ml={2} mb={5}>
-                    <Text fontSize={'14px'} mb={2}>
+                    <Box fontSize={'14px'} mb={2}>
                       {t.rich('jetbrains_guide_step_1', {
                         blue: (chunks) => (
                           <Text
@@ -128,7 +130,7 @@ const JetBrainsGuideModal = ({
                           </Text>
                         )
                       })}
-                    </Text>
+                    </Box>
                     <Image
                       src={'/images/jetbrains/step1.png'}
                       alt="step1"
@@ -138,15 +140,16 @@ const JetBrainsGuideModal = ({
                   </Box>
                   <StepSeparator />
                 </Step>
+                {/* 2 */}
                 <Step>
                   <StepIndicator backgroundColor={'grayModern.100'} borderColor={'grayModern.100'}>
                     <StepStatus incomplete={<StepNumber />} />
                   </StepIndicator>
-                  <Flex flexShrink="0" mt={1} ml={2} gap={4} direction={'column'}>
+                  <Flex flexShrink="0" mt={1} ml={2} gap={4} direction={'column'} mb={5}>
                     <Flex direction={'column'} gap={1}>
                       <Flex alignItems={'center'} gap={2} mb={2}>
                         <Circle size="6px" bg="#47B2FF" />
-                        <Text fontSize={'14px'}>
+                        <Box fontSize={'14px'}>
                           {t.rich('jetbrains_guide_step_2_1', {
                             blue: (chunks) => (
                               <Text
@@ -157,7 +160,7 @@ const JetBrainsGuideModal = ({
                               </Text>
                             )
                           })}
-                        </Text>
+                        </Box>
                       </Flex>
                       <Grid
                         bg={'grayModern.25'}
@@ -206,7 +209,7 @@ const JetBrainsGuideModal = ({
                     <Flex gap={1} direction={'column'}>
                       <Flex alignItems={'center'} gap={2} mb={2}>
                         <Circle size="6px" bg="#47B2FF" />
-                        <Text fontSize={'md'}>
+                        <Box fontSize={'md'}>
                           {t.rich('jetbrains_guide_step_2_2', {
                             blue: (chunks) => (
                               <Text
@@ -217,7 +220,7 @@ const JetBrainsGuideModal = ({
                               </Text>
                             )
                           })}
-                        </Text>
+                        </Box>
                       </Flex>
                       <Button
                         leftIcon={<MyIcon name="download" color={'grayModern.600'} w={'16px'} />}
@@ -240,7 +243,7 @@ const JetBrainsGuideModal = ({
                     <Flex gap={1} direction={'column'}>
                       <Flex alignItems={'center'} gap={2} mb={2} maxWidth={'600px'}>
                         <Circle size="6px" bg="#47B2FF" />
-                        <Text fontSize={'md'} wordBreak={'break-word'}>
+                        <Box fontSize={'md'} wordBreak={'break-word'}>
                           {t.rich('jetbrains_guide_step_2_3', {
                             blue: (chunks) => (
                               <Text
@@ -251,7 +254,7 @@ const JetBrainsGuideModal = ({
                               </Text>
                             )
                           })}
-                        </Text>
+                        </Box>
                       </Flex>
                       <Image
                         src={'/images/jetbrains/step2-3.png'}
@@ -263,6 +266,7 @@ const JetBrainsGuideModal = ({
                   </Flex>
                   <StepSeparator />
                 </Step>
+                {/* 3 */}
                 <Step>
                   <StepIndicator backgroundColor={'grayModern.100'} borderColor={'grayModern.100'}>
                     <StepStatus incomplete={<StepNumber />} />
@@ -270,7 +274,7 @@ const JetBrainsGuideModal = ({
                   <Box mt={1} ml={2} mb={5}>
                     <Flex alignItems={'center'} gap={2} mb={2}>
                       <Circle size="6px" bg="#47B2FF" />
-                      <Text fontSize={'14px'}>
+                      <Box fontSize={'14px'}>
                         {t.rich('jetbrains_guide_step_3_1', {
                           ideVersion: 'IntelliJ IDEA',
                           blue: (chunks) => (
@@ -282,11 +286,11 @@ const JetBrainsGuideModal = ({
                             </Text>
                           )
                         })}
-                      </Text>
+                      </Box>
                     </Flex>
                     <Flex alignItems={'center'} gap={2} mb={2}>
                       <Circle size="6px" bg="#47B2FF" />
-                      <Text fontSize={'14px'}>
+                      <Box fontSize={'14px'}>
                         {t.rich('jetbrains_guide_step_3_2', {
                           projectPath: 'XXX',
                           blue: (chunks) => (
@@ -298,11 +302,11 @@ const JetBrainsGuideModal = ({
                             </Text>
                           )
                         })}
-                      </Text>
+                      </Box>
                     </Flex>
                     <Flex alignItems={'center'} gap={2} mb={2}>
                       <Circle size="6px" bg="#47B2FF" />
-                      <Text fontSize={'14px'}>
+                      <Box fontSize={'14px'}>
                         {t.rich('jetbrains_guide_step_3_3', {
                           blue: (chunks) => (
                             <Text
@@ -313,7 +317,7 @@ const JetBrainsGuideModal = ({
                             </Text>
                           )
                         })}
-                      </Text>
+                      </Box>
                     </Flex>
                     <Image
                       src={'/images/jetbrains/step3-3.png'}
@@ -333,7 +337,7 @@ const JetBrainsGuideModal = ({
                     />
                     <Flex alignItems={'center'} gap={2} mb={2}>
                       <Circle size="6px" bg="#47B2FF" />
-                      <Text fontSize={'14px'}>
+                      <Box fontSize={'14px'}>
                         {t.rich('jetbrains_guide_step_3_5', {
                           ide: 'IntelliJ IDEA',
                           blue: (chunks) => (
@@ -345,7 +349,7 @@ const JetBrainsGuideModal = ({
                             </Text>
                           )
                         })}
-                      </Text>
+                      </Box>
                     </Flex>
                     <Image
                       src={'/images/jetbrains/step3-4.png'}
@@ -356,6 +360,7 @@ const JetBrainsGuideModal = ({
                   </Box>
                   <StepSeparator />
                 </Step>
+                {/* done */}
                 <Step>
                   <Circle
                     size="10px"
