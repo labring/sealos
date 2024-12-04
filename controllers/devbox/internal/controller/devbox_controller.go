24e4227496913ee201731b1ab1060170e354d096
抒情熊 <2669184984@qq.com>
2024-12-04 10:39:01 +0800

    feat:devbox controller support websocket

--- controllers/devbox/internal/controller/devbox_controller.go
+++ controllers/devbox/internal/controller/devbox_controller.go
@@ -585,7 +585,7 @@ func (r *DevboxReconciler) syncProxyPod(ctx context.Context, devbox *devboxv1alp
 						"-c",
 					},
 					Args: []string{
-						fmt.Sprintf("/app/bin server --port=%d --proxy=https://%s-pod-svc:%s -v=true --reverse && /app/bin client -v localhost:8080 R:2222:https://%s-pod-svc:22",
+						fmt.Sprintf("/app/bin server --port=%d --proxy=https://%s-pod-svc:%s -v=true --reverse & /app/bin client -v localhost:8080 R:2222:https://%s-pod-svc:22",
 							8080,
 							devbox.Name,
 							sshPort,
