706b3128d8904032b6e570249f87e709d11568a1
zijiren233 <pyh1670605849@gmail.com>
2024-12-05 11:42:18 +0800

    feat: log model name

--- service/aiproxy/common/ctxkey/key.go
+++ service/aiproxy/common/ctxkey/key.go
@@ -1,9 +1,13 @@
 package ctxkey
 
-//nolint:gosec
+type OriginalModelKey string
+
 const (
-	OriginalModel = "original_model"
-	Channel       = "channel"
-	Group         = "group"
-	Token         = "token"
+	OriginalModel OriginalModelKey = "original_model"
+)
+
+const (
+	Channel = "channel"
+	Group   = "group"
+	Token   = "token"
 )
--- service/aiproxy/middleware/logger.go
+++ service/aiproxy/middleware/logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/labring/sealos/service/aiproxy/common/ctxkey"
 	"github.com/labring/sealos/service/aiproxy/common/helper"
 )
 
@@ -12,6 +13,22 @@ func SetUpLogger(server *gin.Engine) {
 		var requestID string
 		if param.Keys != nil {
 			requestID = param.Keys[string(helper.RequestIDKey)].(string)
+		}
+		var modelName string
+		if param.Keys != nil {
+			modelName = param.Keys[string(ctxkey.OriginalModel)].(string)
+		}
+		if modelName != "" {
+			return fmt.Sprintf("[GIN] %s | %s | %15s | %3d | %13v | %15s | %7s %s\n",
+				param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+				requestID,
+				modelName,
+				param.StatusCode,
+				param.Latency,
+				param.ClientIP,
+				param.Method,
+				param.Path,
+			)
 		}
 		return fmt.Sprintf("[GIN] %s | %s | %3d | %13v | %15s | %7s %s\n",
 			param.TimeStamp.Format("2006/01/02 - 15:04:05"),
