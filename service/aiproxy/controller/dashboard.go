04a979e763e8c39fa56b23250ce7fea281ce959f
zijiren233 <pyh1670605849@gmail.com>
2024-12-16 14:45:02 +0800

    feat: aiproxy dashboard api

---
+++ service/aiproxy/controller/dashboard.go
@@ -0,0 +1,42 @@
+package controller
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/labring/sealos/service/aiproxy/middleware"
+	"github.com/labring/sealos/service/aiproxy/model"
+)
+
+func GetGroupDashboard(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		middleware.ErrorResponse(c, http.StatusOK, "invalid parameter")
+		return
+	}
+
+	end := time.Now()
+	var start time.Time
+	switch c.Query("type") {
+	case "month":
+		start = end.AddDate(0, 0, -30)
+	case "two_week":
+		start = end.AddDate(0, 0, -14)
+	case "week":
+		start = end.AddDate(0, 0, -7)
+	case "day":
+		fallthrough
+	default:
+		start = end.AddDate(0, 0, -1)
+	}
+	tokenName := c.Query("token_name")
+	modelName := c.Query("model")
+
+	dashboards, err := model.GetDashboardData(id, start, end, tokenName, modelName)
+	if err != nil {
+		middleware.ErrorResponse(c, http.StatusOK, "failed to get statistics")
+		return
+	}
+	middleware.SuccessResponse(c, dashboards)
+}
