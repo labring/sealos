8aa645330f5e9eb7e4ca95de1603020f366e2c5e
EthanD4869 <ethandai4869@gmail.com>
2024-12-17 06:39:56 +0000

    fix sa permission

--- frontend/providers/applaunchpad/src/pages/api/platform/login.ts
+++ frontend/providers/applaunchpad/src/pages/api/platform/login.ts
@@ -7,20 +7,84 @@ import jwt from 'jsonwebtoken';
 
 
 async function getKubeconfigForNamespace(req: NextApiRequest, namespace: string) {
-  const { k8sCore } = await getK8s({
+  const { k8sCore, applyYamlList } = await getK8s({
     kubeconfig: await getAdminAuthorization(req.headers)
   });
-
-  const saName = 'default';
+  const saName = namespace;
   const secretName = `${saName}-token`;
 
   let secret = null;
   try {
-   secret = await k8sCore.readNamespacedSecret(secretName, namespace);
-  }
-  catch (err: any) {
+    secret = await k8sCore.readNamespacedSecret(secretName, namespace);
+  } catch (err: any) {
     if (err.response?.statusCode === 404) {
       try {
+        try {
+          await k8sCore.deleteNamespacedServiceAccount(saName, namespace);
+        }
+        catch (err: any) {
+          console.error(err);
+        }
+        const _resp1 = await k8sCore.createNamespacedServiceAccount(namespace, {
+          apiVersion: 'v1',
+          kind: 'ServiceAccount',
+          metadata: {
+            name: saName,
+            namespace: namespace
+          }
+        });
+
+        const rbacYaml = `
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: ${saName}-role
+  namespace: ${namespace}
+rules:
+- apiGroups: ["*"]
+  resources: ["*"]
+  verbs: ["*"]
+---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: read-only-role
+rules:
+- apiGroups: [""]
+  resources: ["*"]
+  verbs: ["get", "list", "watch"]
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: read-only-binding
+subjects:
+- kind: ServiceAccount
+  name: ${saName}
+  namespace: ${namespace}
+roleRef:
+  kind: ClusterRole
+  name: read-only-role
+  apiGroup: rbac.authorization.k8s.io
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: ${saName}-rolebinding
+  namespace: ${namespace}
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: ${saName}-role
+subjects:
+- namespace: ${namespace}
+  kind: ServiceAccount
+  name: ${saName}
+`;
+
+        await applyYamlList([rbacYaml], 'replace', namespace);
+
         const _resp = await k8sCore.createNamespacedSecret(namespace, {
           apiVersion: 'v1',
           kind: 'Secret',
@@ -35,8 +99,7 @@ async function getKubeconfigForNamespace(req: NextApiRequest, namespace: string)
         });
 
         secret = await k8sCore.readNamespacedSecret(secretName, namespace);
-      }
-      catch (err: any) {
+      } catch (err: any) {
         console.error(err);
         throw new Error('创建 secret 失败');
       }
@@ -46,6 +109,13 @@ async function getKubeconfigForNamespace(req: NextApiRequest, namespace: string)
     }
   }
 
+  try {
+    secret = await k8sCore.readNamespacedSecret(secretName, namespace);
+  }
+  catch (err: any) {
+    console.error(err);
+    throw new Error('获取 secret 失败');
+  }
   const token = secret.body.data?.token ? Buffer.from(secret.body.data.token, 'base64').toString() : '';
 
   if (!token) {
@@ -66,25 +136,24 @@ async function getKubeconfigForNamespace(req: NextApiRequest, namespace: string)
     ],
     contexts: [
       {
-        name: 'default',
+        name: saName,
         context: {
           cluster: 'kubernetes',
           namespace: namespace,
-          user: 'default'
+          user: saName
         }
       }
     ],
-    'current-context': 'default',
+    'current-context': saName,
     users: [
       {
-        name: 'default',
+        name: saName,
         user: {
           token: token
         }
       }
     ]
   };
-
   return kubeconfig;
 }
 
