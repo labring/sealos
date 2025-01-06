94d1936c9b5308b25d2367a12f7d2c8b808345ed
zijiren233 <pyh1670605849@gmail.com>
2025-01-06 15:25:23 +0800

    feat: baichuan model config

--- service/aiproxy/relay/adaptor/baichuan/constants.go
+++ service/aiproxy/relay/adaptor/baichuan/constants.go
@@ -7,18 +7,63 @@ import (
 
 var ModelList = []*model.ModelConfig{
 	{
-		Model: "Baichuan2-Turbo",
-		Type:  relaymode.ChatCompletions,
-		Owner: model.ModelOwnerBaichuan,
+		Model:       "Baichuan4-Turbo",
+		Type:        relaymode.ChatCompletions,
+		Owner:       model.ModelOwnerBaichuan,
+		InputPrice:  0.015,
+		OutputPrice: 0.015,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxContextTokens(32768),
+		),
 	},
 	{
-		Model: "Baichuan2-Turbo-192k",
-		Type:  relaymode.ChatCompletions,
-		Owner: model.ModelOwnerBaichuan,
+		Model:       "Baichuan4-Air",
+		Type:        relaymode.ChatCompletions,
+		Owner:       model.ModelOwnerBaichuan,
+		InputPrice:  0.00098,
+		OutputPrice: 0.00098,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxContextTokens(32768),
+		),
 	},
 	{
-		Model: "Baichuan-Text-Embedding",
-		Type:  relaymode.Embeddings,
-		Owner: model.ModelOwnerBaichuan,
+		Model:       "Baichuan4",
+		Type:        relaymode.ChatCompletions,
+		Owner:       model.ModelOwnerBaichuan,
+		InputPrice:  0.1,
+		OutputPrice: 0.1,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxContextTokens(32768),
+		),
+	},
+	{
+		Model:       "Baichuan3-Turbo",
+		Type:        relaymode.ChatCompletions,
+		Owner:       model.ModelOwnerBaichuan,
+		InputPrice:  0.012,
+		OutputPrice: 0.012,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxContextTokens(32768),
+		),
+	},
+	{
+		Model:       "Baichuan3-Turbo-128k",
+		Type:        relaymode.ChatCompletions,
+		Owner:       model.ModelOwnerBaichuan,
+		InputPrice:  0.024,
+		OutputPrice: 0.024,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxContextTokens(131072),
+		),
+	},
+
+	{
+		Model:      "Baichuan-Text-Embedding",
+		Type:       relaymode.Embeddings,
+		Owner:      model.ModelOwnerBaichuan,
+		InputPrice: 0.0005,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxInputTokens(512),
+		),
 	},
 }
