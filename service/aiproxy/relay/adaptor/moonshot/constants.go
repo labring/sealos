516c427b239e11a4811080aec2929776a0cec3f2
zijiren233 <pyh1670605849@gmail.com>
2025-02-06 14:42:15 +0800

    feat: doubao and moonshot model

--- service/aiproxy/relay/adaptor/moonshot/constants.go
+++ service/aiproxy/relay/adaptor/moonshot/constants.go
@@ -42,4 +42,44 @@ var ModelList = []*model.ModelConfig{
 			model.WithModelConfigToolChoice(true),
 		),
 	},
+
+	{
+		Model:       "moonshot-v1-8k-vision-preview",
+		Type:        relaymode.ChatCompletions,
+		Owner:       model.ModelOwnerMoonshot,
+		InputPrice:  0.012,
+		OutputPrice: 0.012,
+		RPM:         500,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxInputTokens(8192),
+			model.WithModelConfigToolChoice(true),
+			model.WithModelConfigVision(true),
+		),
+	},
+	{
+		Model:       "moonshot-v1-32k-vision-preview",
+		Type:        relaymode.ChatCompletions,
+		Owner:       model.ModelOwnerMoonshot,
+		InputPrice:  0.024,
+		OutputPrice: 0.024,
+		RPM:         500,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxInputTokens(32768),
+			model.WithModelConfigToolChoice(true),
+			model.WithModelConfigVision(true),
+		),
+	},
+	{
+		Model:       "moonshot-v1-128k-vision-preview",
+		Type:        relaymode.ChatCompletions,
+		Owner:       model.ModelOwnerMoonshot,
+		InputPrice:  0.06,
+		OutputPrice: 0.06,
+		RPM:         500,
+		Config: model.NewModelConfig(
+			model.WithModelConfigMaxInputTokens(131072),
+			model.WithModelConfigToolChoice(true),
+			model.WithModelConfigVision(true),
+		),
+	},
 }
