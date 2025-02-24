121448d1fa176fe92617de00e1f0745e2feb66c1
luanshaotong <luanshaotong@163.com>
2025-02-24 11:12:05 +0800

    fix bugs

--- frontend/providers/applaunchpad/src/pages/api/getNamespaces.ts
+++ frontend/providers/applaunchpad/src/pages/api/getNamespaces.ts
@@ -11,7 +11,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     const kubeconfig = await authSession(req.headers);
     const { k8sApp, namespace, k8sCore, kube_user } = await getK8s({ kubeconfig });
     let namespacesList = null;
-    // console.log('kube_user:', kube_user);
     if (kube_user.name === 'kubernetes-admin' || kube_user.name === 'inClusterUser') {
       let result = await k8sCore.listNamespace();
       namespacesList = result.body.items.map((item: any) => item.metadata.name);
