2d3431901a256034c1d8fcf2f2c74e6a840a43cd
zijiren <84728412+zijiren233@users.noreply.github.com>
2025-03-02 22:48:55 +0800

    feat: impl disable billing (#5431)

--- service/aiproxy/relay/controller/price.go
+++ service/aiproxy/relay/controller/price.go
@@ -1,21 +1,14 @@
 package controller
 
 import (
-	"github.com/labring/sealos/service/aiproxy/common/config"
 	"github.com/labring/sealos/service/aiproxy/model"
 )
 
 func GetModelPrice(modelConfig *model.ModelConfig) (float64, float64, bool) {
-	if !config.GetBillingEnabled() {
-		return 0, 0, true
-	}
 	return modelConfig.InputPrice, modelConfig.OutputPrice, true
 }
 
 func GetImageSizePrice(modelConfig *model.ModelConfig, size string) (float64, bool) {
-	if !config.GetBillingEnabled() {
-		return 0, false
-	}
 	if len(modelConfig.ImagePrices) == 0 {
 		return 0, true
 	}
