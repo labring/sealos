61aaebcfb0da3750eaead8155d4f57450f2a413b
Tony <zhangdy2027@gmail.com>
2025-03-06 23:37:49 +0800

    type修改

--- frontend/providers/applaunchpad/src/utils/adapt.ts
+++ frontend/providers/applaunchpad/src/utils/adapt.ts
@@ -302,7 +302,7 @@ export const adaptAppDetail = (configs: DeployKindsType[]): AppDetailType => {
     crYamlList: configs,
     id: appDeploy.metadata?.uid || ``,
     appName: appDeploy.metadata?.name || 'app Name',
-    modelName: '',
+    modelName: appDeploy.metadata?.annotations?.[modelNameKey] || '',
     createTime: dayjs(appDeploy.metadata?.creationTimestamp).format('YYYY-MM-DD HH:mm'),
     status: appStatusMap.waiting,
     isPause: !!appDeploy?.metadata?.annotations?.[pauseKey],
