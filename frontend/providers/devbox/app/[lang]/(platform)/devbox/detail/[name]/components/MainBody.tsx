1d2363d935cc08843bd2bcb10f3aa91a4656c2d2
zjy <3161362058@qq.com>
2025-03-07 23:50:47 +0800

    update devbox network status

--- frontend/providers/devbox/app/[lang]/(platform)/devbox/detail/[name]/components/MainBody.tsx
+++ frontend/providers/devbox/app/[lang]/(platform)/devbox/detail/[name]/components/MainBody.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Text, Tooltip, useDisclosure } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, Text, Tooltip, useDisclosure } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 import { useTranslations } from 'next-intl';
 import dynamic from 'next/dynamic';
@@ -12,6 +12,9 @@ import { useCopyData } from '@/utils/tools';
 
 import { useDevboxStore } from '@/stores/devbox';
 import { useEnvStore } from '@/stores/env';
+import { useMemo, useRef } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { checkReady } from '@/api/platform';
 
 const MonitorModal = dynamic(() => import('@/components/modals/MonitorModal'));
 
@@ -21,6 +24,45 @@ const MainBody = () => {
   const { devboxDetail } = useDevboxStore();
   const { env } = useEnvStore();
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const retryCount = useRef(0);
+  const { data: networkStatus, refetch } = useQuery({
+    queryKey: ['networkStatus', devboxDetail?.name],
+    queryFn: () => (devboxDetail?.name ? checkReady(devboxDetail?.name) : []),
+    retry: 5,
+    retryDelay: (attemptIndex) => Math.min(1000 * Math.pow(2, attemptIndex), 30000),
+    onSuccess: (data) => {
+      const hasUnready = data.some((item) => !item.ready);
+      if (!hasUnready) {
+        retryCount.current = 0;
+        return;
+      }
+      if (retryCount.current < 5) {
+        const delay = Math.min(1000 * Math.pow(2, retryCount.current), 30000);
+        retryCount.current += 1;
+        setTimeout(() => {
+          refetch();
+        }, delay);
+      }
+    },
+    refetchIntervalInBackground: false,
+    staleTime: 1000 * 60 * 5
+  });
+
+  const statusMap = useMemo(
+    () =>
+      networkStatus
+        ? networkStatus.reduce((acc, item) => {
+            if (item?.url) {
+              acc[item.url] = item;
+            }
+            return acc;
+          }, {} as Record<string, { ready: boolean; url: string }>)
+        : {},
+    [networkStatus]
+  );
+
+  console.log(statusMap, networkStatus, '12313');
 
   const networkColumn: {
     title: string;
@@ -78,33 +120,85 @@ const MainBody = () => {
       render: (item: NetworkType) => {
         if (item.openPublicDomain) {
           const address = item.customDomain || item.publicDomain;
+          const displayAddress = `https://${address}`;
           return (
-            <Tooltip
-              label={t('open_link')}
-              hasArrow
-              bg={'#FFFFFF'}
-              color={'grayModern.900'}
-              fontSize={'12px'}
-              fontWeight={400}
-              py={2}
-              borderRadius={'md'}
-            >
-              <Text
-                className="guide-network-address"
-                cursor="pointer"
-                color={'grayModern.600'}
-                _hover={{ textDecoration: 'underline' }}
-                onClick={() => window.open(`https://${address}`, '_blank')}
+            <Flex gap={'2px'} alignItems={'center'}>
+              {displayAddress && (
+                <>
+                  {statusMap[displayAddress]?.ready ? (
+                    <Center
+                      fontSize={'12px'}
+                      fontWeight={400}
+                      bg={'rgba(3, 152, 85, 0.05)'}
+                      color={'#039855'}
+                      borderRadius={'full'}
+                      p={'2px 4px'}
+                      gap={'2px'}
+                      minW={'63px'}
+                    >
+                      <Center w={'6px'} h={'6px'} borderRadius={'full'} bg={'#039855'}></Center>
+                      {t('Accessible')}
+                    </Center>
+                  ) : (
+                    <Center
+                      fontSize={'12px'}
+                      fontWeight={400}
+                      bg={'rgba(17, 24, 36, 0.05)'}
+                      color={'#485264'}
+                      borderRadius={'full'}
+                      p={'2px 4px'}
+                      gap={'2px'}
+                      minW={'63px'}
+                    >
+                      <MyIcon
+                        name={'loadingCircle'}
+                        w={'12px'}
+                        h={'12px'}
+                        animation={'spin 1s linear infinite'}
+                        sx={{
+                          '@keyframes spin': {
+                            '0%': {
+                              transform: 'rotate(0deg)'
+                            },
+                            '100%': {
+                              transform: 'rotate(360deg)'
+                            }
+                          }
+                        }}
+                      />
+                      {t('prepare')}
+                    </Center>
+                  )}
+                </>
+              )}
+              <Tooltip
+                label={t('open_link')}
+                hasArrow
+                bg={'#FFFFFF'}
+                color={'grayModern.900'}
+                fontSize={'12px'}
+                fontWeight={400}
+                py={2}
+                borderRadius={'md'}
               >
-                https://{address}
-              </Text>
-            </Tooltip>
+                <Text
+                  className="guide-network-address"
+                  cursor="pointer"
+                  color={'grayModern.600'}
+                  _hover={{ textDecoration: 'underline' }}
+                  onClick={() => window.open(`https://${address}`, '_blank')}
+                >
+                  https://{address}
+                </Text>
+              </Tooltip>
+            </Flex>
           );
         }
         return <Text>-</Text>;
       }
     }
   ];
+
   return (
     <Box bg={'white'} borderRadius="lg" pl={6} pt={2} pr={6} pb={6} h={'full'} borderWidth={1}>
       {/* monitor */}
