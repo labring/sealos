2e839f8cecaf0496b9c0e48361ed846f9332523b
zjy <3161362058@qq.com>
2025-03-10 11:50:18 +0800

    update time

--- frontend/providers/devbox/app/[lang]/(platform)/devbox/detail/[name]/components/MainBody.tsx
+++ frontend/providers/devbox/app/[lang]/(platform)/devbox/detail/[name]/components/MainBody.tsx
@@ -37,8 +37,8 @@ const MainBody = () => {
         retryCount.current = 0;
         return;
       }
-      if (retryCount.current < 5) {
-        const delay = Math.min(1000 * Math.pow(2, retryCount.current), 30000);
+      if (retryCount.current < 14) {
+        const delay = Math.min(1000 * Math.pow(2, retryCount.current), 32000);
         retryCount.current += 1;
         setTimeout(() => {
           refetch();
