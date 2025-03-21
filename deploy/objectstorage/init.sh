3e89aae7c5a841ede7a68dd00a00f22ea992e4a8
xuziyi <ziyi@sealos.io>
2025-03-21 12:08:15 +0800

    1

--- deploy/objectstorage/init.sh
+++ deploy/objectstorage/init.sh
@@ -43,11 +43,11 @@ retryPullImage() {
     fi
 }
 
-retryPullImage ghcr.io/labring/sealos-cloud-objectstorage-controller:0320_01
-retryPullImage ghcr.io/labring/sealos-cloud-objectstorage-frontend:0320_01
+retryPullImage ghcr.io/labring/sealos-cloud-objectstorage-controller:latest
+retryPullImage ghcr.io/labring/sealos-cloud-objectstorage-frontend:latest
 retryPullImage ghcr.io/labring/sealos-cloud-minio-service:latest
 retryPullFile https://dl.min.io/client/mc/release/linux-amd64/mc
 
-sealos save -o tars/objectstorage-controller.tar ghcr.io/labring/sealos-cloud-objectstorage-controller:0320_01
-sealos save -o tars/objectstorage-frontend.tar ghcr.io/labring/sealos-cloud-objectstorage-frontend:0320_01
+sealos save -o tars/objectstorage-controller.tar ghcr.io/labring/sealos-cloud-objectstorage-controller:latest
+sealos save -o tars/objectstorage-frontend.tar ghcr.io/labring/sealos-cloud-objectstorage-frontend:latest
 sealos save -o tars/objectstorage-service.tar ghcr.io/labring/sealos-cloud-minio-service:latest
