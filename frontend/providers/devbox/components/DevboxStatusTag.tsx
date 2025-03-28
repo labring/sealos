2a0ac7589161c5ff4f4b22297caf2a1f0e605983
mlhiter <3076438032@qq.com>
2025-03-28 13:40:15 +0800

    feat: basic ui&logic

--- frontend/providers/devbox/components/DevboxStatusTag.tsx
+++ frontend/providers/devbox/components/DevboxStatusTag.tsx
@@ -32,6 +32,55 @@ const DevboxStatusTag = ({
   const label = status?.label;
   const t = useTranslations();
 
+  if (isShutdown) {
+    return (
+      <Popover trigger="hover" placement="bottom-start">
+        <PopoverTrigger>
+          <Flex
+            cursor={'pointer'}
+            className="guide-status-tag"
+            color={status.color}
+            backgroundColor={thinMode ? 'transparent' : status.backgroundColor}
+            border={showBorder ? '1px solid' : 'none'}
+            borderColor={status.color}
+            py={1}
+            px={thinMode ? 0 : 4}
+            borderRadius={'24px'}
+            fontSize={'xs'}
+            fontWeight={'bold'}
+            alignItems={'center'}
+            minW={'60px'}
+            whiteSpace={'nowrap'}
+            {...props}
+          >
+            <Box
+              w={'6px'}
+              h={'6px'}
+              borderRadius={'10px'}
+              backgroundColor={status.dotColor}
+              display={thinMode ? 'none' : 'block'}
+            ></Box>
+            <Box ml={thinMode ? 0 : 2} flex={1}>
+              {t(label)}
+            </Box>
+            <MyIcon name="help" />
+          </Flex>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverBody>
+            <Box>{t('devbox_shutdown_notice1')}</Box>
+            <Box>
+              <Box>{t('notice')}</Box>
+              <Box>{t('devbox_shutdown_notice2')}</Box>
+              <Box>{t('devbox_shutdown_notice3')}</Box>
+            </Box>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    );
+  }
+
   return (
     <Flex alignItems={'center'} gap={'4px'}>
       <Flex
