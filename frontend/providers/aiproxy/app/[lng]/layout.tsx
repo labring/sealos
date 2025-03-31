fb13e3d60aad679bd69fd0e4616a9df6428e56ed
Yiming <wang1m@outlook.com>
2025-03-31 16:20:56 +0800

    feat: custom script in aiproxy/db/devbox

--- frontend/providers/aiproxy/app/[lng]/layout.tsx
+++ frontend/providers/aiproxy/app/[lng]/layout.tsx
@@ -43,6 +43,9 @@ export default async function RootLayout({
   params: Promise<{ lng: string }>
 }>): Promise<React.JSX.Element> {
   const lng = (await params).lng
+
+  const scripts: { src: string }[] = JSON.parse(process.env.CUSTOM_SCRIPTS || '[]')
+  
   return (
     <html lang={lng} dir={dir(lng)}>
       <body>
@@ -54,6 +57,9 @@ export default async function RootLayout({
             </QueryProvider>
           </ChakraProviders>
         </I18nProvider>
+        {scripts.map((script, i) => (
+          <script key={i} {...script} />
+        ))}
       </body>
     </html>
   )
