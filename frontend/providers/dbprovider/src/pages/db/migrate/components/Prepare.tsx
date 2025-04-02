94dc15624866b531fde4ab9383d3696d1c697426
Yiming <wang1m@outlook.com>
2025-04-02 16:44:46 +0800

    docs: prettify `db/migrate`'s Prepare

--- frontend/providers/dbprovider/src/pages/db/migrate/components/Prepare.tsx
+++ frontend/providers/dbprovider/src/pages/db/migrate/components/Prepare.tsx
@@ -36,7 +36,7 @@ export default function PrepareBox({
           "set binlog_row_image ='FULL';  "
         ],
         checkboxLabel: t('migration_preparations'),
-        permissionCheck: `source account: REPLICATION SLAVE、REPLICATION CLIENT、SELECT \nsink account: SELECT、INSERT、UPDATE、DELETE、CREATE、ALTER、DROP`,
+        permissionCheck: `<b>source account</b>: REPLICATION SLAVE、REPLICATION CLIENT、SELECT \nsink account: SELECT、INSERT、UPDATE、DELETE、CREATE、ALTER、DROP`,
         title: 'MySQL'
       },
       mongodb: {
@@ -75,15 +75,24 @@ export default function PrepareBox({
       <Text mt="20px" fontSize={'16px'} fontWeight={500} color={'#24282C'}>
         {content.title} {t('migration_permission_check')}
       </Text>
-      <Text mt="14px" fontSize={'12px'} fontWeight={400} color={'#24282C'} whiteSpace={'pre'}>
-        {content.permissionCheck}
-      </Text>
+      <Text
+        mt="14px"
+        fontSize={'12px'}
+        fontWeight={400}
+        color={'#24282C'}
+        whiteSpace={'pre'}
+        dangerouslySetInnerHTML={{ __html: content.permissionCheck }}
+      />
       <Text mt="20px" fontSize={'16px'} fontWeight={500} color={'#24282C'}>
         {t('covering_risks')}
       </Text>
-      <Text mt="14px" fontSize={'12px'} fontWeight={400} color={'#24282C'}>
-        {t('important_tips_for_migrating')}
-      </Text>
+      <Text
+        mt="14px"
+        fontSize={'12px'}
+        fontWeight={400}
+        color={'#24282C'}
+        dangerouslySetInnerHTML={{ __html: t('important_tips_for_migrating') }}
+      />
 
       <Checkbox
         isInvalid={!!formHook?.formState?.errors?.isChecked}
