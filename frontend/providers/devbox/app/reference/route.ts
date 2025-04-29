633c4bd79ffcb6bd08631cf5a58998b2f399b529
mlhiter <3076438032@qq.com>
2025-04-29 15:06:17 +0800

    chore: update scalar config

--- frontend/providers/devbox/app/reference/route.ts
+++ frontend/providers/devbox/app/reference/route.ts
@@ -2,27 +2,25 @@ import { ApiReference } from '@scalar/nextjs-api-reference';
 import { openApiDocument } from '../api/openapi';
 
 const config = {
-  spec: {
-    content: openApiDocument,
-    authentication: {
-      preferredSecurityScheme: ['kubeconfigAuth', 'jwtAuth'],
-      securitySchemes: {
-        kubeconfigAuth: {
-          in: 'header',
-          name: 'Authorization',
-          value: process.env.SCALAR_USE_KC // prefill kubeconfig
-        },
-        jwtAuth: {
-          in: 'header',
-          name: 'Authorization-Bearer',
-          value: process.env.SCALAR_USE_TOKEN // prefill jwt token
-        }
+  content: openApiDocument,
+  authentication: {
+    preferredSecurityScheme: ['kubeconfigAuth', 'jwtAuth'],
+    securitySchemes: {
+      kubeconfigAuth: {
+        in: 'header',
+        name: 'Authorization',
+        value: process.env.SCALAR_USE_KC // prefill kubeconfig
+      },
+      jwtAuth: {
+        in: 'header',
+        name: 'Authorization-Bearer',
+        value: process.env.SCALAR_USE_TOKEN // prefill jwt token
       }
-    },
-    cdn: process.env.NEXT_PUBLIC_MOCK_USER
-      ? undefined
-      : `https://devbox.${process.env.SEALOS_DOMAIN}/scalar/cdn.js`
-  }
+    }
+  },
+  cdn: process.env.NEXT_PUBLIC_MOCK_USER
+    ? undefined
+    : `https://devbox.${process.env.SEALOS_DOMAIN}/scalar/cdn.js`
 };
 
 export const GET = ApiReference(config);
