4dcd486090ccacf715827dcbe401a4596492f94c
mlhiter <3076438032@qq.com>
2025-05-07 17:40:49 +0800

    fix: type bug

--- frontend/providers/devbox/app/api/v1/lifecycleDevbox/schema.ts
+++ frontend/providers/devbox/app/api/v1/lifecycleDevbox/schema.ts
@@ -1,7 +1,10 @@
+import 'zod-openapi/extend';
 import { z } from 'zod';
 
 export const RequestSchema = z.object({
-  devboxName: z.string().min(1),
+  devboxName: z.string().min(1).openapi({
+    description: 'Devbox name'
+  }),
   action: z.enum(['start', 'stop', 'restart', 'shutdown']).openapi({
     description:
       'The action to perform on the devbox.Note: stop is to shut down and not release the port, and will continue to charge port fees(a little bit); shutdown is to shut down and release the port, and will not continue to charge any fees'
