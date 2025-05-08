17bc47b7b9fa197e39f4a91c9fd477d18872373f
jingyang <72259332+zjy365@users.noreply.github.com>
2025-05-08 16:09:05 +0800

    refactor: simplify invitation app (#5576)
    * refactor: simplify invitation app
    
    * update

--- frontend/providers/invite/src/pages/api/account/list.ts
+++ frontend/providers/invite/src/pages/api/account/list.ts
@@ -1,26 +1,61 @@
 import { jsonRes } from '@/services/response';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+type TRewardList = {
+  invitee: string;
+  inviter: string;
+  amount: number;
+  arrival_time: string;
+  status: string;
+  registerTime: string;
+};
+
 export type InvitationResult = {
-  totalPeople: string;
+  totalPeople: number;
   totalAmount: number;
-  rewardList: {
-    invitee: string;
-    inviter: string;
-    amount: number;
-    arrival_time: Date;
-    status: 1;
-  }[];
+  completedUsers: number;
+  pendingUsers: number;
+  rewardList: TRewardList[];
+  pagination?: {
+    total: number;
+    currentPage: number;
+    pageSize: number;
+    totalPages: number;
+  };
+};
+
+export const getInvitationList = async (
+  inviterId: string,
+  page: string = '1',
+  pageSize: string = '10'
+) => {
+  const baseUrl = process.env.LAF_BASE_URL;
+  const result = (await fetch(
+    `https://${baseUrl}/listById?inviterId=${inviterId}&page=${page}&pageSize=${pageSize}`
+  ).then((response) => response.json())) as {
+    code: number;
+    data: InvitationResult;
+  };
+  return result.data;
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
-    const { inviterId } = req.query;
-    const baseUrl = process.env.LAF_BASE_URL;
+    const {
+      inviterId,
+      page = '1',
+      pageSize = '10'
+    } = req.query as {
+      inviterId: string;
+      page: string;
+      pageSize: string;
+    };
 
     const defaultObj: InvitationResult = {
       totalAmount: 0,
-      totalPeople: '0',
+      totalPeople: 0,
+      completedUsers: 0,
+      pendingUsers: 0,
       rewardList: []
     };
 
@@ -30,13 +65,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       });
     }
 
-    const result = await fetch(`https://${baseUrl}/listById?inviterId=${inviterId}`).then(
-      (response) => response.json()
-    );
+    const data = await getInvitationList(inviterId, page, pageSize);
 
-    jsonRes(res, result);
+    return jsonRes(res, {
+      data
+    });
   } catch (err) {
-    jsonRes(res, {
+    return jsonRes(res, {
       code: 500,
       error: err
     });
