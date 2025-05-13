c534d91ef47881b3bf32ea58eecbf9ad71f003d8
zjy <3161362058@qq.com>
2025-05-13 15:29:23 +0800

    dev

--- frontend/providers/template/src/pages/_document.tsx
+++ frontend/providers/template/src/pages/_document.tsx
@@ -21,6 +21,17 @@ export default function Document() {
         {scripts.map((script, i) => (
           <script key={i} {...script} async />
         ))}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rf970meahc");
+          `
+          }}
+        />
       </body>
     </Html>
   );
