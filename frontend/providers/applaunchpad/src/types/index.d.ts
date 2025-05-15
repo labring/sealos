caac98d454e1c83a0745c76b86279d463ca6978c
zjy <3161362058@qq.com>
2025-05-15 17:49:58 +0800

    feat: template devbox launchpad support configure tls enable status

--- frontend/providers/applaunchpad/src/types/index.d.ts
+++ frontend/providers/applaunchpad/src/types/index.d.ts
@@ -38,7 +38,7 @@ export type AppConfigType = {
     guideEnabled: boolean;
     apiEnabled: boolean;
     gpuEnabled: boolean;
-    enableHTTPS: boolean;
+    httpsEnabled: boolean;
   };
   launchpad: {
     meta: {
