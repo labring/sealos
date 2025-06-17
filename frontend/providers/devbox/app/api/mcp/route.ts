c6109d55315dda9f886d701118cb4215cd556a5f
抒情熊 <2669184984@qq.com>
2025-06-17 15:29:40 +0800

    fix bug.

--- frontend/providers/devbox/app/api/mcp/route.ts
+++ frontend/providers/devbox/app/api/mcp/route.ts
@@ -1,5 +1,5 @@
 import { McpHandler } from '@mcp-sdk';
 
 
-const handler = McpHandler("./app/api/mcp/devbox.json","http://localhost:3000");
+const handler = McpHandler("./app/api/mcp/devbox.json","http://devbox-frontend.devbox-frontend.svc.cluster.local:3000");
 export { handler as GET, handler as POST, handler as DELETE };
