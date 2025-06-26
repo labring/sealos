a3e771a649ee59c1ad3813968a4b870273819bc2
jiahui <bxy4543@gmail.com>
2025-06-26 16:45:55 +0800

    init

---
+++ controllers/pkg/account/env.go
@@ -0,0 +1,8 @@
+package account
+
+const (
+	PayIsProduction  = "PAY_IS_PRODUCTION"
+	AlipayAppID      = "ALIPAY_APP_ID"
+	AlipayPrivateKey = "ALIPAY_PRIVATE_KEY"
+	AlipayPublicKey  = "ALIPAY_PUBLIC_KEY"
+)
