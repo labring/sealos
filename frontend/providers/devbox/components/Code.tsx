0785f89d7ad061553640b4169401337f075800b8
mlhiter <3076438032@qq.com>
2025-06-27 10:22:05 +0800

    feat: yaml page

--- frontend/providers/devbox/components/Code.tsx
+++ frontend/providers/devbox/components/Code.tsx
@@ -1,55 +1,60 @@
 import { useMemo } from 'react';
-import { Box } from '@chakra-ui/react';
 import ReactMarkdown from 'react-markdown';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 
+import { cn } from '@/lib/utils';
 import { codeTheme } from '@/constants/hljs';
 
-type TMarkDown = {
+interface CodeProps {
   content: string;
   language: string;
-  [key: string]: any;
-};
+  className?: string;
+}
 
-const Code = ({ content, language, ...props }: TMarkDown) => {
+const Code = ({ content, language, className }: CodeProps) => {
   const code = useMemo(() => '```' + language + '\n' + content + '```', [content, language]);
 
   return (
-    <Box
-      sx={{
-        height: '100%',
-        '& div': {
-          overflow: 'auto !important'
-        }
-      }}
-    >
-      <ReactMarkdown
-        {...props}
-        // eslint-disable-next-line react/no-children-prop
-        children={code}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighlighter
-                // eslint-disable-next-line react/no-children-prop
-                children={String(children).replace(/\n$/, '')}
-                showLineNumbers={true}
-                // @ts-ignore nextline
-                style={codeTheme}
-                language={match[1]}
-                PreTag="div"
-                {...props}
-              />
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          }
-        }}
-      />
-    </Box>
+    <div className={cn('h-full w-full rounded-md bg-card text-card-foreground', className)}>
+      <div className="relative h-full overflow-auto">
+        <ReactMarkdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  // eslint-disable-next-line react/no-children-prop
+                  children={String(children).replace(/\n$/, '')}
+                  showLineNumbers={true}
+                  // @ts-ignore nextline
+                  style={codeTheme}
+                  codeTagProps={{
+                    style: {
+                      fontFamily: 'var(--font-fira-code)'
+                    }
+                  }}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code
+                  className={cn(
+                    'rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm',
+                    className
+                  )}
+                  {...props}
+                >
+                  {children}
+                </code>
+              );
+            }
+          }}
+        >
+          {code}
+        </ReactMarkdown>
+      </div>
+    </div>
   );
 };
 
