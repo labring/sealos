89f6620eee4dfbc8ff0dde5e0b6c922d376565c7
mlhiter <3076438032@qq.com>
2025-06-27 10:24:48 +0800

    fix: scrollbar color change

--- frontend/providers/devbox/components/ui/scroll-area.tsx
+++ frontend/providers/devbox/components/ui/scroll-area.tsx
@@ -36,7 +36,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-zinc-100" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
