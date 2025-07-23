8f09a97a315404b50eea2b6854be179dc5f3e1c1
Nixieboluo <me@sagirii.me>
2025-07-23 15:11:10 +0800

    fix(desktop): incorrect title on email/phone change/binding/unbinding modal
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/desktop/src/components/account/AccountCenter/index.tsx
+++ frontend/desktop/src/components/account/AccountCenter/index.tsx
@@ -85,17 +85,6 @@ export default function AccountCenter(props: AccountCenterProps) {
     infoData.refetch();
   };
 
-  const modalTitle = useMemo(() => {
-    if (pageState === PageState.INDEX) return t('common:account_settings');
-    else if (pageState === PageState.PASSWORD) return t('common:changepassword');
-    else if (Object.values(PhoneState).includes(pageState as PhoneState))
-      return t('common:changephone');
-    else if (Object.values(EmailState).includes(pageState as EmailState))
-      return t('common:changeemail');
-    else if (pageState === PageState.REALNAME_AUTH) return t('common:realName_verification');
-    else return '';
-  }, [t, pageState]);
-
   const infoData = useQuery({
     queryFn: UserInfo,
     queryKey: [session?.token, 'UserInfo'],
@@ -145,6 +134,19 @@ export default function AccountCenter(props: AccountCenterProps) {
     state.AVATAR_URL = infoData.data?.avatarUri || '';
     return state;
   }, [infoData.data?.oauthProvider]);
+
+  const modalTitle = useMemo(() => {
+    if (pageState === PageState.INDEX) return t('common:account_settings');
+    else if (pageState === PageState.PASSWORD) return t('common:changepassword');
+    else if (pageState === PageState.EMAIL_BIND) return t('common:bindemail');
+    else if (pageState === PageState.EMAIL_UNBIND) return t('common:unbindemail');
+    else if (pageState === PageState.EMAIL_CHANGE_BIND) return t('common:changeemail');
+    else if (pageState === PageState.PHONE_BIND) return t('common:bindphone');
+    else if (pageState === PageState.PHONE_UNBIND) return t('common:unbindphone');
+    else if (pageState === PageState.PHONE_CHANGE_BIND) return t('common:changephone');
+    else if (pageState === PageState.REALNAME_AUTH) return t('common:realName_verification');
+    else return '';
+  }, [t, pageState]);
 
   return (
     <>
