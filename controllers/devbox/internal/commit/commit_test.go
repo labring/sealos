85e506a6a5f8d9c1bffc46e2085423a4e23bbbbc
Cunzili <cunzili@CunzilideMacBook-Pro.local>
2025-07-28 11:55:23 +0800

    unit test

--- controllers/devbox/internal/commit/commit_test.go
+++ controllers/devbox/internal/commit/commit_test.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"testing"
 	"time"
-
+	"github.com/containerd/containerd/v2/pkg/namespaces"
 	"github.com/stretchr/testify/assert"
+	"sync"
 )
 
 // const (
@@ -15,160 +16,197 @@ import (
 // 	testDataRoot  = "/var/lib/containerd"
 // )
 
-// 测试初始化 Committer
+// init Committer
 func TestNewCommitter(t *testing.T) {
 	committer, err := NewCommitter()
 	assert.NoError(t, err)
 	assert.NotNil(t, committer)
 }
 
-// 测试完整的 commit 流程
+// test commit flow
 func TestCommitFlow(t *testing.T) {
 	ctx:=context.Background()
 
-	// 1. 创建 committer
+	// 1. create committer
 	committer,err:=NewCommitter()
 	assert.NoError(t,err)
 
-	// 2. 准备测试数据
+	// 2. prepare test data
 	devboxName:=fmt.Sprintf("test-devbox-%d",time.Now().Unix())
 	contentID:="test-content-id-123"
 	baseImage:="docker.io/library/busybox:latest"
-	commitImage:=fmt.Sprintf("test-commit-%d",time.Now().Unix())
+	commitImage:=fmt.Sprintf("test-devbox-commit-%d",time.Now().Unix())
 
-	// 3. 创建容器并且commit容器
+	// 3. create container and commit container
 	err=committer.Commit(ctx,devboxName,contentID,baseImage,commitImage)
 	assert.NoError(t,err)
 
-	// // 4. 清理
+	// // 4. clean up
 	// err=committer.(*CommitterImpl).DeleteContainer(ctx,containerID)
 }
 
-// 测试创建容器
+// test create container
 func TestCreateContainer(t *testing.T) {
 	ctx := context.Background()
 	committer, err := NewCommitter()
 	assert.NoError(t, err)
 
-	// 创建容器
+	// create container
 	devboxName := fmt.Sprintf("test-devbox-%d", time.Now().Unix())
 	contentID := "test-content-id-456"
-	baseImage := "docker.io/library/nginx:latest"  // 使用另一个公共镜像测试
+	baseImage := "docker.io/library/nginx:latest"  // use another public image to test
 
 	containerID, err := committer.(*CommitterImpl).CreateContainer(ctx, devboxName, contentID, baseImage)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, containerID)
 
-	// 清理
-	defer committer.(*CommitterImpl).DeleteContainer(ctx, containerID)
+	// // clean up
+	// defer committer.(*CommitterImpl).DeleteContainer(ctx, containerID)
 
-	// 验证容器标签
+	// verify container labels
 	annotations, err := committer.(*CommitterImpl).GetContainerAnnotations(ctx, containerID)
 	assert.NoError(t, err)
 	assert.Equal(t, contentID, annotations["devbox.sealos.io/content-id"])
 }
 
-// 测试删除容器
+// test delete container
 func TestDeleteContainer(t *testing.T) {
 	ctx := context.Background()
 	committer, err := NewCommitter()
 	assert.NoError(t, err)
 
-	// 先创建一个容器
+	// create a container
 	devboxName := fmt.Sprintf("test-devbox-%d", time.Now().Unix())
 	containerID, err := committer.(*CommitterImpl).CreateContainer(ctx, devboxName, "test-content-id-789", "docker.io/library/alpine:latest")
 	assert.NoError(t, err)
 
-	// 删除容器
+	// show all containers in current namespace
+	ctx = namespaces.WithNamespace(ctx, namespace)
+    containers, err := committer.(*CommitterImpl).containerdClient.Containers(ctx)
+    assert.NoError(t, err)
+    
+    fmt.Printf("=== All Containers in current namespace ===\n")
+    for _, container := range containers {
+        fmt.Printf("Container ID: %s\n", container.ID())
+    }
+    fmt.Printf("=== Total %d containers ===\n", len(containers))
+
+	// delete container
 	err = committer.(*CommitterImpl).DeleteContainer(ctx, containerID)
 	assert.NoError(t, err)
 
-	// 验证容器已删除（尝试获取标签应该返回错误）
+	containers, err = committer.(*CommitterImpl).containerdClient.Containers(ctx)
+    assert.NoError(t, err)
+    
+    fmt.Printf("=== All Containers in current namespace ===\n")
+    for _, container := range containers {
+        fmt.Printf("Container ID: %s\n", container.ID())
+    }
+    fmt.Printf("=== Total %d containers ===\n", len(containers))
+
+	// verify container is deleted (try to get labels should return error)
 	_, err = committer.(*CommitterImpl).GetContainerAnnotations(ctx, containerID)
 	assert.Error(t, err)
 }
 
-// 测试错误情况
+// test error cases
 func TestErrorCases(t *testing.T) {
 	ctx := context.Background()
 	committer, err := NewCommitter()
 	assert.NoError(t, err)
 
-	// 测试使用不存在的镜像
+	// test use not exist image to create container
 	_, err = committer.(*CommitterImpl).CreateContainer(ctx, "test-devbox", "test-content-id", "not-exist-image:latest")
 	assert.Error(t, err)
 
-	// 测试删除不存在的容器
+	// test use not exist container to delete 
 	err = committer.(*CommitterImpl).DeleteContainer(ctx, "not-exist-container")
 	assert.Error(t, err)
 
-	// 测试获取不存在容器的标签
+	// test get not exist container label
 	_, err = committer.(*CommitterImpl).GetContainerAnnotations(ctx, "not-exist-container")
 	assert.Error(t, err)
 }
 
-// 测试并发操作
+// test concurrent operations
 func TestConcurrentOperations(t *testing.T) {
+	// fmt.Println("=== 这是最新的测试代码 ===") 
 	ctx := context.Background()
 	committer, err := NewCommitter()
 	assert.NoError(t, err)
-
-	// 并发创建多个容器
+	
+	// concurrent to create container
 	containerCount := 3
-	errChan := make(chan error, containerCount)
-	containerIDs := make(chan string, containerCount)
+	wg:=sync.WaitGroup{}
+	wg.Add(containerCount)
+	var mu sync.Mutex
+	containers:=make([]string,containerCount)
+	// errChan := make(chan error, containerCount)
+	// containerIDs := make(chan string, containerCount)
+	// defer close(errChan)
+	// defer close(containerIDs)
 
 	for i := 0; i < containerCount; i++ {
 		go func(index int) {
+			defer wg.Done()
 			devboxName := fmt.Sprintf("test-devbox-concurrent-%d-%d", time.Now().Unix(), index)
 			containerID, err := committer.(*CommitterImpl).CreateContainer(ctx, devboxName, 
 				fmt.Sprintf("test-content-id-%d", index), 
 				"docker.io/library/busybox:latest")
 			if err != nil {
-				errChan <- err
+				t.Errorf("Failed to create container: %v", err)
 				return
 			}
-			containerIDs <- containerID
+			mu.Lock()
+			containers=append(containers,containerID)
+			mu.Unlock()			
 		}(i)
 	}
+	wg.Wait()
 
-	// 收集结果
-	for i := 0; i < containerCount; i++ {
-		select {
-		case err := <-errChan:
-			assert.NoError(t, err)
-		case containerID := <-containerIDs:
-			// 清理创建的容器
-			err := committer.(*CommitterImpl).DeleteContainer(ctx, containerID)
-			assert.NoError(t, err)
-		}
-	}
-}
-
-// 测试大镜像的处理
-func TestLargeImage(t *testing.T) {
-	ctx := context.Background()
-	committer, err := NewCommitter()
-	assert.NoError(t, err)
-
-	// 使用较大的镜像进行测试
-	devboxName := fmt.Sprintf("test-devbox-%d", time.Now().Unix())
-	contentID := "test-content-id-large"
-	baseImage := "docker.io/library/tensorflow/tensorflow:latest"  // 大镜像
-	commitImage := fmt.Sprintf("localhost:5000/test-large-commit-%d:latest", time.Now().Unix())
-
-	// 创建容器
-	containerID, err := committer.(*CommitterImpl).CreateContainer(ctx, devboxName, contentID, baseImage)
-	if err != nil {
-		t.Logf("Skip large image test due to error: %v", err)
-		t.Skip()
+	// delete containers
+	for _,containerID:=range containers{
+		committer.(*CommitterImpl).DeleteContainer(ctx,containerID)
 	}
 
-	// Commit 容器
-	err = committer.Commit(ctx, devboxName, contentID, baseImage, commitImage)
-	assert.NoError(t, err)
+	// get current containers list
+	ctx = namespaces.WithNamespace(ctx, namespace)
+	currentContainers, err := committer.(*CommitterImpl).containerdClient.Containers(ctx)
+	fmt.Printf("=== All Containers in current namespace ===\n")
+    for _, container := range currentContainers {
+        // labels, _ := container.Labels(ctx)
+        fmt.Printf("Container ID: %s\n", container.ID())
+        // fmt.Printf("Labels: %+v\n", labels)
+        // fmt.Printf("---\n")
+    }
+    fmt.Printf("=== Total %d containers ===\n", len(containers))
 
-	// 清理
-	err = committer.(*CommitterImpl).DeleteContainer(ctx, containerID)
-	assert.NoError(t, err)
 }
+
+// // 测试大镜像的处理
+// func TestLargeImage(t *testing.T) {
+// 	ctx := context.Background()
+// 	committer, err := NewCommitter()
+// 	assert.NoError(t, err)
+
+// 	// 使用较大的镜像进行测试
+// 	devboxName := fmt.Sprintf("test-devbox-%d", time.Now().Unix())
+// 	contentID := "test-content-id-large"
+// 	baseImage := "docker.io/library/tensorflow/tensorflow:latest"  // 大镜像
+// 	commitImage := fmt.Sprintf("localhost:5000/test-large-commit-%d:latest", time.Now().Unix())
+
+// 	// 创建容器
+// 	containerID, err := committer.(*CommitterImpl).CreateContainer(ctx, devboxName, contentID, baseImage)
+// 	if err != nil {
+// 		t.Logf("Skip large image test due to error: %v", err)
+// 		t.Skip()
+// 	}
+
+// 	// Commit 容器
+// 	err = committer.Commit(ctx, devboxName, contentID, baseImage, commitImage)
+// 	assert.NoError(t, err)
+
+// 	// 清理
+// 	err = committer.(*CommitterImpl).DeleteContainer(ctx, containerID)
+// 	assert.NoError(t, err)
+// }
