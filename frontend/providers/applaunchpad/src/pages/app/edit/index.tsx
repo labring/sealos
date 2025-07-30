725dbac762d48b155c2423c9bce197703b494c0b
Nixieboluo <me@sagirii.me>
2025-07-30 11:47:10 +0800

    feat(applaunchpad): add gtmv2 error and paywall event
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/providers/applaunchpad/src/pages/app/edit/index.tsx
+++ frontend/providers/applaunchpad/src/pages/app/edit/index.tsx
@@ -196,12 +196,27 @@ const EditApp = ({ appName, tabType }: { appName?: string; tabType: string }) =>
         if (error?.code === ResponseCode.BALANCE_NOT_ENOUGH) {
           setErrorMessage(t('user_balance_not_enough'));
           setErrorCode(ResponseCode.BALANCE_NOT_ENOUGH);
+
+          track('paywall_triggered', {
+            module: 'applaunchpad',
+            type: 'insufficient_balance'
+          });
         } else if (error?.code === ResponseCode.FORBIDDEN_CREATE_APP) {
           setErrorMessage(t('forbidden_create_app'));
           setErrorCode(ResponseCode.FORBIDDEN_CREATE_APP);
+
+          track('error_occurred', {
+            module: 'applaunchpad',
+            error_code: 'FORBIDDEN_CREATE_APP'
+          });
         } else if (error?.code === ResponseCode.APP_ALREADY_EXISTS) {
           setErrorMessage(t('app_already_exists'));
           setErrorCode(ResponseCode.APP_ALREADY_EXISTS);
+
+          track('error_occurred', {
+            module: 'applaunchpad',
+            error_code: 'APP_ALREADY_EXISTS'
+          });
         } else {
           setErrorMessage(JSON.stringify(error));
         }
