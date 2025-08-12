0df76747d17cba7d0c451a5fffe91caecc28616c
asterRyan <137432928+PeiranXu-108@users.noreply.github.com>
2025-08-12 16:11:07 +0800

    feat(dbprovider)：add remark function & fix database creation time (#5814)
    * update
    
    * update
    
    * upt

--- frontend/providers/dbprovider/src/utils/adapt.ts
+++ frontend/providers/dbprovider/src/utils/adapt.ts
@@ -1,4 +1,5 @@
 import { BACKUP_REMARK_LABEL_KEY, BackupTypeEnum, backupStatusMap } from '@/constants/backup';
+import { DB_REMARK_KEY } from '@/constants/db';
 import {
   DBBackupMethodNameMap,
   DBNameLabel,
@@ -36,7 +37,12 @@ import {
 } from '@/utils/tools';
 import type { CoreV1EventList, V1Pod } from '@kubernetes/client-node';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import { has } from 'lodash';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 import type { BackupItemType } from '../types/db';
 
 export const getDBSource = (
@@ -109,20 +115,25 @@ export const adaptDBListItem = (db: KbPgClusterType): DBListItemType => {
       db?.status?.phase && dbStatusMap[db?.status?.phase]
         ? dbStatusMap[db?.status?.phase]
         : dbStatusMap.UnKnow,
-    createTime: dayjs(db.metadata?.creationTimestamp).format('YYYY/MM/DD HH:mm'),
+    createTime: dayjs(db.metadata?.creationTimestamp)
+      .tz('Asia/Shanghai')
+      .format('YYYY/MM/DD HH:mm'),
     ...calcTotalResource(db.spec.componentSpecs),
     replicas: db.spec?.componentSpecs.find((comp) => comp.name === dbType)?.replicas || 1,
     conditions: db?.status?.conditions || [],
     isDiskSpaceOverflow: false,
     labels: db.metadata.labels || {},
-    source: getDBSource(db)
+    source: getDBSource(db),
+    remark: db.metadata?.annotations?.[DB_REMARK_KEY] || ''
   };
 };
 
 export const adaptDBDetail = (db: KbPgClusterType): DBDetailType => {
   return {
     id: db.metadata?.uid || ``,
-    createTime: dayjs(db.metadata?.creationTimestamp).format('YYYY/MM/DD HH:mm'),
+    createTime: dayjs(db.metadata?.creationTimestamp)
+      .tz('Asia/Shanghai')
+      .format('YYYY/MM/DD HH:mm'),
     status:
       db?.status?.phase && dbStatusMap[db?.status?.phase]
         ? dbStatusMap[db?.status?.phase]
