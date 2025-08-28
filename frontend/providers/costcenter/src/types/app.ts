32233ff44af2f4aee4f7650c5f9fdc80dbf289b2
Nixieboluo <me@sagirii.me>
2025-08-28 16:07:40 +0800

    feat(costcenter): new app icon component
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/providers/costcenter/src/types/app.ts
+++ frontend/providers/costcenter/src/types/app.ts
@@ -3,3 +3,19 @@ export type AppListItem = {
   appType: number;
   appName: string;
 };
+
+export const AppType = {
+  TERMINAL: 'TERMINAL',
+  JOB: 'JOB',
+  OTHER: 'OTHER',
+  OBJECT_STORAGE: 'OBJECT-STORAGE',
+  CLOUD_VM: 'CLOUD-VM',
+  DB: 'DB',
+  APP: 'APP',
+  APP_STORE: 'APP-STORE',
+  DB_BACKUP: 'DB-BACKUP',
+  DEV_BOX: 'DEV-BOX',
+  LLM_TOKEN: 'LLM-TOKEN'
+} as const;
+
+export type AppType = (typeof AppType)[keyof typeof AppType];
