95a4a52d95932f1269cef63c721cc4165673b944
Peiran <xupeiran2048@gmail.com>
2025-09-01 11:02:18 +0800

    upt

--- frontend/providers/dbprovider/src/pages/db/edit/index.tsx
+++ frontend/providers/dbprovider/src/pages/db/edit/index.tsx
@@ -251,7 +251,7 @@ const EditApp = ({ dbName, tabType }: { dbName?: string; tabType?: 'form' | 'yam
         );
         console.log('filtered backups for db:', formData.dbName, backups);
         const inProgressBackups = backups.filter(
-          (backup: BackupItemType) => backup.status.value === 'Completed'
+          (backup: BackupItemType) => backup.status.value === 'Running'
         );
         console.log('inProgressBackups', inProgressBackups);
 
