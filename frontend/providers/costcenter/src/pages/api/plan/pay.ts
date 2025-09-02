65f6dcdf132255f26902c6a2634d46701e139929
zjy365 <3161362058@qq.com>
2025-09-02 10:09:51 +0800

    add Congratulations

--- frontend/providers/costcenter/src/pages/api/plan/pay.ts
+++ frontend/providers/costcenter/src/pages/api/plan/pay.ts
@@ -125,7 +125,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       planName,
       period,
       payMethod,
-      operator: 'created'
+      operator
     };
 
     if (cardId) {
