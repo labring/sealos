af9e7516ab4a4ecc2182ffc30f111fae5b15fa0b
cuisongliu <cuisongliu@qq.com>
2025-09-02 12:58:51 +0800

    feat(images): apply CRDs for VictoriaMetrics Helm chart using server-side apply
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- deploy/base/victoria_metrics_k8s_stack/v1.124.0/entrypoint.sh
+++ deploy/base/victoria_metrics_k8s_stack/v1.124.0/entrypoint.sh
@@ -11,4 +11,5 @@ HELM_OPTS=${HELM_OPTS:-" \
 cp kb-fix/prometheus-additional.yaml ./charts/victoria-metrics-k8s-stack/files/prometheus-additional.yaml
 cp kb-fix/kubeblocks-fix.yaml ./charts/victoria-metrics-k8s-stack/templates/victoria-metrics-operator/vmagent/kubeblocks-fix.yaml
 
+helm show crds charts/victoria-metrics-k8s-stack | kubectl apply -f - --server-side  --force-conflicts
 helm upgrade -i ${NAME} ${CHARTS} -n ${NAMESPACE} --create-namespace -f ./charts/victoria-metrics-k8s-stack/values.yaml -f values-cloud.yaml ${HELM_OPTS}
