c06945c3ab9d2b1532fc4e8bd36d2fa8d084ba9b
Nixieboluo <me@sagirii.me>
2025-09-03 15:15:13 +0800

    feat(costcenter): new pricing table
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/providers/costcenter/src/components/icons/NetworkIcon.tsx
+++
@@ -1,5 +0,0 @@
-import { Img, ImgProps } from '@chakra-ui/react';
-import network from '@/assert/network.svg';
-export function NetworkIcon(props: ImgProps) {
-  return <Img width="16" height="16" src={network.src} {...props} />;
-}
