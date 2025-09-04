c4f40dd8056d77eb166fa0ab154c8130fd1f057d
cuisongliu <cuisongliu@qq.com>
2025-09-04 16:54:02 +0800

    feat(chart): add configmap.yaml and update install script for Higress configuration
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- deploy/base/higress/v2.1.3/install.sh
+++ deploy/base/higress/v2.1.3/install.sh
@@ -4,4 +4,6 @@ HELM_OPTS="${HELM_OPTS:-}"
 CLOUD_PORT="${CLOUD_PORT:-443}"
 
 cp -r fix/plugins.yaml charts/higress/charts/higress-core/templates/plugins.yaml
+cp -r fix/configmap.yaml charts/higress/charts/higress-core/templates/configmap.yaml
+
 helm upgrade --install higress -n higress-system charts/higress --create-namespace --render-subchart-notes -f values-cloud.yaml  ${HELM_OPTS} --set gateway.httpsPort=${CLOUD_PORT}
