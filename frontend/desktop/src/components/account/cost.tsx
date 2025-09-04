bd99e0435a86d4b62d16ef0ddb6c4bb187018409
zjy365 <3161362058@qq.com>
2025-09-04 16:19:50 +0800

    fix desktop build

--- frontend/desktop/src/components/account/cost.tsx
+++ frontend/desktop/src/components/account/cost.tsx
@@ -109,6 +109,7 @@ export default function Cost() {
             if (!costcenter) return;
             openApp(costcenter);
           }}
+          openCostCenterTopup={() => {}}
         >
           <Flex
             borderRadius={'6px'}
