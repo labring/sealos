6d9830d19b5080a77927269e56b28a9ea30901f5
cuisongliu <cuisongliu@qq.com>
2025-09-04 17:37:59 +0800

    feat(chart): add HTTPS configuration and update install script for Higress
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- deploy/base/higress/v2.1.3/install.sh
+++ deploy/base/higress/v2.1.3/install.sh
@@ -2,8 +2,10 @@
 
 HELM_OPTS="${HELM_OPTS:-}"
 CLOUD_PORT="${CLOUD_PORT:-443}"
+CLOUD_DOMAIN="${CLOUD_DOMAIN:-"sealos.cloud"}"
 
 cp -r fix/plugins.yaml charts/higress/charts/higress-core/templates/plugins.yaml
 cp -r fix/configmap.yaml charts/higress/charts/higress-core/templates/configmap.yaml
 
-helm upgrade --install higress -n higress-system charts/higress --create-namespace --render-subchart-notes -f values-cloud.yaml  ${HELM_OPTS} --set gateway.httpsPort=${CLOUD_PORT}
+helm upgrade --install higress -n higress-system charts/higress --create-namespace --render-subchart-notes -f values-cloud.yaml  ${HELM_OPTS} \
+      --set higress-core.gateway.httpsPort="${CLOUD_PORT}" --set higress-core.cloud.domain="${CLOUD_DOMAIN}"
