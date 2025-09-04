243c629496eb7ced84629a02a7e2f70f915f48b9
cuisongliu <cuisongliu@qq.com>
2025-09-04 13:55:40 +0800

    feat(chart): update install script and add EnvoyFilter configurations for Higress
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- deploy/base/higress/v2.1.3/install.sh
+++ deploy/base/higress/v2.1.3/install.sh
@@ -3,4 +3,5 @@
 HELM_OPTS="${HELM_OPTS:-}"
 CLOUD_PORT="${CLOUD_PORT:-443}"
 
-helm upgrade --install higress -n higress-system charts/higress --create-namespace --render-subchart-notes ${HELM_OPTS} --set gateway.httpsPort=${CLOUD_PORT}
+cp -r fix/plugins.yaml charts/higress/charts/higress-core/templates/plugins.yaml
+helm upgrade --install higress -n higress-system charts/higress --create-namespace --render-subchart-notes -f values-cloud.yaml  ${HELM_OPTS} --set gateway.httpsPort=${CLOUD_PORT}
