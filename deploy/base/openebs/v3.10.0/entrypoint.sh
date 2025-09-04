d2e0267591058f8d90f38c96e75579bb715d4f28
cuisongliu <cuisongliu@qq.com>
2025-09-04 15:56:04 +0800

    feat(chart): enable lvm localpv and update entrypoint script for storage configuration
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- deploy/base/openebs/v3.10.0/entrypoint.sh
+++ deploy/base/openebs/v3.10.0/entrypoint.sh
@@ -3,12 +3,18 @@ set -e
 
 NAME=${NAME:-"openebs"}
 NAMESPACE=${NAMESPACE:-"openebs"}
-UNINSTALL=${UNINSTALL:-"false"}
 HELM_OPTS=${HELM_OPTS:-" \
 --set ndm.enabled=false \
 --set ndmOperator.enabled=false \
 --set localprovisioner.deviceClass.enabled=false \
 --set localprovisioner.hostpathClass.isDefaultClass=true \
+--set lvm-localpv.enabled=true \
 "}
 
-helm upgrade -i ${NAME} ./charts/${NAME} -n ${NAMESPACE} --create-namespace ${HELM_OPTS}
+
+OPENEBS_STORAGE_PREFIX=${OPENEBS_STORAGE_PREFIX:-"/var/openebs"}
+OPENEBS_LOCAL_ENABLE=${OPENEBS_LOCAL_ENABLE:-"true"}
+
+helm upgrade -i ${NAME} ./charts/${NAME} -n ${NAMESPACE} --create-namespace ${HELM_OPTS} --set varDirectoryPath.baseDir="${OPENEBS_STORAGE_PREFIX}" \
+    --set localprovisioner.basePath="${OPENEBS_STORAGE_PREFIX}/local" --set localprovisioner.enabled="${OPENEBS_LOCAL_ENABLE}"
+
