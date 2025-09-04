48a86d68f8ad60b8d8f8c0b2c7c112b993e83a41
zjy365 <3161362058@qq.com>
2025-09-04 17:47:36 +0800

    fix InternalAppCall

--- frontend/desktop/src/components/team/WorkspaceToggle.tsx
+++ frontend/desktop/src/components/team/WorkspaceToggle.tsx
@@ -69,6 +69,10 @@ export default function WorkspaceToggle() {
       pathname: '/',
       query: {
         mode: 'create'
+      },
+      messageData: {
+        type: 'InternalAppCall',
+        mode: 'create'
       }
     });
   };
