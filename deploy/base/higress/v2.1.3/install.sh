9ccd3a917c3016ffab3489740e9f35d2bd4b3c1f
cuisongliu <cuisongliu@qq.com>
2025-09-04 22:38:43 +0800

    feat(chart): update domain configuration for Higress and add HTTPS settings
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- deploy/base/higress/v2.1.3/install.sh
+++ deploy/base/higress/v2.1.3/install.sh
@@ -2,10 +2,11 @@
 
 HELM_OPTS="${HELM_OPTS:-}"
 CLOUD_PORT="${CLOUD_PORT:-443}"
-CLOUD_DOMAIN="${CLOUD_DOMAIN:-"sealos.cloud"}"
+CLOUD_DOMAIN="${CLOUD_DOMAIN:-"cloud.sealos.io"}"
 
 cp -r fix/plugins.yaml charts/higress/charts/higress-core/templates/plugins.yaml
 cp -r fix/configmap.yaml charts/higress/charts/higress-core/templates/configmap.yaml
+cp -r fix/https.yaml charts/higress/charts/higress-core/templates/https.yaml
 
 helm upgrade --install higress -n higress-system charts/higress --create-namespace --render-subchart-notes -f values-cloud.yaml  ${HELM_OPTS} \
-      --set higress-core.gateway.httpsPort="${CLOUD_PORT}" --set higress-core.cloud.domain="${CLOUD_DOMAIN}"
+      --set higress-core.gateway.httpsPort="${CLOUD_PORT}" --set higress-core.sealos.domain="${CLOUD_DOMAIN}"
