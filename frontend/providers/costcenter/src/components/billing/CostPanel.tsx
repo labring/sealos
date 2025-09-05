8909da900c5b8e3cbef60568c78f986c17cd43e3
Nixieboluo <me@sagirii.me>
2025-09-05 16:43:41 +0800

    chore(costcenter): fixed width for cost panel
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/providers/costcenter/src/components/billing/CostPanel.tsx
+++ frontend/providers/costcenter/src/components/billing/CostPanel.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@sealos/shadcn-ui/button';
+import { cn } from '@sealos/shadcn-ui';
 import { formatMoney } from '@/utils/format';
 
 type CostPanelProps = {
@@ -6,9 +6,10 @@ type CostPanelProps = {
   region?: string | null;
   workspace?: string | null;
   totalCost: number;
+  className?: string;
 };
 
-export function CostPanel({ children, region, workspace, totalCost }: CostPanelProps) {
+export function CostPanel({ children, region, workspace, totalCost, className }: CostPanelProps) {
   const displayTitle = (() => {
     if (!region && !workspace) {
       return 'Total Cost';
@@ -23,7 +24,7 @@ export function CostPanel({ children, region, workspace, totalCost }: CostPanelP
   })();
 
   return (
-    <div className="[&>*]:mb-4">
+    <div className={cn('[&>*]:mb-4', className)}>
       <div className="shadow-sm border border-r-0 border-t-0 bg-blue-50 text-sm flex p-4 justify-between rounded-xl rounded-r-none rounded-t-none items-center gap-16 sticky top-0 z-10">
         <div className="font-semibold">
           <span>{displayTitle}: </span>
