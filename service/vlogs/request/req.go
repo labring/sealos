d569323c7d775bf8cd36007f9bbcfb9266f1434b
Bearix <2669184984@qq.com>
2025-09-08 15:36:57 +0800

    fix(vlogs service): change time query logic.

--- service/vlogs/request/req.go
+++ service/vlogs/request/req.go
@@ -40,19 +40,19 @@ func QueryLogsByParams(query *QueryParams) (*http.Response, error) {
 }
 
 func generateReq(query *QueryParams) (*http.Request, error) {
-	baseURL, err := url.Parse(query.path + "/select/logsql/query")
+	baseURL, err := url.Parse(query.Path + "/select/logsql/query")
 	if err != nil {
 		return nil, fmt.Errorf("can not parser API URL: %v", err)
 	}
 	params := url.Values{}
-	params.Add("query", query.query)
-	params.Add("start", query.startTime)
-	params.Add("end", query.endTime)
+	params.Add("query", query.Query)
+	params.Add("start", query.StartTime)
+	params.Add("end", query.EndTime)
 	baseURL.RawQuery = params.Encode()
 	req, err := http.NewRequest("GET", baseURL.String(), nil)
 	if err != nil {
 		return nil, fmt.Errorf("create HTTP req error: %v", err)
 	}
-	req.SetBasicAuth(query.username, query.password)
+	req.SetBasicAuth(query.Username, query.Password)
 	return req, nil
 }
