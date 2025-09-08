a679e2a184e100d111e5fc093c37cf5d9b9ae7e2
cuisongliu <cuisongliu@qq.com>
2025-09-08 15:06:16 +0800

    fix(image): update timestamp label to use formatted commit date
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- .github/scripts/image_build_push.sh
+++ .github/scripts/image_build_push.sh
@@ -8,13 +8,14 @@ AUTHOR=${5:-"labring"}
 COMMIT_ID=${6:-"latest"}
 IMAGE_NAME="ghcr.io/${AUTHOR}/sealos/${name}:${version}"
 
+commitDATE=$(date +%Y%m%d%H%M%S)
 sealos login -u "${AUTHOR}" -p "${TOKEN}" ghcr.io
 sealos build --pull=always --platform=linux/"${ARCH}" -t "$IMAGE_NAME"-"${ARCH}" \
   --label org.opencontainers.image.description="kubernetes and app cluster image" \
   --label org.opencontainers.image.licenses="Sealos Sustainable Use License" \
   --label org.opencontainers.image.source="https://github.com/${AUTHOR}/sealos" \
   --label org.opencontainers.image.title="sealos-image" \
-  --label org.opencontainers.image.time="$(date '%Y%m%d%H%M%S')" \
+  --label org.opencontainers.image.time="${commitDATE}" \
   --label org.opencontainers.image.url="https://github.com/${AUTHOR}/sealos" \
   --label org.opencontainers.image.version="${version}" \
   --label org.opencontainers.image.revision="${COMMIT_ID}" .
