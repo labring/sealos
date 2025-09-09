3e24538df6a8f04ed09f88726ed7a16ad802befd
cuisongliu <cuisongliu@qq.com>
2025-09-09 15:00:18 +0800

    fix(install): update nodePort range handling in installation script
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- deploy/base/cilium/v1.17.1/install.sh
+++ deploy/base/cilium/v1.17.1/install.sh
@@ -6,6 +6,13 @@ KUBEADM_POD_SUBNET="${KUBEADM_POD_SUBNET:-"100.64.0.0/16"}"
 KUBEADM_SERVICE_RANGE="${KUBEADM_SERVICE_RANGE:-"30000-50000"}"
 CILIUM_MASKSIZE="${CILIUM_MASKSIZE:-"24"}"
 
+KUBEADM_SERVICE_RANGE=$(echo "$KUBEADM_SERVICE_RANGE" | sed 's/-/,/')
+
+cat << EOF > np.yaml
+nodePort:
+  range: "${KUBEADM_SERVICE_RANGE}"
+EOF
+
 helm upgrade --install cilium -n kube-system charts/cilium -f values-cloud.yaml ${HELM_OPTS} \
       --set ipv4NativeRoutingCIDR="${KUBEADM_POD_SUBNET}" --set ipam.operator.clusterPoolIPv4PodCIDRList="${KUBEADM_POD_SUBNET}" \
-      --set nodePort.range="${KUBEADM_SERVICE_RANGE}" --set ipam.operator.clusterPoolIPv4MaskSize="${CILIUM_MASKSIZE}"
+      -f np.yaml --set ipam.operator.clusterPoolIPv4MaskSize="${CILIUM_MASKSIZE}"
