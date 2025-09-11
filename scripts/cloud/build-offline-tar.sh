0cedc35358dc4898f82ad59bcab69b109365e09f
cuisongliu <cuisongliu@qq.com>
2025-09-11 15:02:34 +0800

    feat(proxy): add proxy support for image pulling in build-offline-tar.sh (#5968)
    * feat(proxy): add proxy support for image pulling in build-offline-tar.sh
    
    Signed-off-by: cuisongliu <cuisongliu@qq.com>
    
    * fix(build-offline-tar): update script URLs to use main branch for cloud installation
    
    Signed-off-by: cuisongliu <cuisongliu@qq.com>
    
    * fix(build-offline-tar): update script URLs to use main branch for cloud installation
    
    Signed-off-by: cuisongliu <cuisongliu@qq.com>
    
    ---------
    
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- scripts/cloud/build-offline-tar.sh
+++ scripts/cloud/build-offline-tar.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-set -x
-
 ARCH=${ARCH:-"amd64"}
 CLOUD_VERSION=${CLOUD_VERSION:-"latest"}
-
+PROXY=${PROXY:-"ghcr.io"}
 # pull and save images
 mkdir -p output/tars
 
 images=(
-  ghcr.io/labring/sealos/kubernetes:v1.28.15
-  ghcr.io/labring/sealos/helm:v3.16.2
-  ghcr.io/labring/sealos/cilium:v1.17.1
-  ghcr.io/labring/sealos/cert-manager:v1.14.6
-  ghcr.io/labring/sealos/openebs:v3.10.0
-  ghcr.io/labring/sealos/victoria-metrics-k8s-stack:v1.124.0
-  ghcr.io/labring/sealos/higress:v2.1.3
-  ghcr.io/labring/sealos/kubeblocks:v0.8.2
-  ghcr.io/labring/sealos/kubeblocks-kafka:v0.8.2
-  ghcr.io/labring/sealos/cockroach:v2.12.0
-  ghcr.io/labring/sealos/metrics-server:v0.6.4
-  ghcr.io/labring/sealos/sealos-certs:v0.1.0
-  ghcr.io/labring/sealos/sealos-finish:v0.1.0
+  ${PROXY}/labring/sealos/kubernetes:v1.28.15
+  ${PROXY}/labring/sealos/helm:v3.16.2
+  ${PROXY}/labring/sealos/cilium:v1.17.1
+  ${PROXY}/labring/sealos/cert-manager:v1.14.6
+  ${PROXY}/labring/sealos/openebs:v3.10.0
+  ${PROXY}/labring/sealos/victoria-metrics-k8s-stack:v1.124.0
+  ${PROXY}/labring/sealos/higress:v2.1.3
+  ${PROXY}/labring/sealos/kubeblocks:v0.8.2
+  ${PROXY}/labring/sealos/kubeblocks-kafka:v0.8.2
+  ${PROXY}/labring/sealos/cockroach:v2.12.0
+  ${PROXY}/labring/sealos/metrics-server:v0.6.4
+  ${PROXY}/labring/sealos/sealos-certs:v0.1.0
+  ${PROXY}/labring/sealos/sealos-finish:v0.1.0
+  ${PROXY}/labring/sealos-cloud:$CLOUD_VERSION
 )
 
 for image in "${images[@]}"; do
@@ -31,11 +30,6 @@ for image in "${images[@]}"; do
   fi
 done
 
-sealos pull --platform "linux/$ARCH" ghcr.io/labring/sealos-cloud:$CLOUD_VERSION
-sealos tag ghcr.io/labring/sealos-cloud:$CLOUD_VERSION docker.io/labring/sealos-cloud:$CLOUD_VERSION
-sealos save -o output/tars/sealos-cloud.tar docker.io/labring/sealos-cloud:$CLOUD_VERSION
-
-# get and save cli
 mkdir -p output/cli
 
 VERSION="v5.0.1"
@@ -44,8 +38,7 @@ wget https://github.com/labring/sealos/releases/download/${VERSION}/sealos_${VER
    && tar zxvf sealos_${VERSION#v}_linux_${ARCH}.tar.gz sealos && chmod +x sealos && mv sealos output/cli
 
 
-echo '
-#!/bin/bash
+echo '#!/bin/bash
 
 cp cli/sealos /usr/local/bin
 
@@ -55,8 +48,8 @@ done
 
 '  > output/load-images.sh
 
-curl -sfL https://raw.githubusercontent.com/labring/sealos/${CLOUD_VERSION}/scripts/cloud/install-v2.sh -o output/scripts/install-v2.sh
-curl -sfL https://raw.githubusercontent.com/labring/sealos/${CLOUD_VERSION}/scripts/cloud/sealos.env -o output/scripts/sealos.env
+curl -sfL https://raw.githubusercontent.com/labring/sealos/main/scripts/cloud/install-v2.sh -o output/install-v2.sh
+curl -sfL https://raw.githubusercontent.com/labring/sealos/main/scripts/cloud/sealos.env -o output/sealos.env
 
 # tar output to a tar.gz
 mv output sealos-cloud
