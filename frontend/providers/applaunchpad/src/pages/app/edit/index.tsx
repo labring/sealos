2d8e0f752146774fe6037a7031d5991e0e497dcd
Nixieboluo <me@sagirii.me>
2025-09-11 18:21:18 +0800

    feat(applaunchpad): do not let user create deployment if exceedes quota
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/providers/applaunchpad/src/pages/app/edit/index.tsx
+++ frontend/providers/applaunchpad/src/pages/app/edit/index.tsx
@@ -182,7 +182,8 @@ const EditApp = ({ appName, tabType }: { appName?: string; tabType: string }) =>
             existingStores.reduce((sum, item) => sum + item.value, 0)) *
           resourcePropertyMap.storage.scale
         : realTimeForm.current.storeList.reduce((sum, item) => sum + item.value, 0) *
-          resourcePropertyMap.storage.scale
+          resourcePropertyMap.storage.scale,
+      traffic: 1
     });
   }, [checkExceededQuotas, existingStores, formHook.formState, isEdit]);
 
@@ -553,7 +554,8 @@ const EditApp = ({ appName, tabType }: { appName?: string; tabType: string }) =>
         items={exceededQuotas}
         onOpenChange={setIsInsufficientQuotaDialogOpen}
         open={isInsufficientQuotaDialogOpen}
-        onConfirm={confirmSubmit}
+        onConfirm={() => {}}
+        showControls={false}
       />
     </>
   );
