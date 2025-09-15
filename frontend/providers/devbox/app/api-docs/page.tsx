b142da420563c5ac6563480ccc5dbd9ae7789e9d
Nixieboluo <me@sagirii.me>
2025-09-15 15:01:04 +0800

    refactor(frontned): remove all user subs info apis in favor of the one in session
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/providers/devbox/app/api-docs/page.tsx
+++ frontend/providers/devbox/app/api-docs/page.tsx
@@ -4,21 +4,16 @@ import { useEffect, useState } from 'react';
 import { ApiReferenceReact } from '@scalar/api-reference-react';
 
 import { useEnvStore } from '@/stores/env';
-import { getDesktopSessionFromSessionStorage, getSessionFromSessionStorage } from '@/utils/user';
 
 import '@scalar/api-reference-react/style.css';
 
 export default function References() {
   const { env } = useEnvStore();
   const [apiData, setApiData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const devboxToken = getSessionFromSessionStorage();
-  const session = getDesktopSessionFromSessionStorage();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading(true);
         const response = await fetch('/api/openapi');
         if (!response.ok) {
           throw new Error(`API request failed: ${response.status}`);
@@ -26,14 +21,12 @@ export default function References() {
         const data = await response.json();
         setApiData(data);
       } catch (error) {
-        // @ts-ignore
-        setError(error.message);
-      } finally {
-        setLoading(false);
+        console.error('Error fetching API data:', error);
       }
     };
     fetchData();
   }, []);
+
   const config = {
     content: JSON.stringify(apiData),
     authentication: {
@@ -41,13 +34,11 @@ export default function References() {
       securitySchemes: {
         kubeconfigAuth: {
           in: 'header' as const,
-          name: 'Authorization',
-          value: encodeURIComponent(session?.kubeconfig || '')
+          name: 'Authorization'
         },
         jwtAuth: {
           in: 'header' as const,
-          name: 'Authorization-Bearer',
-          value: encodeURIComponent(devboxToken || session?.token || '')
+          name: 'Authorization-Bearer'
         }
       }
     },
@@ -55,6 +46,10 @@ export default function References() {
       ? undefined
       : `https://devbox.${env.sealosDomain}/scalar/cdn.js`
   };
+
+  if (!apiData) {
+    return <div>Loading...</div>;
+  }
 
   return <ApiReferenceReact configuration={config} />;
 }
