37530bbf4a7ebbe68e306f771b2ba7c7756b6614
zjy365 <3161362058@qq.com>
2025-11-17 17:20:05 +0800

    update

--- frontend/desktop/src/services/backend/db/init.ts
+++ frontend/desktop/src/services/backend/db/init.ts
@@ -1,15 +1,10 @@
 import { PrismaClient as RegionPrismaClient } from 'prisma/region/generated/client';
 import { PrismaClient as GlobalPrismaClient } from 'prisma/global/generated/client';
 
-const globalForPrisma = global as unknown as {
-  prisma: RegionPrismaClient | undefined;
-  globalPrisma: GlobalPrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new RegionPrismaClient();
-export const globalPrisma = globalForPrisma.globalPrisma ?? new GlobalPrismaClient();
+export const prisma = global.prisma ?? new RegionPrismaClient();
+export const globalPrisma = global.globalPrisma ?? new GlobalPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-  globalForPrisma.globalPrisma = globalPrisma;
+  global.prisma = prisma;
+  global.globalPrisma = globalPrisma;
 }
