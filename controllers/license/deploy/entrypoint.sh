4dddf0b3cff36d675abc9ca2a71b28a69dbcce50
cuisongliu <cuisongliu@qq.com>
2025-11-19 10:24:30 +0800

    fix(deploy): enhance uninstall process by removing specific resources in entrypoint.sh (#6213)
    * fix(deploy): add uninstall script to Kubefile
    
    * fix(deploy): remove unnecessary namespace definition from deploy.yaml
    
    * fix(deploy): enhance uninstall process by removing specific resources in entrypoint.sh
    
    * fix(deploy): remove CRD deletion from entrypoint.sh to streamline uninstall process
    
    (cherry picked from commit 38d5c91e7403825e1a00771a1ef958c6dc16f81d)

--- controllers/license/deploy/entrypoint.sh
+++ controllers/license/deploy/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 HELM_OPTS=${HELM_OPTS:-""}
-kubectl delete --ignore-not-found -f uninstall
+kubectl delete clusterrole  kube-system-namespace-read-cluster-role --ignore-not-found
+kubectl delete clusterrolebinding license-controller-role-binding --ignore-not-found
+kubectl delete deployment license-controller-manager -n account-system --ignore-not-found
+kubectl delete service license-controller-manager-metrics-service -n account-system --ignore-not-found
+kubectl delete clusterrolebinding license-proxy-rolebinding --ignore-not-found
+kubectl delete clusterrolebinding license-manager-rolebinding --ignore-not-found
+kubectl delete rolebinding license-leader-election-rolebinding -n account-system --ignore-not-found
 kubectl delete clusterrole license-manager-role --ignore-not-found
+kubectl delete clusterrole license-metrics-reader --ignore-not-found
+kubectl delete clusterrole license-proxy-role --ignore-not-found
+kubectl delete role license-leader-election-role -n account-system --ignore-not-found
+kubectl delete serviceaccount license-controller-manager -n account-system --ignore-not-found
 helm upgrade  -i license -n license-system --create-namespace ./charts/license  ${HELM_OPTS}
 helm show crds ./charts/license | kubectl apply -f - --server-side  --force-conflicts
