74cb9608f6b8a1c1df264734b983cd0a1b8b923d
Nixieboluo <me@sagirii.me>
2025-12-09 16:02:21 +0800

    feat(applaunchpad): move network editing to a separate subform
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/providers/applaunchpad/src/pages/app/edit/components/Form.tsx
+++ frontend/providers/applaunchpad/src/pages/app/edit/components/Form.tsx
@@ -1,11 +1,11 @@
 import { obj2Query } from '@/api/tools';
 import MyIcon from '@/components/Icon';
 import { MyRangeSlider, MySelect, MySlider, MyTooltip, RangeInput, Tabs, Tip } from '@sealos/ui';
-import { APPLICATION_PROTOCOLS, defaultSliderKey, ProtocolList } from '@/constants/app';
+import { defaultSliderKey } from '@/constants/app';
 import { GpuAmountMarkList } from '@/constants/editApp';
 import { useToast } from '@/hooks/useToast';
 import { useGlobalStore } from '@/store/global';
-import { PVC_STORAGE_MAX, SEALOS_DOMAIN } from '@/store/static';
+import { PVC_STORAGE_MAX } from '@/store/static';
 import { useUserStore } from '@/store/user';
 import type { QueryType } from '@/types';
 import { type AppEditType } from '@/types/app';
@@ -28,8 +28,6 @@ import {
   Grid,
   IconButton,
   Input,
-  Switch,
-  Tooltip,
   useDisclosure,
   useTheme
 } from '@chakra-ui/react';
@@ -41,16 +39,15 @@ import { useRouter } from 'next/router';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useFieldArray, UseFormReturn } from 'react-hook-form';
 import type { ConfigMapType } from './ConfigmapModal';
-import type { CustomAccessModalParams } from './CustomAccessModal';
 import PriceBox from './PriceBox';
 import QuotaBox from './QuotaBox';
 import type { StoreType } from './StoreModal';
 import styles from './index.module.scss';
-import { mountPathToConfigMapKey, useCopyData } from '@/utils/tools';
+import { NetworkSection } from './NetworkSection';
+import { mountPathToConfigMapKey } from '@/utils/tools';
 import { useQuery } from '@tanstack/react-query';
 import { WorkspaceQuotaItem } from '@/types/workspace';
 
-const CustomAccessModal = dynamic(() => import('./CustomAccessModal'));
 const ConfigmapModal = dynamic(() => import('./ConfigmapModal'));
 const StoreModal = dynamic(() => import('./StoreModal'));
 const EditEnvs = dynamic(() => import('./EditEnvs'));
@@ -86,7 +83,6 @@ const Form = ({
   const { userSourcePrice } = useUserStore();
   const router = useRouter();
   const { toast } = useToast();
-  const { copyData } = useCopyData();
   const { name } = router.query as QueryType;
   const theme = useTheme();
   const isEdit = useMemo(() => !!name, [name]);
@@ -98,16 +94,6 @@ const Form = ({
     getValues,
     formState: { errors }
   } = formHook;
-
-  const {
-    fields: networks,
-    append: appendNetworks,
-    remove: removeNetworks,
-    update: updateNetworks
-  } = useFieldArray({
-    control,
-    name: 'networks'
-  });
 
   const { fields: envs, replace: replaceEnvs } = useFieldArray({
     control,
@@ -167,7 +153,6 @@ const Form = ({
   );
 
   const [activeNav, setActiveNav] = useState(navList[0].id);
-  const [customAccessModalData, setCustomAccessModalData] = useState<CustomAccessModalParams>();
   const [configEdit, setConfigEdit] = useState<ConfigMapType>();
   const [storeEdit, setStoreEdit] = useState<StoreType>();
   const { isOpen: isEditEnvs, onOpen: onOpenEditEnvs, onClose: onCloseEditEnvs } = useDisclosure();
@@ -931,270 +916,15 @@ const Form = ({
           </Box>
 
           {/* network */}
-          <Box id={'network'} {...boxStyles}>
-            <Box {...headerStyles}>
-              <MyIcon name={'network'} mr={'12px'} w={'24px'} color={'grayModern.900'} />
-              {t('Network Configuration')}
-            </Box>
-            <Box px={'42px'} py={'24px'} userSelect={'none'}>
-              {networks.map((network, i) => (
-                <Flex
-                  alignItems={'flex-start'}
-                  key={network.id}
-                  _notLast={{ pb: 6, borderBottom: theme.borders.base }}
-                  _notFirst={{ pt: 6 }}
-                >
-                  {/* Container Port Column - Fixed Width */}
-                  <Box w={'140px'}>
-                    <Box mb={'10px'} h={'20px'} fontSize={'base'} color={'grayModern.900'}>
-                      {t('Container Port')}
-                    </Box>
-                    <Input
-                      h={'32px'}
-                      type={'number'}
-                      w={'110px'}
-                      bg={'grayModern.50'}
-                      {...register(`networks.${i}.port`, {
-                        required:
-                          t('app.The container exposed port cannot be empty') ||
-                          'The container exposed port cannot be empty',
-                        valueAsNumber: true,
-                        min: {
-                          value: 1,
-                          message: t('app.The minimum exposed port is 1')
-                        },
-                        max: {
-                          value: 65535,
-                          message: t('app.The maximum number of exposed ports is 65535')
-                        }
-                      })}
-                    />
-                    {i === networks.length - 1 && networks.length + 1 <= 15 && (
-                      <Box mt={3}>
-                        <Button
-                          w={'100px'}
-                          variant={'outline'}
-                          leftIcon={<MyIcon name="plus" w={'18px'} fill={'#485264'} />}
-                          onClick={() =>
-                            appendNetworks({
-                              networkName: '',
-                              portName: nanoid(),
-                              port: 80,
-                              protocol: 'TCP',
-                              appProtocol: 'HTTP',
-                              openPublicDomain: false,
-                              publicDomain: '',
-                              customDomain: '',
-                              domain: SEALOS_DOMAIN,
-                              openNodePort: false,
-                              nodePort: undefined
-                            })
-                          }
-                        >
-                          {t('Add Port')}
-                        </Button>
-                      </Box>
-                    )}
-                  </Box>
+          <NetworkSection
+            formHook={formHook}
+            exceededQuotas={exceededQuotas}
+            onDomainVerified={onDomainVerified}
+            handleOpenCostcenter={handleOpenCostcenter}
+            boxStyles={boxStyles}
+            headerStyles={headerStyles}
+          />
 
-                  {/* Enable Internet Access Column - Fixed Width */}
-                  <Box w={'200px'} mx={7}>
-                    <Box mb={'8px'} h={'20px'} fontSize={'base'} color={'grayModern.900'}>
-                      {t('Open Public Access')}
-                    </Box>
-                    <Flex alignItems={'center'} h={'35px'}>
-                      <Switch
-                        className="driver-deploy-network-switch"
-                        size={'lg'}
-                        isChecked={!!network.openPublicDomain || !!network.openNodePort}
-                        onChange={(e) => {
-                          if (e.target.checked) {
-                            if (APPLICATION_PROTOCOLS.includes(network.appProtocol)) {
-                              updateNetworks(i, {
-                                ...getValues('networks')[i],
-                                networkName: network.networkName || `network-${nanoid()}`,
-                                protocol: 'TCP',
-                                appProtocol: network.appProtocol || 'HTTP',
-                                openPublicDomain: true,
-                                openNodePort: false,
-                                publicDomain: network.publicDomain || nanoid(),
-                                domain: network.domain || SEALOS_DOMAIN
-                              });
-                            } else {
-                              updateNetworks(i, {
-                                ...getValues('networks')[i],
-                                networkName: network.networkName || `network-${nanoid()}`,
-                                protocol: network.protocol,
-                                appProtocol: 'HTTP',
-                                openNodePort: true,
-                                openPublicDomain: false,
-                                customDomain: ''
-                              });
-                            }
-                          } else {
-                            updateNetworks(i, {
-                              ...getValues('networks')[i],
-                              openPublicDomain: false,
-                              openNodePort: false
-                            });
-                          }
-                        }}
-                      ></Switch>
-                    </Flex>
-                  </Box>
-
-                  {/* Protocol and Domain Column - Fixed Width */}
-                  <Box w={'500px'}>
-                    <Box mb={'8px'} h={'20px'}></Box>
-                    <Flex alignItems={'center'} h={'35px'}>
-                      {network.openPublicDomain || network.openNodePort ? (
-                        <>
-                          <MySelect
-                            width={'120px'}
-                            height={'32px'}
-                            borderTopRightRadius={0}
-                            borderBottomRightRadius={0}
-                            value={
-                              network.openPublicDomain
-                                ? network.appProtocol
-                                : network.openNodePort
-                                  ? network.protocol
-                                  : 'HTTP'
-                            }
-                            list={ProtocolList}
-                            onchange={(val: any) => {
-                              if (APPLICATION_PROTOCOLS.includes(val)) {
-                                updateNetworks(i, {
-                                  ...getValues('networks')[i],
-                                  serviceName: '',
-                                  protocol: 'TCP',
-                                  appProtocol: val,
-                                  openNodePort: false,
-                                  openPublicDomain: true,
-                                  networkName: network.networkName || `network-${nanoid()}`,
-                                  publicDomain: network.publicDomain || nanoid()
-                                });
-                              } else {
-                                updateNetworks(i, {
-                                  ...getValues('networks')[i],
-                                  serviceName: '',
-                                  protocol: val,
-                                  appProtocol: 'HTTP',
-                                  openNodePort: true,
-                                  openPublicDomain: false,
-                                  customDomain: ''
-                                });
-                              }
-                            }}
-                          />
-                          <Flex
-                            maxW={'350px'}
-                            flex={'1 0 0'}
-                            alignItems={'center'}
-                            h={'32px'}
-                            bg={'grayModern.50'}
-                            px={4}
-                            border={theme.borders.base}
-                            borderLeft={0}
-                            borderTopRightRadius={'md'}
-                            borderBottomRightRadius={'md'}
-                          >
-                            <Tooltip label={t('click_to_copy_tooltip')}>
-                              <Box
-                                flex={1}
-                                userSelect={'all'}
-                                className="textEllipsis"
-                                onClick={() => {
-                                  copyData(`${network.publicDomain}.${network.domain}`);
-                                }}
-                              >
-                                {network.customDomain
-                                  ? network.customDomain
-                                  : network.openNodePort
-                                    ? network?.nodePort
-                                      ? `${network.protocol.toLowerCase()}.${network.domain}:${
-                                          network.nodePort
-                                        }`
-                                      : `${network.protocol.toLowerCase()}.${network.domain}:${t(
-                                          'pending_to_allocated'
-                                        )}`
-                                    : `${network.publicDomain}.${network.domain}`}
-                              </Box>
-                            </Tooltip>
-
-                            {network.openPublicDomain && !network.openNodePort && (
-                              <Box
-                                fontSize={'11px'}
-                                color={'brightBlue.600'}
-                                cursor={'pointer'}
-                                onClick={() =>
-                                  setCustomAccessModalData({
-                                    publicDomain: network.publicDomain,
-                                    currentCustomDomain: network.customDomain,
-                                    domain: network.domain
-                                  })
-                                }
-                              >
-                                {t('Custom Domain')}
-                              </Box>
-                            )}
-                          </Flex>
-                        </>
-                      ) : (
-                        <Box w={'470px'} h={'32px'}></Box>
-                      )}
-                    </Flex>
-                  </Box>
-
-                  {/* Delete Button Column - Fixed Width */}
-                  {networks.length > 1 && (
-                    <Box w={'50px'} ml={3}>
-                      <Box mb={'8px'} h={'20px'}></Box>
-                      <IconButton
-                        height={'32px'}
-                        width={'32px'}
-                        aria-label={'button'}
-                        variant={'outline'}
-                        bg={'#FFF'}
-                        _hover={{
-                          color: 'red.600',
-                          bg: 'rgba(17, 24, 36, 0.05)'
-                        }}
-                        icon={<MyIcon name={'delete'} w={'16px'} fill={'#485264'} />}
-                        onClick={() => removeNetworks(i)}
-                      />
-                    </Box>
-                  )}
-                </Flex>
-              ))}
-              {exceededQuotas.some(({ type }) => type === 'nodeport') && (
-                <Box pt={'16px'}>
-                  <Box fontSize={'md'} color={'red.500'} mb={1}>
-                    {t('nodeport_exceeds_quota', {
-                      requested:
-                        getValues('networks').filter((item) => item.openNodePort)?.length || 0,
-                      limit: exceededQuotas.find(({ type }) => type === 'nodeport')?.limit ?? 0,
-                      used: exceededQuotas.find(({ type }) => type === 'nodeport')?.used ?? 0
-                    })}
-                  </Box>
-                  <Box fontSize={'md'} color={'red.500'}>
-                    {t('please_upgrade_plan.0')}
-                    <Box
-                      as="span"
-                      cursor="pointer"
-                      fontWeight="semibold"
-                      color="blue.600"
-                      textDecoration="underline"
-                      onClick={handleOpenCostcenter}
-                    >
-                      {t('please_upgrade_plan.1')}
-                    </Box>
-                    {t('please_upgrade_plan.2')}
-                  </Box>
-                </Box>
-              )}
-            </Box>
-          </Box>
           {/* settings */}
           {already && (
             <Accordion
@@ -1486,23 +1216,7 @@ const Form = ({
           )}
         </Box>
       </Grid>
-      {!!customAccessModalData && (
-        <CustomAccessModal
-          {...customAccessModalData}
-          onClose={() => setCustomAccessModalData(undefined)}
-          onSuccess={(e) => {
-            const i = networks.findIndex(
-              (item) => item.publicDomain === customAccessModalData.publicDomain
-            );
-            if (i === -1) return;
-            updateNetworks(i, {
-              ...networks[i],
-              customDomain: e
-            });
-            onDomainVerified?.({ index: i, customDomain: e });
-          }}
-        />
-      )}
+
       {isEditEnvs && (
         <EditEnvs defaultEnv={envs} onClose={onCloseEditEnvs} successCb={(e) => replaceEnvs(e)} />
       )}
