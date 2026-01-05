b60b828fd71c77b9d7fdf57f70808ba3cae69c72
jingyang <72259332+zjy365@users.noreply.github.com>
2026-01-05 12:32:42 +0800

    fix(applaunchpad): prevent using internal domains as custom domain (#6482)

--- frontend/providers/applaunchpad/src/pages/app/edit/components/NetworkSection.tsx
+++ frontend/providers/applaunchpad/src/pages/app/edit/components/NetworkSection.tsx
@@ -296,8 +296,8 @@ export function NetworkSection({
                           network.openPublicDomain
                             ? network.appProtocol
                             : network.openNodePort
-                              ? network.protocol
-                              : 'HTTP'
+                            ? network.protocol
+                            : 'HTTP'
                         }
                         list={ProtocolList}
                         onchange={(val: any) => {
@@ -334,14 +334,14 @@ export function NetworkSection({
                             {network.customDomain
                               ? network.customDomain
                               : network.openNodePort
-                                ? network?.nodePort
-                                  ? `${network.protocol.toLowerCase()}.${network.domain}:${
-                                      network.nodePort
-                                    }`
-                                  : `${network.protocol.toLowerCase()}.${network.domain}:${t(
-                                      'pending_to_allocated'
-                                    )}`
-                                : `${network.publicDomain}.${network.domain}`}
+                              ? network?.nodePort
+                                ? `${network.protocol.toLowerCase()}.${network.domain}:${
+                                    network.nodePort
+                                  }`
+                                : `${network.protocol.toLowerCase()}.${network.domain}:${t(
+                                    'pending_to_allocated'
+                                  )}`
+                              : `${network.publicDomain}.${network.domain}`}
                           </Box>
                         </Tooltip>
 
