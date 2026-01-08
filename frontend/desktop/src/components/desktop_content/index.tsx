7cbd1e6464cd46f63d1fa7eaa3e5206dd129bb1c
jingyang <72259332+zjy365@users.noreply.github.com>
2026-01-08 17:24:08 +0800

    feat(template): add refresh button to instance header (#6498)
    * feat(template): add refresh button to instance header
    
    * fix regex
    
    * update
    
    * update

--- frontend/desktop/src/components/desktop_content/index.tsx
+++ frontend/desktop/src/components/desktop_content/index.tsx
@@ -274,7 +274,7 @@ export default function Desktop() {
         >
           <Image
             src={backgroundImage || desktopBackgroundImage || '/images/bg-light.svg'}
-            alt="background"
+            alt=""
             width="100%"
             height="100vh"
             objectFit="cover"
