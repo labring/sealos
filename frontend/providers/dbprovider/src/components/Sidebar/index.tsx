ea7c2a4a08aec6a832b49fcfe9576566828e2529
jingyang <72259332+zjy365@users.noreply.github.com>
2026-02-26 14:31:25 +0800

    feat(dbprovider): respect BACKUP_ENABLED config (#6716)

--- frontend/providers/dbprovider/src/components/Sidebar/index.tsx
+++ frontend/providers/dbprovider/src/components/Sidebar/index.tsx
@@ -3,37 +3,46 @@ import MyIcon from '../Icon';
 
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
+import useEnvStore from '@/store/env';
+import { useMemo } from 'react';
 
 export default function Sidebar() {
   const { t } = useTranslation();
   const router = useRouter();
+  const { SystemEnv } = useEnvStore();
 
-  const siderbarMap = [
-    {
-      label: t('DataBase'),
-      icon: (
-        <MyIcon
-          name="logoLinear"
-          w={'24px'}
-          h={'24px'}
-          color={router.pathname === '/dbs' ? 'black' : 'grayModern.500'}
-        />
-      ),
-      path: '/dbs'
-    },
-    {
-      label: t('Backup'),
-      icon: (
-        <MyIcon
-          name="backup"
-          w={'24px'}
-          h={'24px'}
-          color={router.pathname === '/backups' ? 'black' : 'grayModern.500'}
-        />
-      ),
-      path: '/backups'
-    }
-  ];
+  const siderbarMap = useMemo(() => {
+    return [
+      {
+        label: t('DataBase'),
+        icon: (
+          <MyIcon
+            name="logoLinear"
+            w={'24px'}
+            h={'24px'}
+            color={router.pathname === '/dbs' ? 'black' : 'grayModern.500'}
+          />
+        ),
+        path: '/dbs'
+      },
+      ...(SystemEnv.BACKUP_ENABLED
+        ? [
+            {
+              label: t('Backup'),
+              icon: (
+                <MyIcon
+                  name="backup"
+                  w={'24px'}
+                  h={'24px'}
+                  color={router.pathname === '/backups' ? 'black' : 'grayModern.500'}
+                />
+              ),
+              path: '/backups'
+            }
+          ]
+        : [])
+    ];
+  }, [SystemEnv.BACKUP_ENABLED, router.pathname, t]);
 
   return (
     <Stack w={'80px'} py={'12px'} px={'8px'} flexShrink={0} spacing={'8px'}>
