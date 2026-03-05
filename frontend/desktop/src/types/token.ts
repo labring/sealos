aad9fa0cec880af6dd121a547dc21f9a47284101
Nixieboluo <me@sagirii.me>
2026-03-05 11:35:20 +0800

    feat(desktop): basic oauth2 scaffolding
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- frontend/desktop/src/types/token.ts
+++ frontend/desktop/src/types/token.ts
@@ -3,6 +3,22 @@ export type AuthenticationTokenPayload = {
   userId: string;
   regionUid?: string;
 };
+
+export type OAuth2TokenPayload = {
+  sub: string;
+  client_id: string;
+  preferred_username?: string;
+  scope?: string;
+};
+
+export type OAuth2AccessTokenPayload = OAuth2TokenPayload & {
+  token_type: 'access_token';
+};
+
+export type OAuth2RefreshTokenPayload = OAuth2TokenPayload & {
+  token_type: 'refresh_token';
+};
+
 export type AccessTokenPayload = {
   regionUid: string;
   userCrUid: string;
