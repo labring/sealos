b43b5873a7d061ade92421750aee4d9bbf6dd4c9
zjy365 <3161362058@qq.com>
2026-03-18 12:03:47 +0800

    fix launchpad

---
+++ frontend/providers/applaunchpad/src/components/ApiDocs.tsx
@@ -0,0 +1,26 @@
+import { ApiReferenceReact } from '@scalar/api-reference-react';
+import { openApiDocument } from '@/types/openapi';
+import { SEALOS_DOMAIN } from '@/store/static';
+import { getUserKubeConfig } from '@/utils/user';
+
+import '@scalar/api-reference-react/style.css';
+
+export default function ApiDocs() {
+  const config = {
+    content: openApiDocument(SEALOS_DOMAIN),
+    authentication: {
+      preferredSecurityScheme: ['kubeconfigAuth'],
+      securitySchemes: {
+        kubeconfigAuth: {
+          in: 'header',
+          name: 'Authorization',
+          value: encodeURIComponent(getUserKubeConfig() || '')
+        }
+      }
+    }
+    // cdn: process.env.NEXT_PUBLIC_MOCK_USER
+    //   ? undefined
+    //   : `https://devbox.${SEALOS_DOMAIN}/scalar/cdn.js`
+  };
+  return <ApiReferenceReact configuration={config} />;
+}
