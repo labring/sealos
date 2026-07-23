9a4a1533db75b05e6bb49c3b097603f3afffd026
cuisongliu <cuisongliu@qq.com>
2026-07-23 13:41:49 +0800

    chore(frontend): remove cronjob references from configuration files
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- frontend/providers/cronjob/src/services/kubernet.ts
+++
@@ -1,9 +0,0 @@
-export interface ApiResp<T = any> {
-  code: number;
-  message: string;
-  data?: T;
-}
-
-const isApiResp = (x: any): x is ApiResp =>
-  typeof x.code === 'number' && typeof x.message === 'string';
-export { isApiResp };
