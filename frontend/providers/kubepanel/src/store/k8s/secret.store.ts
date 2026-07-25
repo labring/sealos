883b50c7d8837a05c00b7348984ff877e5f67789
cuisongliu <cuisongliu@qq.com>
2026-07-26 00:22:50 +0800

    chore(frontend): remove kubepanel provider

--- frontend/providers/kubepanel/src/store/k8s/secret.store.ts
+++
@@ -1,8 +0,0 @@
-import { Secret, SecretData } from '@/k8slens/kube-object';
-import { SecretStore } from '@/types/state';
-import { create } from 'zustand';
-import { createKubeStoreSlice } from './kube.store';
-
-export const useSecretStore = create<SecretStore>()((...a) => ({
-  ...createKubeStoreSlice<Secret, SecretData>(Secret.kind, Secret)(...a)
-}));
